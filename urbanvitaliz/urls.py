45706d7cd7261abdbcc87f6455e095d321a84113
Guillaume Libersat <glibersat@sigill.org>
2021-06-01 13:00:38 +0200

    Engine/Static files  - Add Sass engine  - Add Bootstrap 5 files  - Serve static files while in dev
    Global
     - Add base scss stylesheets
     - Beginning of base skeleton template
    
    Onboarding
     - Beginning of form styling

--- urbanvitaliz/urls.py
+++ urbanvitaliz/urls.py
@@ -7,7 +7,10 @@ author  : raphael.marvie@beta.gouv.fr,guillaume.libersat@beta.gouv.fr
 created : 2021-05-26 11:29:25 CEST
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+
 from django.urls import path
 
 from magicauth import views as magicauth_views
@@ -18,7 +21,7 @@ from urbanvitaliz.apps.projects.urls import urlpatterns as projects_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns.extend(magicauth_urls)
 urlpatterns.extend(projects_urls)
