5f006a6dc5549cbc494c8eed84bddee287222efa
Raphael Marvie <160491+rmarvie@users.noreply.github.com>
2021-06-01 19:13:52 +0200

    Fix error on scss usage
    - introduce new entry in MANIFEST.in to include home/static
    - update deploy task in fabfile to compile scss on deployment

--- urbanvitaliz/settings/common.py
+++ urbanvitaliz/settings/common.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2_hlxu=(eyru-h_o-n!e2@xc080zfqk0x6v$wm2#%b*=*-i^)$"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
