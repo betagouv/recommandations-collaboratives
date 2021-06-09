42fb1ce03524cb047d9658150b1ace848a896bba
Raphael Marvie <raphael.marvie@beta.gouv.fr>
2021-06-09 09:32:52 +0200

    Introduce login management
    - introduces the creation of new user in the db when email is unknown
    - protect pages with staff status of user and is_staff_or_403 helper
    - first tests for checking access control of pages

--- urbanvitaliz/apps/home/utils.py
+++ urbanvitaliz/apps/home/utils.py
@@ -9,11 +9,12 @@ created: 2021-06-08 09:56:53 CEST
 
 from django.contrib.auth import models as auth
 
+
 def create_user(email=None):
-    """Create a new user if no one exists with given email
-    """
+    """Create a new user if no one exists with given email"""
     if not email:
         raise forms.ValidationError("Adresse email non reconnue")
     auth.User.objects.create_user(email=email, username=email)
+
 
 # eof
