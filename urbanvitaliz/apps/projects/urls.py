904ea6b1f83ff40b5e88d2af831cab19c8334041
Guillaume Libersat <glibersat@sigill.org>
2021-06-28 17:43:14 +0200

    Add latest Projects feed (RSS)

--- urbanvitaliz/apps/projects/urls.py
+++ urbanvitaliz/apps/projects/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     path(r"macollectivite/", views.local_authority, name="projects-local-authority"),
     # projects for switchtenders
     path(r"projects/", views.project_list, name="projects-project-list"),
+    path("projects/feed/", views.LatestProjectsFeed()),
     path(
         r"project/<int:project_id>/",
         views.project_detail,
