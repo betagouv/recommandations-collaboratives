7e6d54c139d5f8e43cfa54d46efdbd26732265cd
Raphael Marvie <raphael.marvie@beta.gouv.fr>
2021-06-29 11:59:57 +0200

    Introduce template tag for current project
    - fetch project from session

---
+++ urbanvitaliz/apps/projects/templatetags/projects_extra.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+"""
+Extra template tags for projects
+
+authors: raphael.marvie@beta.gouv.fr, guillaume.libersat@beta.gouv.fr
+created: 2021-06-29 11:30:42 CEST
+"""
+
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def current_project(session):
+    """Return current project contained in session"""
+    try:
+        project_id = session.get("project_id")
+        return models.Project.fetch().get(id=project_id)
+    except:
+        pass
+
+
+# eof
