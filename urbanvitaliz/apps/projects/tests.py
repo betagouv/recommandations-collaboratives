1844a783ebbbf1e2225e08ccf7dbdebce0a2c3ac
Raphael Marvie <raphael.marvie@beta.gouv.fr>
2021-06-29 11:59:57 +0200

    Introduce template tag for current project
    - fetch project from session

--- urbanvitaliz/apps/projects/tests.py
+++ urbanvitaliz/apps/projects/tests.py
@@ -3,7 +3,7 @@
 """
 Tests for project application
 
-authors : raphael.marvie@beta.gouv.fr, guillaume.libersat@beta.gouv.fr
+authors: raphael.marvie@beta.gouv.fr, guillaume.libersat@beta.gouv.fr
 created: 2021-06-01 10:11:56 CEST
 """
 
