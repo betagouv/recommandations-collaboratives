c7dff554b13436d583aa00e655e2fe39ee32719b
Guillaume Libersat <glibersat@sigill.org>
2021-07-19 17:44:18 +0200

    Addressbook  - Add Contact  - Listing of contacts for an org  - Creation of contact

--- urbanvitaliz/apps/addressbook/views.py
+++ urbanvitaliz/apps/addressbook/views.py
@@ -54,4 +54,38 @@ def organization_details(request, organization_id):
     is_staff_or_403(request.user)
 
     organization = get_object_or_404(models.Organization, pk=organization_id)
-    return render(request, "addressbook/organization/details.html", locals())
+    return render(request, "addressbook/organization_details.html", locals())
+
+
+########################################################################################
+# Contact
+########################################################################################
+
+
+class ContactForm(forms.ModelForm):
+    """Form for creating an Contact"""
+
+    class Meta:
+        model = models.Contact
+        fields = ["first_name", "last_name", "division", "email", "phone_no"]
+
+
+@login_required
+def contact_create(request, organization_id: int):
+    """Create a new Contact"""
+    is_staff_or_403(request.user)
+
+    organization = get_object_or_404(models.Organization, pk=organization_id)
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.organization = organization
+            instance.save()
+            return redirect(
+                reverse("addressbook-organization-details", args=[organization.pk])
+            )
+    else:
+        form = ContactForm()
+    return render(request, "addressbook/contact_create.html", locals())
