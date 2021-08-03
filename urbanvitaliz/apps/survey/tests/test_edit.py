4522132c5e39cd6476861724b157d876b0e3fd8c
Raphael Marvie <raphael.marvie@beta.gouv.fr>
2021-08-03 14:47:36 +0200

    Refactor survey application views
    - split views into fill and edit modules
    - introduce tests for survey details

--- urbanvitaliz/apps/survey/tests/test_edit.py
+++ urbanvitaliz/apps/survey/tests/test_edit.py
@@ -17,20 +17,72 @@ from .. import models
 
 
 ########################################################################
-# Survey
+# surveys
 ########################################################################
 
 
 @pytest.mark.django_db
-def test_survey_detail_contains_question_set(client):
+def test_survey_detail_contains_question_set_links(client):
     qs = Recipe(models.QuestionSet).make()
-    url = reverse("survey-editor-survey-details", args=(qs.survey_id,))
+    url = reverse("survey-editor-survey-details", args=[qs.survey_id])
     with login(client, is_staff=True):
         response = client.get(url)
-    detail_url = reverse("survey-editor-question-set-details", args=(qs.id,))
-    assertContains(response, detail_url)
-    new_url = reverse("survey-editor-question-set-create", args=(qs.survey_id,))
-    assertContains(response, detail_url)
+    detail_url = reverse("survey-editor-question-set-details", args=[qs.id])
+    assertContains(response, f'href="{detail_url}"')
+    new_url = reverse("survey-editor-question-set-create", args=[qs.survey_id])
+    assertContains(response, f'href="{new_url}"')
+
+
+########################################################################
+# question_set
+########################################################################
+
+
+@pytest.mark.django_db
+def test_question_set_detail_contains_update_links(client):
+    qs = Recipe(models.QuestionSet).make()
+    url = reverse("survey-editor-question-set-details", args=[qs.id])
+    with login(client, is_staff=True):
+        response = client.get(url)
+    update_url = reverse("survey-editor-question-set-update", args=[qs.id])
+    assertContains(response, f'href="{update_url}"')
+
+
+########################################################################
+# question
+########################################################################
+
+
+@pytest.mark.django_db
+def test_question_set_detail_contains_question_links(client):
+    qs = Recipe(models.QuestionSet).make()
+    question = Recipe(models.Question, question_set=qs).make()
+    url = reverse("survey-editor-question-set-details", args=[qs.id])
+    with login(client, is_staff=True):
+        response = client.get(url)
+    update_url = reverse("survey-editor-question-update", args=[question.id])
+    assertContains(response, f'href="{update_url}"')
+    create_url = reverse("survey-editor-question-create", args=[qs.id])
+    assertContains(response, f'href="{create_url}"')
+
+
+########################################################################
+# choice
+########################################################################
+
+
+@pytest.mark.django_db
+def test_question_set_detail_contains_choice_links(client):
+    qs = Recipe(models.QuestionSet).make()
+    question = Recipe(models.Question, question_set=qs).make()
+    choice = Recipe(models.Choice, question=question).make()
+    url = reverse("survey-editor-question-set-details", args=[qs.id])
+    with login(client, is_staff=True):
+        response = client.get(url)
+    update_url = reverse("survey-editor-choice-update", args=[choice.id])
+    assertContains(response, f'href="{update_url}"')
+    create_url = reverse("survey-editor-choice-create", args=[question.id])
+    assertContains(response, f'href="{create_url}"')
 
 
 # eof
--- urbanvitaliz/apps/survey/urls.py
+++ urbanvitaliz/apps/survey/urls.py
@@ -10,84 +10,87 @@ created : 2021-07-27 10:06:23 CEST
 
 from django.urls import path
 
-from . import views
+from .views import fill
+from .views import edit
 
 urlpatterns = [
+    #
+    # filling surveys
     path(
         "projects/survey/<int:session_id>/q-<int:question_id>/",
-        views.survey_question_details,
+        fill.survey_question_details,
         name="survey-question-details",
     ),
     path(
         "projects/survey/<int:session_id>/",
-        views.SessionDetailsView.as_view(),
+        fill.SessionDetailsView.as_view(),
         name="survey-session-details",
     ),
     path(
         "projects/survey/<int:session_id>/done",
-        views.SessionDoneView.as_view(),
+        fill.SessionDoneView.as_view(),
         name="survey-session-done",
     ),
     #
-    # editor
+    # editing surveys
     path(
         r"survey/editor/survey/<int:survey_id>/",
-        views.editor_survey_details,
+        edit.survey_details,
         name="survey-editor-survey-details",
     ),
     path(
         r"survey/editor/survey/<int:survey_id>/question_set/create/",
-        views.editor_question_set_create,
+        edit.question_set_create,
         name="survey-editor-question-set-create",
     ),
     path(
         r"survey/editor/question_set/<int:question_set_id>/",
-        views.editor_question_set_details,
+        edit.question_set_details,
         name="survey-editor-question-set-details",
     ),
     path(
         r"survey/editor/question_set/<int:question_set_id>/update/",
-        views.editor_question_set_update,
+        edit.question_set_update,
         name="survey-editor-question-set-update",
     ),
     path(
         r"survey/editor/question_set/<int:question_set_id>/delete/",
-        views.editor_question_set_delete,
+        edit.question_set_delete,
         name="survey-editor-question-set-delete",
     ),
     path(
         r"survey/editor/question_set/<int:question_set_id>/question/create/",
-        views.editor_question_create,
+        edit.question_create,
         name="survey-editor-question-create",
     ),
     path(
         r"survey/editor/question/<int:question_id>/",
-        views.editor_question_details,
+        edit.question_details,
         name="survey-editor-question-details",
     ),
     path(
         r"survey/editor/question/<int:question_id>/update/",
-        views.editor_question_update,
+        edit.question_update,
         name="survey-editor-question-update",
     ),
     path(
         r"survey/editor/question/<int:question_id>/delete/",
-        views.editor_question_delete,
+        edit.question_delete,
         name="survey-editor-question-delete",
     ),
     path(
         r"survey/editor/question/<int:question_id>/choice/create/",
-        views.editor_choice_create,
+        edit.choice_create,
         name="survey-editor-choice-create",
     ),
     path(
         r"survey/editor/choice/<int:choice_id>/update/",
-        views.editor_choice_update,
+        edit.choice_update,
         name="survey-editor-choice-update",
     ),
     path(
         r"survey/editor/choice/<int:choice_id>/delete/",
-        views.editor_choice_delete,
+        edit.choice_delete,
         name="survey-editor-choice-delete",
     ),
 ]
