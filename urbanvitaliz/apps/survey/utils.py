b1bb94ee0aabafb715884bd1221d092bcc910b2b
Guillaume Libersat <glibersat@sigill.org>
2021-09-07 16:53:50 +0200

    Survey  - Enhance QuestionSet Summary Card
    Project
     - Split page using tabs
     - Integrate survey state

---
+++ urbanvitaliz/apps/survey/utils.py
@@ -0,0 +1,15 @@
+import math
+
+from . import models
+
+
+def compute_qs_completion(session, question_set):
+    """Return an optimistic percentage of completion for a question_set"""
+    answer_count = models.Answer.objects.filter(
+        session=session, question__question_set=question_set
+    ).count()
+    question_count = models.Question.objects.filter(
+        question_set=question_set, precondition=""
+    ).count()
+
+    return min(math.ceil(answer_count / question_count * 100), 100)
