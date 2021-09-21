7fef44c3971d32de1ead335a42beb450ac3bfe82
Guillaume Libersat <glibersat@sigill.org>
2021-09-21 15:57:28 +0200

    Survey  - how and why field are now by default collapsed and expandable
    App
     - Move foldable text utility to base project dirs

---
+++ urbanvitaliz/static/js/text.js
@@ -0,0 +1,15 @@
+function textFoldToggle(id) {
+    var excerpt = document.getElementById("excerpt-" + id);
+    var moreText = document.getElementById("more-" + id);
+    var btnText = document.getElementById("foldBtn-" + id);
+
+    if (moreText.style.display === "inline") {
+            excerpt.style.display = "inline";
+        btnText.innerHTML = "[Lire tout]";
+        moreText.style.display = "none";
+    } else {
+        excerpt.style.display = "none";
+        btnText.innerHTML = "[Réduire]";
+        moreText.style.display = "inline";
+    }
+}
