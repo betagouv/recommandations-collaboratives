f72049d734c7934edceeef96e2a87f499635d02a
Raphael Marvie <raphael.marvie@beta.gouv.fr>
2021-09-28 12:30:46 +0200

    Fix error in settings

--- urbanvitaliz/settings/common.py
+++ urbanvitaliz/settings/common.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "urbanvitaliz.apps.geomatics",
     "urbanvitaliz.apps.addressbook",
     "urbanvitaliz.apps.survey",
-    "urbanvitaliz.apps.notifications",
+    "urbanvitaliz.apps.reminders",
     "django_gravatar",
 ]
 
