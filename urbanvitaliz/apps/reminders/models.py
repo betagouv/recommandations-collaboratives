006b5eabd5ac4637b98df9a93b3af43607b9b9ab
Raphael Marvie <raphael.marvie@beta.gouv.fr>
2021-09-28 13:40:38 +0200

    Introduces base model and api to add reminder.

--- urbanvitaliz/apps/reminders/models.py
+++ urbanvitaliz/apps/reminders/models.py
@@ -1,3 +1,50 @@
+# encoding: utf-8
+
+"""
+Models for reminders
+
+author  : raphael.marvie@beta.gouv.fr, guillaume.libersat@beta.gouv.fr
+created : 2021-09-28 12:40:54 CEST
+"""
+
+
 from django.db import models
 
-# Create your models here.
+
+class MailManager(models.Manager):
+    """Manager for mails to send"""
+
+    def get_queryset(self):
+        return super().get_queryset().filter(sent=None)
+
+
+class SentMailManager(models.Manager):
+    """Manager for sent mails"""
+
+    def get_queryset(self):
+        return super().get_queryset().exclude(sent=None)
+
+
+class Mail(models.Model):
+    """Represents a mail to be sent on a given date"""
+
+    to_send = MailManager()
+    sent = SentMailManager()
+
+    recipient = models.CharField(max_length=128)
+    subject = models.TextField(default="")
+    text = models.TextField(default="")
+    html = models.TextField(default="")
+    deadline = models.DateField()
+
+    sent = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "mail"
+        verbose_name_plural = "mails"
+
+    def __str__(self):
+        return f"{self.recipient} {self.deadline}"
+
+
+# eof
--- urbanvitaliz/apps/reminders/tests.py
+++ urbanvitaliz/apps/reminders/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+# encoding: utf-8
 
-# Create your tests here.
+"""
+Tests for reminders application
+
+authors: raphael.marvie@beta.gouv.fr, guillaume.libersat@beta.gouv.fr
+created: 2021-09-28 13:17:53 CEST
+"""
+
+import datetime
+import pytest
+
+from django.test.client import RequestFactory
+
+# from model_bakery.recipe import Recipe
+
+from . import api
+from . import models
+
+
+@pytest.mark.django_db
+def test_create_mail_reminder_using_provided_information():
+    url = "/project/action/0/"
+    request = RequestFactory().get(url)
+    recipient = "test@example.com"
+    subject = "[UV] action reminder"
+    template = "reminders/test"
+    delay = 14
+    extra = {"notes": "some notes."}
+
+    api.create_reminder_email(
+        request, recipient, subject, template, delay=delay, extra_context=extra
+    )
+
+    reminder = models.Mail.to_send.all()[0]
+
+    assert reminder.recipient == recipient
+    assert reminder.subject == subject
+    assert extra["notes"] in reminder.text
+    assert url in reminder.text
+
+    assert reminder.deadline == datetime.date.today() + datetime.timedelta(days=delay)
+
+
+# eof
