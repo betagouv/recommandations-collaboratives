c310c5673a577daeb4c1d9af0ea19fa6596edd5a
Guillaume Libersat <glibersat@sigill.org>
2021-10-05 16:59:21 +0200

    Global  - don't create action if user is staff

--- urbanvitaliz/apps/projects/signals.py
+++ urbanvitaliz/apps/projects/signals.py
@@ -21,9 +21,13 @@ reminder_created = django.dispatch.Signal()
 
 @receiver(reminder_created)
 def log_reminder_created(sender, task, project, user, **kwargs):
-    action.send(
-        user, verb="a créé un rappel sur l'action", action_object=task, target=project
-    )
+    if not user.is_staff:
+        action.send(
+            user,
+            verb="a créé un rappel sur l'action",
+            action_object=task,
+            target=project,
+        )
 
 
 ######
@@ -37,19 +41,25 @@ action_undone = django.dispatch.Signal()
 
 @receiver(action_accepted)
 def log_action_accepted(sender, task, project, user, **kwargs):
-    action.send(user, verb="a accepté l'action", action_object=task, target=project)
+    if not user.is_staff:
+        action.send(user, verb="a accepté l'action", action_object=task, target=project)
 
 
 @receiver(action_rejected)
 def log_action_rejected(sender, task, project, user, **kwargs):
-    action.send(user, verb="a refusé l'action", action_object=task, target=project)
+    if not user.is_staff:
+        action.send(user, verb="a refusé l'action", action_object=task, target=project)
 
 
 @receiver(action_done)
 def log_action_done(sender, task, project, user, **kwargs):
-    action.send(user, verb="a terminé l'action", action_object=task, target=project)
+    if not user.is_staff:
+        action.send(user, verb="a terminé l'action", action_object=task, target=project)
 
 
 @receiver(action_undone)
 def log_action_undone(sender, task, project, user, **kwargs):
-    action.send(user, verb="a redémarré l'action", action_object=task, target=project)
+    if not user.is_staff:
+        action.send(
+            user, verb="a redémarré l'action", action_object=task, target=project
+        )
