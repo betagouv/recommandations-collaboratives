cfaa99ee673dd8845dff874a64e9455c45c1aad6
Guillaume Libersat <glibersat@sigill.org>
2021-10-05 16:59:21 +0200

    Global  - don't create action if user is staff

--- urbanvitaliz/apps/survey/signals.py
+++ urbanvitaliz/apps/survey/signals.py
@@ -7,9 +7,10 @@ survey_started = django.dispatch.Signal()
 
 @receiver(survey_started)
 def log_survey_started(sender, survey, project, request, **kwargs):
-    action.send(
-        request.user,
-        verb="a démarré le questionnaire",
-        action_object=survey,
-        target=project,
-    )
+    if not request.user.is_staff:
+        action.send(
+            request.user,
+            verb="a démarré le questionnaire",
+            action_object=survey,
+            target=project,
+        )
