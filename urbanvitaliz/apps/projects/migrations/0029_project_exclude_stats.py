f3f2db6330784a04ef95068bb3881e38bf6f7841
Guillaume Libersat <glibersat@sigill.org>
2021-11-01 11:07:29 +0100

    Home  - Update stats

--- urbanvitaliz/apps/projects/migrations/0029_project_exclude_stats.py
+++ urbanvitaliz/apps/projects/migrations/0029_project_exclude_stats.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0028_taskfollowuprsvp'),
+        ("projects", "0028_taskfollowuprsvp"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='project',
-            name='exclude_stats',
+            model_name="project",
+            name="exclude_stats",
             field=models.BooleanField(default=False),
         ),
     ]
