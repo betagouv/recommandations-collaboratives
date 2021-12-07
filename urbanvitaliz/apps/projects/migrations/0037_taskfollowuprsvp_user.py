5fabeb75aa382b958c84290887bbdb98f3c29e43
Guillaume Libersat <glibersat@sigill.org>
2021-12-07 10:46:20 +0100

    Resource  - Widget: Show icon when a task recommendation exists

--- urbanvitaliz/apps/projects/migrations/0037_taskfollowuprsvp_user.py
+++ urbanvitaliz/apps/projects/migrations/0037_taskfollowuprsvp_user.py
@@ -9,14 +9,19 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('projects', '0036_project_switchtender'),
+        ("projects", "0036_project_switchtender"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='taskfollowuprsvp',
-            name='user',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='rsvp_followups', to='auth.user'),
+            model_name="taskfollowuprsvp",
+            name="user",
+            field=models.ForeignKey(
+                default=1,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="rsvp_followups",
+                to="auth.user",
+            ),
             preserve_default=False,
         ),
     ]
