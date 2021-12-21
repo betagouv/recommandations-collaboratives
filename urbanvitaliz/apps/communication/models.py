bd2b7f3924ccec5058b58a34e896c7d19eca17c4
Guillaume Libersat <glibersat@sigill.org>
2021-12-21 17:47:40 +0100

    Communication  - Introduce module for SendInBlue
    Projects
     - Use send_email from communication

---
+++ urbanvitaliz/apps/communication/models.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+"""
+Models for communication
+
+author  : raphael.marvie@beta.gouv.fr, guillaume.libersat@beta.gouv.fr
+created : 2021-12-21 12:40:54 CEST
+"""
+
+from django.db import models
+
+
+class EmailTemplate(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    sib_id = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} - {self.sib_id}"
+
+
+# eof
