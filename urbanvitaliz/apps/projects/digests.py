3d2a4cae3b2c93ef767ee7aeedeff3ff48e9f050
Raphael Marvie <raphael.marvie@beta.gouv.fr>
2022-02-03 16:52:43 +0100

    Refactoring the digest module
    Part one: refactoring the recommendations digest function.

--- urbanvitaliz/apps/projects/digests.py
+++ urbanvitaliz/apps/projects/digests.py
@@ -1,3 +1,12 @@
+# encoding: utf-8
+
+"""
+sending digest email to users and switchtenders
+
+authors: guillaume.libersat@beta.gouv.fr, raphael.marvie@beta.gouv.fr
+updated: 2022-02-03 16:16:37 CET
+"""
+
 from itertools import groupby
 
 from django.contrib.contenttypes.models import ContentType
@@ -8,10 +17,14 @@ from urbanvitaliz.apps.communication.api import send_email
 from . import models
 
 
+########################################################################
+# reco digests
+########################################################################
+
+
 def send_digests_for_new_recommendations_by_user(user):
     """
-    For a given user, send a digest email containing all new recommendation.
-    Each project generates a single email.
+    Send a digest email per project with all its new recommendations for given user.
     """
     project_ct = ContentType.objects.get_for_model(models.Project)
 
@@ -22,7 +35,25 @@ def send_digests_for_new_recommendations_by_user(user):
     )
 
     if notifications.count() == 0:
+        # NOTE should we return a boolean or the number of notification sent?
         return False
+
+    skipped_projects = send_digest_by_project(user, notifications)
+
+    # Mark them as dispatched
+    # NOTE it would mean more request but isn;t it cleaner to do it in inner function?
+    notifications.exclude(target_object_id__in=skipped_projects).mark_as_sent()
+
+    # NOTE should we return a boolean or the number of notification sent?
+    return True
+
+
+def send_digest_by_project(user, notifications):
+    """Send an email per project containing its notifications."""
+
+    user_name = f"{user.first_name} {user.last_name}"
+    if user_name.strip() == "":
+        user_name = "Madame/Monsieur"
 
     skipped_projects = []
     for project_id, project_notifications in groupby(
@@ -34,71 +65,77 @@ def send_digests_for_new_recommendations_by_user(user):
             skipped_projects.append(project_id)
             continue
 
-        recommendations = []
-        notification_count = 0
-        for notification in project_notifications:
-            action = notification.action_object
-            notification_count += 1
-
-            action_link = utils.build_absolute_url(
-                reverse("projects-project-detail", args=[action.project_id])
-                + "#actions",
-            )
-
-            recommendations.append(
-                {
-                    "created_by": {
-                        "first_name": action.created_by.first_name,
-                        "last_name": action.created_by.last_name,
-                        "organization": {
-                            "name": action.created_by.profile.organization
-                            and action.created_by.profile.organization.name
-                            or None
-                        },
-                    },
-                    "intent": action.intent,
-                    "content": action.content,
-                    "resource": {
-                        "title": action.resource and action.resource.title or ""
-                    },
-                    "url": action_link,
-                }
-            )
-
-        project_link = utils.build_absolute_url(
-            reverse("projects-project-detail", args=[action.project_id])
-        )
-        email_params = {
-            "notification_count": notification_count,
-            "project": {
-                "name": action.project.name,
-                "url": project_link,
-                "commune": {
-                    "postal": action.project.commune
-                    and action.project.commune.postal
-                    or "",
-                    "name": action.project.commune
-                    and action.project.commune.name
-                    or "",
-                },
-            },
-            "recos": recommendations,
-        }
-
-        name = f"{user.first_name} {user.last_name}"
-        if name.strip() == "":
-            name = "Madame/Monsieur"
+        digest = make_digest_of_project_recommendations(project, project_notifications)
 
         send_email(
             "new_recommendations_digest",
-            {"name": name, "email": user.email},
-            params=email_params,
+            {"name": user_name, "email": user.email},
+            params=digest,
         )
 
-    # Mark them as dispatched
-    notifications.exclude(target_object_id__in=skipped_projects).mark_as_sent()
+    return skipped_projects
 
-    return True
+
+def make_digest_of_project_recommendations(project, project_notifications):
+    """Return digest for project recommendations to be sent to user"""
+    recommendations = make_recommendations_digest(project_notifications)
+
+    project_link = utils.build_absolute_url(
+        reverse("projects-project-detail", args=[project.id])
+    )
+    digest = {
+        "notification_count": len(recommendations),
+        "project": {
+            "name": project.name,
+            "url": project_link,
+            "commune": {
+                "postal": project.commune and project.commune.postal or "",
+                "name": project.commune and project.commune.name or "",
+            },
+        },
+        "recos": recommendations,
+    }
+
+    return digest
+
+
+def make_recommendations_digest(project_notifications):
+    """Return a digest of all project recommendations"""
+    recommendations = []
+
+    for notification in project_notifications:
+        action = notification.action_object
+        recommendation = make_action_digest(action)
+        recommendations.append(recommendation)
+
+    return recommendations
+
+
+def make_action_digest(action):
+    """Return digest of action"""
+    action_link = utils.build_absolute_url(
+        reverse("projects-project-detail", args=[action.project_id]) + "#actions",
+    )
+    return {
+        "created_by": {
+            "first_name": action.created_by.first_name,
+            "last_name": action.created_by.last_name,
+            "organization": {
+                "name": action.created_by.profile.organization
+                and action.created_by.profile.organization.name
+                or ""
+            },
+        },
+        "intent": action.intent,
+        "content": action.content,
+        "resource": {"title": action.resource and action.resource.title or ""},
+        "url": action_link,
+    }
+
+
+########################################################################
+# new site digests
+########################################################################
 
 
 def send_digests_for_new_sites_by_user(user):
@@ -217,3 +254,6 @@ def send_digest_by_user(user, template_name):
     notifications.mark_as_sent()
 
     return True
+
+
+# eof
