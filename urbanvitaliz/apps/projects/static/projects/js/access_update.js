db320bc8016342f85fbdc609cd238669af4eed2e
R <kalfane.rahman@gmail.com>
2022-03-14 09:40:40 +0000

    Share: Better UI interaction when copying share URL

--- urbanvitaliz/apps/projects/static/projects/js/access_update.js
+++ urbanvitaliz/apps/projects/static/projects/js/access_update.js
@@ -2,10 +2,14 @@ function access_update_app(url) {
     return {
         url: url,
         isCopied: false,
+        selectText: function () {
+            this.$refs.input.select();
+        },
         clipboardCopy: function() {
             navigator.clipboard.writeText(url).then(function () {
                 this.isCopied = true;
                 this.$refs.button.blur();
+                this.selectText();
             }.bind(this));
         }
     }
