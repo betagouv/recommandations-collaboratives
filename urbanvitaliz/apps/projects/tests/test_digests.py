d2b40fb12d23efe14f5e8c2a1275a13fd3563d9f
Guillaume Libersat <glibersat@sigill.org>
2022-03-21 16:31:41 +0100

    Projects  - Remove test for digest / not done projects

--- urbanvitaliz/apps/projects/tests/test_digests.py
+++ urbanvitaliz/apps/projects/tests/test_digests.py
@@ -54,28 +54,6 @@ def test_send_digests_for_new_reco_empty(client):
     assert user.notifications.unsent().count() == 0
 
 
-def test_send_digests_for_new_reco_not_sent_for_not_done_projects(client):
-    user = Recipe(auth.User, username="auser", email="user@example.com").make()
-    switchtender = Recipe(
-        auth.User, username="switchtender", email="switchtender@example.com"
-    ).make()
-    project = baker.make(models.Project, status="READY", emails=[user.email])
-
-    # Generate a notification
-    signals.action_created.send(
-        sender=test_send_digests_for_new_reco,
-        task=models.Task.objects.create(
-            project=project, created_by=switchtender, public=True
-        ),
-        project=project,
-        user=switchtender,
-    )
-
-    digests.send_digests_for_new_recommendations_by_user(user)
-
-    assert user.notifications.unsent().count() == 1
-
-
 ########################################################################
 # new sites digests
 ########################################################################
