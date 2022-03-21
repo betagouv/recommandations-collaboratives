0d7aecb95f80df7fa443ebcaf43df5f292e22222
Guillaume Libersat <glibersat@sigill.org>
2022-03-21 16:00:00 +0100

    Projects  - Add completion bar for knowledge

--- urbanvitaliz/apps/projects/context_processors.py
+++ urbanvitaliz/apps/projects/context_processors.py
@@ -1,3 +1,4 @@
+from urbanvitaliz.apps.survey import models as survey_models
 from urbanvitaliz.utils import check_if_switchtender
 
 from .utils import (can_administrate_project, can_manage_project,
@@ -15,6 +16,14 @@ def active_project_processor(request):
     }
 
     if active_project:
+        try:
+            survey = survey_models.Survey.objects.get(pk=1)  # XXX Hardcoded survey ID
+            session, created = survey_models.Session.objects.get_or_create(
+                project=active_project, survey=survey
+            )
+        except survey_models.Survey.DoesNotExist:
+            session = None
+
         context.update(
             {
                 "active_project_can_manage": can_manage_project(
@@ -23,6 +32,7 @@ def active_project_processor(request):
                 "active_project_can_administrate": can_administrate_project(
                     active_project, request.user
                 ),
+                "active_project_survey_session": session,
             }
         )
 
--- urbanvitaliz/apps/projects/views/detail.py
+++ urbanvitaliz/apps/projects/views/detail.py
@@ -28,53 +28,6 @@ def project_detail(request, project_id=None):
 
 
 @login_required
-def project_detailx(request, project_id=None):
-    """Return the details of given project for switchtender"""
-    project = get_object_or_404(models.Project, pk=project_id)
-
-    # Set this project as active
-    set_active_project_id(request, project.pk)
-
-    # compute permissions
-    can_manage = can_manage_project(project, request.user)
-    can_manage_draft = can_manage_project(project, request.user, allow_draft=True)
-    is_regional_actor = is_regional_actor_for_project(
-        project, request.user, allow_national=True
-    )
-    can_administrate = can_administrate_project(project, request.user)
-
-    # check user can administrate project (member or switchtender)
-    if request.user.email != project.email:
-        # bypass if user is switchtender, all are allowed to view at least
-        if not check_if_switchtender(request.user):
-            can_manage_or_403(project, request.user)
-
-    # Set this project as active
-    set_active_project_id(request, project.pk)
-
-    try:
-        survey = survey_models.Survey.objects.get(pk=1)  # XXX Hardcoded survey ID
-        session, created = survey_models.Session.objects.get_or_create(
-            project=project, survey=survey
-        )
-    except survey_models.Survey.DoesNotExist:
-        session = None
-
-    # Mark this project notifications unread
-    project_ct = ContentType.objects.get_for_model(project)
-    request.user.notifications.filter(
-        target_content_type=project_ct.pk, target_object_id=project.pk
-    ).mark_all_as_read()
-
-    private_note_form = PrivateNoteForm()
-    public_note_form = PublicNoteForm()
-
-    recipients = get_notification_recipients_for_project(project)
-
-    return render(request, "projects/project/", locals())
-
-
-@login_required
 def project_knowledge(request, project_id=None):
     """Return the details of given project for switchtender"""
     project = get_object_or_404(models.Project, pk=project_id)
