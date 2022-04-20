85834af7d5e1d9b11fd0628357f0fe100eabafbe
Guillaume Libersat <glibersat@sigill.org>
2022-04-20 11:40:50 +0200

    Invites  - Finish implementation and tests of invite accept

--- urbanvitaliz/apps/invites/tests.py
+++ urbanvitaliz/apps/invites/tests.py
@@ -1,3 +1,280 @@
-from django.test import TestCase
+# encoding: utf-8
 
-# Create your tests here.
+"""
+Tests for invite application
+
+authors: raphael.marvie@beta.gouv.fr, guillaume.libersat@beta.gouv.fr
+created: 2022-04-20 10:11:56 CEST
+"""
+
+
+import pytest
+from django.contrib.auth import models as auth_models
+from django.urls import reverse
+from django.utils import timezone
+from model_bakery.recipe import Recipe
+from notifications import notify
+from pytest_django.asserts import (assertContains, assertNotContains,
+                                   assertRedirects)
+from urbanvitaliz.utils import login
+
+from . import models
+
+
+################################################################
+# Invite details
+################################################################
+@pytest.mark.django_db
+def test_invite_available_for_everyone(client):
+    invite = Recipe(models.Invite).make()
+    url = reverse("invites-invite-details", args=[invite.pk])
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_invite_show_error_message_if_not_for_current_logged_in_user(client):
+    invite = Recipe(models.Invite).make()
+    url = reverse("invites-invite-details", args=[invite.pk])
+
+    with login(client):
+        response = client.get(url)
+
+    assert response.status_code == 200
+    assertContains(response, "Désolé")
+
+
+@pytest.mark.django_db
+def test_invite_does_not_match_existing_account(client):
+    invited = Recipe(
+        auth_models.User, username="invited", email="invited@example.com"
+    ).make()
+
+    invite = Recipe(models.Invite, email=invited.email).make()
+    url = reverse("invites-invite-details", args=[invite.pk])
+
+    with login(client):
+        response = client.get(url)
+
+    assert response.status_code == 200
+    assertContains(response, "Désolé")
+
+
+@pytest.mark.django_db
+def test_invite_matches_existing_account_for_logged_in_user(client):
+    with login(client) as user:
+        invite = Recipe(models.Invite, email=user.email).make()
+        url = reverse("invites-invite-details", args=[invite.pk])
+        response = client.get(url)
+
+    assert response.status_code == 200
+    assertNotContains(response, "Désolé")
+
+
+@pytest.mark.django_db
+def test_invite_matches_existing_account_redirects_anonyous_user_to_login(client):
+    invited = Recipe(
+        auth_models.User, username="invited", email="invited@example.com"
+    ).make()
+
+    invite = Recipe(models.Invite, email=invited.email).make()
+
+    url = reverse("invites-invite-details", args=[invite.pk])
+    response = client.get(url)
+
+    assert response.status_code == 302
+
+
+################################################################
+# Invite accepts
+################################################################
+@pytest.mark.django_db
+def test_accept_invite_returns_to_details_if_get(client):
+    invite = Recipe(models.Invite).make()
+    url = reverse("invites-invite-accept", args=[invite.pk])
+    response = client.get(url)
+
+    assert response.status_code == 302
+    assert invite.accepted_on is None
+
+
+@pytest.mark.django_db
+def test_accept_invite_matches_existing_account(client):
+    with login(client) as user:
+        invite = Recipe(models.Invite, email=user.email).make()
+        url = reverse("invites-invite-accept", args=[invite.pk])
+        response = client.post(url)
+
+    assert response.status_code == 302
+    invite = models.Invite.objects.get(pk=invite.pk)
+    assert invite.accepted_on is not None
+
+
+@pytest.mark.django_db
+def test_logged_in_user_accepts_invite_without_existing_account(
+    client,
+):
+    with login(client, email="invited@here.tld") as user:
+        invite = Recipe(
+            models.Invite,
+            email="whatever@wherever.com",
+        ).make()
+        url = reverse("invites-invite-accept", args=[invite.pk])
+        response = client.post(url)
+
+    assert response.status_code == 403
+    invite = models.Invite.objects.get(pk=invite.pk)
+    assert invite.accepted_on is None
+    assert user.email not in invite.project.emails
+    assert user not in invite.project.switchtenders.all()
+
+
+@pytest.mark.django_db
+def test_logged_in_user_accepts_invite_switchtender_with_matching_existing_account(
+    client,
+):
+    with login(client, email="invited@here.tld") as user:
+        invite = Recipe(
+            models.Invite,
+            role="SWITCHTENDER",
+            email=user.email,
+            project__name="project",
+            project__location="here",
+        ).make()
+        url = reverse("invites-invite-accept", args=[invite.pk])
+        response = client.post(url)
+
+    assert response.status_code == 302
+    invite = models.Invite.objects.get(pk=invite.pk)
+    assert invite.accepted_on is not None
+    assert user.email not in invite.project.emails
+    assert user in invite.project.switchtenders.all()
+
+
+@pytest.mark.django_db
+def test_logged_in_user_accepts_invite_switchtender_with_mismatched_existing_account(
+    client,
+):
+    invited = Recipe(
+        auth_models.User, username="invited", email="invited@example.com"
+    ).make()
+
+    invite = Recipe(
+        models.Invite,
+        email=invited.email,
+    ).make()
+
+    with login(client, email="invited@here.tld") as user:
+        url = reverse("invites-invite-accept", args=[invite.pk])
+        response = client.post(url)
+
+    assert response.status_code == 403
+    invite = models.Invite.objects.get(pk=invite.pk)
+    assert invite.accepted_on is None
+    assert user.email not in invite.project.emails
+    assert user not in invite.project.switchtenders.all()
+
+
+@pytest.mark.django_db
+def test_logged_in_user_accepts_invite_collaborator_with_matching_existing_account(
+    client,
+):
+    with login(client, email="invited@here.tld") as user:
+        invite = Recipe(
+            models.Invite,
+            role="COLLABORATOR",
+            email=user.email,
+            project__name="project",
+            project__location="here",
+        ).make()
+        url = reverse("invites-invite-accept", args=[invite.pk])
+        response = client.post(url)
+
+    assert response.status_code == 302
+    invite = models.Invite.objects.get(pk=invite.pk)
+    assert invite.accepted_on is not None
+    assert user.email in invite.project.emails
+    assert user not in invite.project.switchtenders.all()
+
+
+@pytest.mark.django_db
+def test_logged_in_user_accepts_invite_collaborator_with_mismatched_existing_account(
+    client,
+):
+    invited = Recipe(
+        auth_models.User, username="invited", email="invited@example.com"
+    ).make()
+
+    invite = Recipe(
+        models.Invite,
+        role="COLLABORATOR",
+        email=invited.email,
+    ).make()
+
+    with login(client, email="invited@here.tld") as user:
+        url = reverse("invites-invite-accept", args=[invite.pk])
+        response = client.post(url)
+
+    assert response.status_code == 403
+    invite = models.Invite.objects.get(pk=invite.pk)
+    assert invite.accepted_on is None
+    assert user.email not in invite.project.emails
+    assert user not in invite.project.switchtenders.all()
+
+
+@pytest.mark.django_db
+def test_anonymous_accepts_invite_with_existing_account(
+    client,
+):
+    invited = Recipe(
+        auth_models.User, username="invited", email="invited@example.com"
+    ).make()
+
+    invite = Recipe(
+        models.Invite,
+        role="COLLABORATOR",
+        email=invited.email,
+    ).make()
+
+    url = reverse("invites-invite-accept", args=[invite.pk])
+    response = client.post(url)
+
+    assert response.status_code == 403
+    invite = models.Invite.objects.get(pk=invite.pk)
+    assert invite.accepted_on is None
+    assert invite.project.emails == []
+    assert invite.project.switchtenders.count() == 0
+
+
+@pytest.mark.django_db
+def test_anonymous_accepts_invite_as_switchtender(
+    client,
+):
+    invite = Recipe(
+        models.Invite,
+        role="SWITCHTENDER",
+        email="a@new.one",
+    ).make()
+
+    data = {
+        "first_name": "First",
+        "last_name": "Last",
+        "organization": "Some Organization",
+        "position": "Doing Stuff",
+    }
+
+    url = reverse("invites-invite-accept", args=[invite.pk])
+    response = client.post(url, data=data)
+
+    assert response.status_code == 302
+    invite = models.Invite.objects.get(pk=invite.pk)
+    assert invite.accepted_on is not None
+    assert invite.project.emails == []
+    assert invite.project.switchtenders.count() == 1
+
+    user = auth_models.User.objects.get(email=invite.email)
+    assert user.username == invite.email
+    assert user.first_name == data["first_name"]
+    assert user.last_name == data["last_name"]
+    assert user.profile.organization.name == data["organization"]
+    assert user.profile.organization_position == data["position"]
--- urbanvitaliz/apps/invites/views.py
+++ urbanvitaliz/apps/invites/views.py
@@ -1,25 +1,78 @@
+from django.contrib.auth import login
 from django.contrib.auth import models as auth_models
+from django.http import HttpResponseForbidden
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.utils import timezone
+from urbanvitaliz.apps.addressbook import models as addressbook_models
 
 from . import forms, models
 
 
 def invite_accept(request, invite_id):
     invite = get_object_or_404(models.Invite, pk=invite_id, accepted_on=None)
+    project = invite.project
+
+    # Check if this email already exists as an account
+    existing_account = None
+    try:
+        existing_account = auth_models.User.objects.get(email=invite.email)
+    except auth_models.User.DoesNotExist:
+        pass
 
     if request.method == "POST":
-        if invite.role == "SWITCHTENDER":
-            if existing_account not in project.switchtenders:
-                project.switchtenders.add(existing_account)
+        form = forms.InviteAcceptForm(request.POST)
+        user = None
 
-                invite.accepted_on = timezone.now()
-                invite.save()
+        if existing_account:
+            if existing_account == request.user:
+                user = request.user
+            else:
+                return HttpResponseForbidden()
 
-        return redirect(project.get_absolute_url())
+        # New account
+        else:
+            # we shouldn't be logged in at this point
+            if request.user.is_authenticated:
+                return HttpResponseForbidden()
 
-    return redirect(reverse("invites-invite-details", args=(invite.pk)))
+            if form.is_valid():
+                user = auth_models.User.objects.create(
+                    username=invite.email,
+                    email=invite.email,
+                    first_name=form.cleaned_data.get("first_name"),
+                    last_name=form.cleaned_data.get("last_name"),
+                )
+
+                organization, _ = addressbook_models.Organization.objects.get_or_create(
+                    name=form.cleaned_data.get("organization")
+                )
+
+                user.profile.organization = organization
+                user.profile.organization_position = form.cleaned_data.get("position")
+
+                user.profile.save()
+
+                login(
+                    request, user, backend="django.contrib.auth.backends.ModelBackend"
+                )
+
+        if user:
+            # Now, grant the user her new rights
+            if invite.role == "SWITCHTENDER":
+                if user not in project.switchtenders.all():
+                    project.switchtenders.add(user)
+            else:
+                if user.email not in project.emails:
+                    project.emails.append(user.email)
+
+            invite.accepted_on = timezone.now()
+            invite.save()
+            project.save()
+
+            return redirect(project.get_absolute_url())
+
+    return redirect(reverse("invites-invite-details", args=(invite.pk,)))
 
 
 def invite_details(request, invite_id):
@@ -36,7 +89,7 @@ def invite_details(request, invite_id):
 
     project = invite.project
 
-    # Are we currently logged in and usurping a link?
+    # Are we currently logged in and usurpating a link?
     if request.user.is_authenticated:
         if not existing_account or (existing_account != request.user):
             error_msg = "Désolé, cette invitation ne semble pas vous concerner."
@@ -46,7 +99,7 @@ def invite_details(request, invite_id):
             return redirect(
                 reverse("magicauth-login")
                 + "?next="
-                + reverse("invites-invite-details", args=(invite.pk))
+                + reverse("invites-invite-details", args=(invite.pk,))
             )
 
     form = forms.InviteAcceptForm(request.GET or None)
