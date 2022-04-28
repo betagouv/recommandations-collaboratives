f3753cc4d345d8554cde1f8f64310fdabaeeb083
Guillaume Libersat <glibersat@sigill.org>
2022-04-28 10:02:48 +0200

    Base  - Add ordered_models
    Projects
     - Add order to Tasks

--- urbanvitaliz/apps/projects/models.py
+++ urbanvitaliz/apps/projects/models.py
@@ -17,6 +17,8 @@ from django.urls import reverse
 from django.utils import timezone
 from markdownx.utils import markdownify
 from notifications import models as notifications_models
+from ordered_model.models import (OrderedModel, OrderedModelManager,
+                                  OrderedModelQuerySet)
 from tagging.fields import TagField
 from tagging.models import TaggedItem
 from tagging.registry import register as tagging_register
@@ -292,13 +294,16 @@ class Note(models.Model):
         return cls.objects.filter(deleted=None)
 
 
-class TaskManager(models.Manager):
+class TaskQuerySet(OrderedModelQuerySet):
+    pass
+
+
+class TaskManager(OrderedModelManager):
     """Manager for active tasks"""
 
     def get_queryset(self):
         return (
-            super()
-            .get_queryset()
+            TaskQuerySet(self.model, using=self._db)
             .order_by("-priority", "-updated_on")
             .filter(deleted=None)
         )
@@ -342,11 +347,13 @@ class DeletedTaskManager(models.Manager):
         return super().get_queryset().exclude(deleted=None)
 
 
-class Task(models.Model):
+class Task(OrderedModel):
     """Représente une action pour faire avancer un project"""
 
     objects = TaskManager()
     deleted_objects = DeletedTaskManager()
+
+    order_with_respect_to = "project"
 
     PROPOSED = 0
     INPROGRESS = 1
