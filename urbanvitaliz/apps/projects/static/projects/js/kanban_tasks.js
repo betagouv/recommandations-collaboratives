ae18aa582ed8300d6072beb8327fd1185e0b6829
R <kalfane.rahman@gmail.com>
2022-05-04 13:03:51 +0000

    Project : Fix gravatar urls, now uses proper initials

--- urbanvitaliz/apps/projects/static/projects/js/kanban_tasks.js
+++ urbanvitaliz/apps/projects/static/projects/js/kanban_tasks.js
@@ -179,3 +179,11 @@ function reminderTooltip(task) {
     return "Aucun rappel prévu"
   }
 }
+
+function generateGravatarUrl(person, size=50) {
+  const hash = md5(person.email);
+  let name = `${person.first_name}+${person.last_name}`;
+  if (name.trim() === "+") name = "Inconnu";
+  const  encoded_fallback_uri = encodeURIComponent(`https://ui-avatars.com/api/${name}/${size}`);
+  return `https://www.gravatar.com/avatar/${hash}?s=${size}&d=${encoded_fallback_uri}`
+}
