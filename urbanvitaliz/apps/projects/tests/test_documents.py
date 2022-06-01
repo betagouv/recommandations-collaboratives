ab38761cd67f7319c7b3f40d11873b59d5c3fbdc
Guillaume Libersat <glibersat@sigill.org>
2022-06-01 10:19:27 +0200

    make nice

--- urbanvitaliz/apps/projects/tests/test_documents.py
+++ urbanvitaliz/apps/projects/tests/test_documents.py
@@ -9,12 +9,10 @@ created: 2022-05-31 10:11:56 CEST
 
 
 import pytest
-from actstream.models import action_object_stream
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
 from model_bakery import baker
 from model_bakery.recipe import Recipe
-from pytest_django.asserts import assertContains
 from urbanvitaliz.utils import login
 
 from .. import models
