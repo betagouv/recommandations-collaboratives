e4338d62c8d100e0cb492e7ebd32369d6feaad63
Raphael Marvie <raphael.marvie@beta.gouv.fr>
2022-06-01 14:58:25 +0200

    Ajout des commentaires sur les recos dans les stats

--- drafts/stats-uv.sql
+++ drafts/stats-uv.sql
@@ -1,19 +1,48 @@
 --
 -- stats of project activity
-copy (
-SELECT
-    id,
-    name,
-    sum(reco) AS reco,
-    sum(notes) AS notes,
-    sum(rappels) AS rappels
-FROM ((
+--
+-- add creation date of project
+-- add creation date of first task
+-- count followup from the project members
+--
+COPY (
+    SELECT
+        id,
+        name AS project,
+        created AS creation,
+        max(FIRST) AS premiere_reco,
+        sum(reco) AS reco,
+        sum(notes) AS notes,
+        sum(rappels) AS rappels,
+        sum(commentaires) AS commentaires
+    FROM ((
+            SELECT
+                p.id AS id,
+                p.name AS name,
+                p.created_on AS created,
+                min(t.created_on) AS first,
+                0 AS reco,
+                0 AS notes,
+                0 AS rappels,
+                0 AS commentaires
+            FROM
+                projects_project AS p
+            LEFT JOIN projects_task AS t ON p.id = t.project_id
+        WHERE
+            p.exclude_stats = 'f'
+        GROUP BY
+            p.id,
+            p.name)
+    UNION (
         SELECT
             p.id AS id,
             p.name AS name,
+            p.created_on AS created,
+            min(t.created_on) AS first,
             count(t.id) AS reco,
             0 AS notes,
-            0 AS rappels
+            0 AS rappels,
+            0 AS commentaires
         FROM
             projects_project AS p
         LEFT JOIN projects_task AS t ON p.id = t.project_id
@@ -27,9 +56,12 @@ UNION (
     SELECT
         p.id AS id,
         p.name AS name,
+        p.created_on AS created,
+        NULL AS first,
         0 AS reco,
         count(n.id) AS notes,
-        0 AS rappels
+        0 AS rappels,
+        0 AS commentaires
     FROM
         projects_project AS p
     LEFT JOIN projects_note AS n ON p.id = n.project_id
@@ -50,24 +82,59 @@ UNION (
     SELECT
         p.id AS id,
         p.name AS name,
+        p.created_on AS created,
+        NULL AS first,
+        0 AS reco,
+        count(n.id) AS notes,
+        0 AS rappels,
+        0 AS commentaires
+    FROM
+        projects_project AS p
+    LEFT JOIN projects_note AS n ON p.id = n.project_id
+        AND n.public = 't'
+        AND n.created_by_id NOT IN (
+            SELECT
+                s.user_id
+            FROM
+                projects_project_switchtenders AS s
+        WHERE
+            s.id = p.id)
+    WHERE
+        p.exclude_stats = 'f'
+    GROUP BY
+        p.id,
+        p.name)
+UNION (
+    SELECT
+        p.id AS id,
+        p.name AS name,
+        p.created_on AS created,
+        NULL AS first,
         0 AS reco,
         0 AS notes,
-        count(r.id) AS rappels
+        0 AS rappels,
+        count(f.id) AS commentaires
     FROM
         projects_project AS p
     RIGHT JOIN projects_task AS t ON p.id = t.project_id
-    RIGHT JOIN reminders_mail AS r ON t.id = r.object_id
+    RIGHT JOIN projects_taskfollowup AS f ON t.id = f.task_id
 WHERE
     p.exclude_stats = 'f'
-    AND r.origin = 2
-    AND r.content_type_id = 10
+    AND f.who_id NOT IN ( -- who pas un switchtender / prendre dans les acteurs
+        SELECT
+            s.user_id
+        FROM projects_project_switchtenders AS s
+    WHERE
+        s.id = p.id)
 GROUP BY
     p.id,
     p.name)) AS stats
 GROUP BY
     id,
-    name
+    name,
+    creation
 ORDER BY
-    id
-) to '/tmp/stats.csv' delimiter ',' csv header ;
+    id)
+TO '/tmp/stats.csv' DELIMITER ',' csv header;
+
 -- eof
