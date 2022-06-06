0a1239953f36610a69b38f94103dd7ec9afdee9b
Guillaume Libersat <glibersat@sigill.org>
2022-06-06 17:11:22 +0200

    Onboarding is now enforcing a common tree form and allows one to add extra fields

--- urbanvitaliz/apps/onboarding/admin.py
+++ urbanvitaliz/apps/onboarding/admin.py
@@ -6,8 +6,3 @@ from . import models
 @admin.register(models.Onboarding)
 class OnboardingAdmin(admin.ModelAdmin):
     pass
-
-
-@admin.register(models.OnboardingResponse)
-class OnboardingResponseAdmin(admin.ModelAdmin):
-    pass
