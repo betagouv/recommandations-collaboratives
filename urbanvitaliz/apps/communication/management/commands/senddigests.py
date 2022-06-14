c15759caa23b020fb1cd6a460856c56312890386
Guillaume Libersat <glibersat@sigill.org>
2022-06-14 12:24:39 +0200

    reminders are now sent in digest, per project

--- urbanvitaliz/apps/communication/management/commands/senddigests.py
+++ urbanvitaliz/apps/communication/management/commands/senddigests.py
@@ -21,7 +21,14 @@ class Command(BaseCommand):
         self.send_email_digests()
 
     def send_email_digests(self):
+        print("** Sending Reminders **")
+        # Send reminders
+        for user in auth_models.User.objects.filter(is_active=True):
+            if digests.send_digests_for_task_reminders_by_user(user):
+                print(f"Sent reminder digests for {user}")
+
         # Send project collaborators new recommendations
+        print("** Sending new recommendations digests **")
         for project in project_models.Project.objects.all():
             for user in project.members.all():
                 if digests.send_digests_for_new_recommendations_by_user(user):
@@ -31,17 +38,19 @@ class Command(BaseCommand):
         sw_group = auth_models.Group.objects.get(name="switchtender")
 
         # Digests for non switchtenders
+        print("** Sending general digests **")
         for user in auth_models.User.objects.exclude(groups__in=[sw_group]):
             if digests.send_digest_for_non_switchtender_by_user(user):
                 print(f"Sent general digest for {user})")
 
         # Digests for switchtenders
+        print("** Sending general switchtender digests **")
         for user in auth_models.User.objects.filter(groups__in=[sw_group]):
             if digests.send_digests_for_new_sites_by_user(user):
-                print(f"Sent new site digests for {user}")
+                print(f"* Sent new site digests for {user}")
 
             if digests.send_digest_for_switchtender_by_user(user):
-                print(f"Sent general digest for switchtender (to {user})")
+                print(f"* Sent general digest for switchtender (to {user})")
 
 
 # eof
