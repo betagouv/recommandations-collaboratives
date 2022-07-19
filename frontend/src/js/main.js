bf6f6d316bc857d5de0a1295e9340357519f3141
qdanneville <quentin@esperluette.io>
2022-07-19 13:35:07 +0000

    [Refacto] Alpine removed from CDN - Added as a global module

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,1 +1,4 @@
+import 'vite/modulepreload-polyfill';
+import './utils/globals'
+
 console.log('js main added');
---
+++ frontend/src/js/utils/globals.js
@@ -0,0 +1,9 @@
+import Alpine from "alpinejs"
+
+document.addEventListener("DOMContentLoaded", function () {
+    window.Alpine = Alpine
+
+    Alpine.start()
+});
+
+export default Alpine
