cecf2695250c304b2484a93076aa0d865eb8c932
Guillaume Libersat <glibersat@sigill.org>
2022-07-20 18:10:05 +0200

    crm: timeline for user and projects

--- urbanvitaliz/apps/crm/views.py
+++ urbanvitaliz/apps/crm/views.py
@@ -5,7 +5,7 @@ author  : raphael.marvie@beta.gouv.fr,guillaume.libersat@beta.gouv.fr
 created : 2022-07-20 12:27:25 CEST
 """
 
-from actstream.models import Action
+from actstream.models import Action, actor_stream, target_stream
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
@@ -46,11 +46,15 @@ def organization_details(request, organization_id):
 def user_details(request, user_id):
     crm_user = get_object_or_404(User, pk=user_id)
 
+    actions = actor_stream(crm_user)
+
     return render(request, "crm/user_details.html", locals())
 
 
 @staff_member_required
 def project_details(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
+
+    actions = target_stream(project)
 
     return render(request, "crm/project_details.html", locals())
