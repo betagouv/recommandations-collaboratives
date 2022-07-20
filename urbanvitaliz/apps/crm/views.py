eb7f0007bd369865c1c831bff40ca245409977ec
Guillaume Libersat <glibersat@sigill.org>
2022-07-20 17:40:12 +0200

    crm: timeline for orgs

--- urbanvitaliz/apps/crm/views.py
+++ urbanvitaliz/apps/crm/views.py
@@ -5,8 +5,10 @@ author  : raphael.marvie@beta.gouv.fr,guillaume.libersat@beta.gouv.fr
 created : 2022-07-20 12:27:25 CEST
 """
 
+from actstream.models import Action
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import get_object_or_404, render
 from urbanvitaliz.apps.addressbook.models import Organization
 from urbanvitaliz.apps.projects.models import Project
@@ -27,6 +29,15 @@ def organization_details(request, organization_id):
     unadvised_projects = Project.objects.filter(
         commune__department__in=org_departments
     ).exclude(switchtenders__in=participants)
+
+    participant_ids = list(participants.values_list("id", flat=True))
+
+    user_ct = ContentType.objects.get_for_model(User)
+
+    actions = Action.objects.filter(
+        actor_content_type=user_ct,
+        actor_object_id__in=participant_ids,
+    )
 
     return render(request, "crm/organization_details.html", locals())
 
