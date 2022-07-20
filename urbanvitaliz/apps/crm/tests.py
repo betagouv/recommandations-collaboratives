e5f330b96127b964afa712e4210c82e71d2a46bc
Guillaume Libersat <glibersat@sigill.org>
2022-07-20 16:20:23 +0200

    crm: ensure only staff can access

--- urbanvitaliz/apps/crm/tests.py
+++ urbanvitaliz/apps/crm/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from model_bakery.recipe import Recipe
+from urbanvitaliz.utils import login
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_crm_organization_not_available_for_non_staff_logged_users(client):
+    url = reverse("crm-organization-details", args=[1])
+    with login(client):
+        response = client.get(url)
+
+    assert response.status_code == 302
