d4c54e317f0922cbf65944e1b960404208c3d07c
qdanneville <quentin@esperluette.io>
2022-07-26 09:09:14 +0000

    [CRM] Sidebar scroll behavior handled  - Relative if sidebar height > window height

--- urbanvitaliz/frontend/src/js/components/Crm.js
+++ urbanvitaliz/frontend/src/js/components/Crm.js
@@ -1,1 +1,19 @@
-console.log('CRM ready');
+import Alpine from "alpinejs";
+
+Alpine.data("Crm", Crm)
+
+function Crm() {
+    return {
+        init() {
+            const sidebar = this.$refs.sidebar;
+            const sidebarHeight = sidebar.offsetHeight;
+            const windowHeight = window.innerHeight;
+
+            if (sidebarHeight > windowHeight) {
+                sidebar.classList.remove('crm-sticky')
+                sidebar.classList.add('crm-relative')
+            }
+
+        }
+    }
+}
