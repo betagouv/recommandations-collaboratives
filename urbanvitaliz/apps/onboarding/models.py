11cad15a0e2102b36594d98e9e0d412b60769dea
Guillaume Libersat <glibersat@sigill.org>
2022-08-09 11:14:42 +0200

    OnboardingRespone: Link to project and add to admin

--- urbanvitaliz/apps/onboarding/models.py
+++ urbanvitaliz/apps/onboarding/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from dynamic_forms.models import FormField, ResponseField
+from urbanvitaliz.apps.projects import models as projects_models
 
 
 class Onboarding(models.Model):
@@ -10,4 +11,8 @@ class OnboardingResponse(models.Model):
     onboarding = models.ForeignKey(
         Onboarding, on_delete=models.CASCADE, related_name="responses"
     )
+    project = models.OneToOneField(
+        projects_models.Project, on_delete=models.CASCADE, related_name="onboarding"
+    )
+
     response = ResponseField()
