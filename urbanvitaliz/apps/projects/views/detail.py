34968cb79bc7e2e77bb0340730fc19ef0ad244ab
qdanneville <quentin@esperluette.io>
2022-08-10 09:27:59 +0000

    [Projects] Details templates changed WIP

--- urbanvitaliz/apps/projects/views/detail.py
+++ urbanvitaliz/apps/projects/views/detail.py
@@ -25,9 +25,54 @@ from ..utils import (can_administrate_or_403, can_administrate_project,
 
 @login_required
 def project_detail(request, project_id=None):
-    """Set as active project, then redirect to knowledge page"""
-    return redirect(reverse("projects-project-detail-knowledge", args=[project_id]))
+    """Set as active project, then redirect to overview page"""
+    return redirect(reverse("projects-project-detail-overview", args=[project_id]))
 
+@login_required
+def project_overview(request, project_id=None):
+    """Return the details of given project for switchtender"""
+    project = get_object_or_404(models.Project, sites=request.site, pk=project_id)
+
+    # compute permissions
+    can_manage = can_manage_project(project, request.user)
+    can_manage_draft = can_manage_project(project, request.user, allow_draft=True)
+    is_national_actor = check_if_national_actor(request.user)
+    is_regional_actor = is_regional_actor_for_project(
+        project, request.user, allow_national=True
+    )
+    can_administrate = can_administrate_project(project, request.user)
+
+    # check user can administrate project (member or switchtender)
+    if request.user != project.members.filter(projectmember__is_owner=True).first():
+        # bypass if user is switchtender, all are allowed to view at least
+        if not check_if_switchtender(request.user):
+            can_manage_or_403(project, request.user)
+
+    # Set this project as active
+    set_active_project_id(request, project.pk)
+
+    site_config = get_site_config_or_503(request.site)
+    session, created = survey_models.Session.objects.get_or_create(
+        project=project, survey=site_config.project_survey
+    )
+
+    # Mark some notifications as seen (general ones)
+    project_ct = ContentType.objects.get_for_model(project)
+    general_notifications = request.user.notifications.unread().filter(
+        Q(verb="est devenu·e conseiller·e sur le projet")
+        | Q(verb="a été validé")
+        | Q(verb="a validé le projet")
+        | Q(verb="a soumis pour modération le projet")
+        | Q(verb="a mis à jour le questionnaire")
+        | Q(verb="a ajouté un document")
+        | Q(verb="a envoyé un message"),
+        target_content_type=project_ct.pk,
+        target_object_id=project.pk,
+    )
+
+    general_notifications.mark_all_as_read()
+
+    return render(request, "projects/project/overview.html", locals())
 
 @login_required
 def project_knowledge(request, project_id=None):
