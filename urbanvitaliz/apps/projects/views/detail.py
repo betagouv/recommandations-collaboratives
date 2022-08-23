aa45ed137cd7b0172f3aafd49a6d65082144e271
Guillaume Libersat <glibersat@sigill.org>
2022-08-23 15:34:07 +0200

    add test for synopsis and move json logic from knowledge to overview page

--- urbanvitaliz/apps/projects/views/detail.py
+++ urbanvitaliz/apps/projects/views/detail.py
@@ -17,16 +17,11 @@ from urbanvitaliz.utils import check_if_switchtender, get_site_config_or_503
 
 from .. import models
 from ..forms import PrivateNoteForm, PublicNoteForm, SynopsisForm
-from ..utils import (
-    can_administrate_or_403,
-    can_administrate_project,
-    can_manage_or_403,
-    can_manage_project,
-    check_if_national_actor,
-    get_notification_recipients_for_project,
-    is_regional_actor_for_project,
-    set_active_project_id,
-)
+from ..utils import (can_administrate_or_403, can_administrate_project,
+                     can_manage_or_403, can_manage_project,
+                     check_if_national_actor,
+                     get_notification_recipients_for_project,
+                     is_regional_actor_for_project, set_active_project_id)
 
 
 @login_required
@@ -48,6 +43,11 @@ def project_overview(request, project_id=None):
         project, request.user, allow_national=True
     )
     can_administrate = can_administrate_project(project, request.user)
+
+    if project.onboarding:
+        onboarding_response = dict(project.onboarding.response)
+    else:
+        onboarding_response = None
 
     # check user can administrate project (member or switchtender)
     if request.user != project.members.filter(projectmember__is_owner=True).first():
@@ -100,11 +100,6 @@ def project_knowledge(request, project_id=None):
 
     # Set this project as active
     set_active_project_id(request, project.pk)
-
-    if project.onboarding:
-        onboarding_response = dict(project.onboarding.response)
-    else:
-        onboarding_response = None
 
     site_config = get_site_config_or_503(request.site)
     session, created = survey_models.Session.objects.get_or_create(
