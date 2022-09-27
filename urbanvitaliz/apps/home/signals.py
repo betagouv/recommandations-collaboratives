89c5a832d624f9d94715d6ac56e2408eea9ed011
Guillaume Libersat <glibersat@sigill.org>
2022-09-27 19:17:32 +0200

    delete login activity when identity is hijacked

--- urbanvitaliz/apps/home/signals.py
+++ urbanvitaliz/apps/home/signals.py
@@ -1,9 +1,16 @@
 from actstream import action
+from actstream.models import actor_stream
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
+from hijack.signals import hijack_started
 
 
 @receiver(user_logged_in)
 def post_login(sender, user, request, **kwargs):
     if not user.is_staff:
         action.send(user, verb="s'est connecté")
+
+
+@receiver(hijack_started)
+def delete_login_trace(sender, hijacker, hijacked, request, **kwargs):
+    actor_stream(hijacked).last().delete()
