c266f4244afb2d0d701de1362c977d9ee96738e1
Guillaume Libersat <glibersat@sigill.org>
2022-11-03 17:32:44 +0100

    [management] add stale notification supression

--- urbanvitaliz/apps/home/management/commands/clean_stale_notifications.py
+++ urbanvitaliz/apps/home/management/commands/clean_stale_notifications.py
@@ -10,6 +10,7 @@ created: 2022-09-26 18:57:48 CET
 from actstream.models import Action
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
+from notifications.models import Notification
 from urbanvitaliz.apps.projects.models import Project
 
 
@@ -19,7 +20,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         actions = Action.objects.all()
 
-        # clean actions for hard deleted objects
+        # clean actions for deleted objects
         for action in actions.all():
             try:
                 project_ct = ContentType.objects.get_for_id(
@@ -33,19 +34,21 @@ class Command(BaseCommand):
             try:
                 klass.objects.get(pk=action.target_object_id)
             except klass.DoesNotExist:
-                print(f"[Deleting stale action for hard deleted] {action}")
+                print(f"[Deleting stale action] {action}")
                 action.delete()
 
-        # clean actions for soft deleted projects
+        # clean notifications for deleted projects
         deleted_projects = Project.objects_deleted.all()
         project_ct = ContentType.objects.get_for_model(Project)
 
-        to_be_deleted = actions.filter(
+        to_be_deleted = Notification.objects.filter(
             target_content_type=project_ct,
             target_object_id__in=list(deleted_projects.values_list(flat=True)),
         )
 
-        print(f"[Deleting stale action for soft deleted] {to_be_deleted.count()}")
+        for notification in to_be_deleted:
+            print(f"[Deleting stale notification] {notification}")
+
         to_be_deleted.delete()
 
 
