43fb4039653b26f40e1ec51c6584787689c3c88b
qdanneville <quentin@esperluette.io>
2022-11-15 16:16:56 +0000

    [Cypress][Resource] tests added and up to date

---
+++ frontend_tests/cypress/e2e/resource/canAskQuestion.cy.js
@@ -0,0 +1,30 @@
+describe('I can ask a question on a resource', () => {
+    beforeEach(() => {
+        cy.login("staff");
+    })
+
+    it('asks a question', () => {
+        cy.visit('/ressource/3/')
+
+        cy.contains("Poser une question").click({ force: true })
+        cy.url().should('include', '/contact/')
+        cy.contains(" Contacter l'équipe UrbanVitaliz")
+
+        cy.get('#input-project-content')
+            .type('Question sur la resource numéro 3', { force: true })
+            .should('have.value', 'Question sur la resource numéro 3')
+
+        cy.document().then((doc) => {
+            var iframe = doc.getElementById('id_captcha').querySelector('iframe');
+            var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+            innerDoc.querySelector('.recaptcha-checkbox').click()
+        })
+
+        cy.wait(500)
+
+        cy.contains('Envoyer mon message').click({ force: true });
+        cy.url().should('include', '/ressource/3/')
+        cy.contains("Merci, votre demande a été transmis à l'équipe Urbanvitaliz !")
+
+    })
+})
---
+++ frontend_tests/cypress/e2e/resource/pushAResource.cy.js
@@ -0,0 +1,19 @@
+describe('I can push a resource to a project', () => {
+    beforeEach(() => {
+        cy.login("staff");
+    })
+
+    it('pushes a resource', () => {
+        cy.visit('/projects')
+        cy.contains("Friche numéro 1").click({force:true})
+        cy.visit('/ressource/2/')
+        cy.contains("Pousser la ressource").click({force:true})
+        cy.get('[name="content"]')
+            .type(`contenu`, { force: true })
+            .should('have.value', `contenu`)
+
+        cy.get("[type=submit]").click({ force: true });
+
+        cy.url().should('include', '/actions#action-')
+    })
+})
