6b2365fd8fc6ed24650ed071e07e47c1a8802c3a
qdanneville <quentin@esperluette.io>
2023-01-10 16:48:52 +0000

    [Project] Task : reminder color fixed if old reminder

--- urbanvitaliz/apps/projects/static/projects/js/tmp_kanban_globals.js
+++ urbanvitaliz/apps/projects/static/projects/js/tmp_kanban_globals.js
@@ -99,18 +99,26 @@ function reminderTooltip(task) {
     if (task.reminders.length > 0) {
         const reminder = task.reminders[0];
 
-        const now = new Date().getTime();
-        const reminderDate = new Date(reminder.deadline).getTime();
-
-        if (reminderDate < now) {
-            return "Aucun rappel prévu"    
-        }
+        if (isOldReminder(reminder)) return "Aucun rappel prévu"
 
         return `Rappel pour ${reminder.recipient} prévu le ${reminder.deadline}`
     } else {
         return "Aucun rappel prévu"
     }
 }
+
+function isOldReminder(reminder) {
+
+    console.log('reminder : ', reminder)
+
+    if (!reminder) return false
+
+    const now = new Date().getTime();
+    const reminderDate = new Date(reminder.deadline).getTime();
+
+    return (reminderDate < now)
+}
+
 
 function toArchiveTooltip() {
     return 'Archiver cette action'
