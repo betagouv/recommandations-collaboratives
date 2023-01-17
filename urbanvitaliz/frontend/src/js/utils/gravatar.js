4fbca68fbde853ca582a53a3ca16eb3a88128aad
qdanneville <quentin@esperluette.io>
2023-01-17 16:54:40 +0000

    [Projects] Gravatar -> md5 is not required

--- urbanvitaliz/frontend/src/js/utils/gravatar.js
+++ urbanvitaliz/frontend/src/js/utils/gravatar.js
@@ -1,5 +1,5 @@
 export function generateGravatarUrl(person, size = 50) {
-    const hash = md5(person.email);
+    const hash = person.email;
     let name = `${person.first_name}+${person.last_name}`;
     if (name.trim() === "+") name = "Inconnu";
     const encoded_fallback_uri = encodeURIComponent(`https://ui-avatars.com/api/${name}/${size}`);
