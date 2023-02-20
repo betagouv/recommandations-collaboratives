62b2720b73283c6e0f023633a8bce5e32e37eb58
qdanneville <quentin@esperluette.io>
2023-02-20 17:14:00 +0000

    [Cypress][Widget][SearchableList] Tests #WIP

---
+++ frontend_tests/cypress/e2e/tools/searchableList/canUpdateContactOfAResource.cy.js
@@ -0,0 +1,39 @@
+describe('I can assign new contacts when I edit a resource', () => {
+    beforeEach(() => {
+        cy.login("staff");
+    })
+
+    it('goes to edit a resource and assign 3 new contacts', () => {
+        cy.visit('/ressource/create/')
+
+        cy.get('#id_title')
+            .type('Ressource de test', { force: true })
+            .should('have.value', 'Ressource de test')
+
+        cy.get('#id_subtitle')
+            .type('Soustitre de la ressource de test', { force: true })
+            .should('have.value', 'Soustitre de la ressource de test')
+
+        cy.get('#id_summary')
+            .type('résumé de la ressource de test', { force: true })
+            .should('have.value', 'résumé de la ressource de test')
+
+        cy.get('#id_tags')
+            .type('etiquette1', { force: true })
+            .should('have.value', 'etiquette1')
+
+        // cy.get('#id_departments')
+        //     .select(1, {force:true})
+
+        cy.get('#id_expires_on')
+            .type('20/12/2022', { force: true })
+            .should('have.value', '20/12/2022')
+
+        cy.get('[type="submit"]').click({ force: true });
+
+        cy.url().should('include', '/ressource/')
+
+        cy.contains("Ressource de test")
+        cy.contains("résumé de la ressource de test")
+    })
+})
