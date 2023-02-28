95cb5fb776584eb66dbc827d583a128a98bbcca1
Guillaume Libersat <glibersat@sigill.org>
2023-02-28 17:02:05 +0100

    Fix migration that tried to assign permissions (not possible)

--- urbanvitaliz/apps/home/management/commands/update_permissions.py
+++ urbanvitaliz/apps/home/management/commands/update_permissions.py
@@ -13,11 +13,36 @@ from django.contrib.sites.models import Site
 
 from urbanvitaliz.utils import get_group_for_site
 from urbanvitaliz.apps.home.models import SITE_GROUP_PERMISSIONS
+from urbanvitaliz.apps.projects.models import Project, ProjectSwitchtender
+from urbanvitaliz.apps.projects.utils import (
+    assign_collaborator,
+    assign_observer,
+    assign_advisor,
+)
 from guardian.shortcuts import assign_perm
 
 
-def do_update_permissions():
-    # Per site permissions for Group
+def assign_user_permissions_by_projects():
+    """Per project permission for user"""
+    for project in Project.objects.all():
+        for membership in project.projectmember_set.all():
+            assign_collaborator(
+                membership.member, project, is_owner=membership.is_owner
+            )
+
+        for project_advisor in ProjectSwitchtender.objects.filter(project=project):
+            if project_advisor.is_observer:
+                assign_observer(
+                    project_advisor.switchtender, project, site=project_advisor.site
+                )
+            else:
+                assign_advisor(
+                    project_advisor.switchtender, project, site=project_advisor.site
+                )
+
+
+def assign_group_permissions_by_sites():
+    """Per site permissions for Group"""
     for site in Site.objects.all():
         print("site:", site)
         for group_name, permissions in SITE_GROUP_PERMISSIONS.items():
@@ -31,4 +56,8 @@ class Command(BaseCommand):
     help = "Update permissions for User and Groups"
 
     def handle(self, *args, **options):
-        do_update_permissions()
+        assign_group_permissions_by_sites()
+        assign_user_permissions_by_projects()
+
+
+# eof
