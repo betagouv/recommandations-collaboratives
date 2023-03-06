f0eb93c84180d91c6b8699afcb62cf67f00addfe
qdanneville <quentin@esperluette.io>
2023-03-06 16:08:01 +0000

    [Project][Navigation] Advisor tests added

--- frontend_tests/cypress/e2e/project/navigation/canAccessOverview.cy.js
+++ frontend_tests/cypress/e2e/project/navigation/canAccessOverview.cy.js
@@ -16,3 +16,19 @@ describe('I can access overview tab in a project as a member', () => {
         cy.url().should('include', '/presentation')
     })
 })
+
+describe('I can access overview tab in a project as an advisor', () => {
+
+    beforeEach(() => {
+        cy.login("jean");
+    })
+
+    it('goes to knowledge page and then overview page of my project', () => {
+
+        cy.visit(`/project/${currentProject.pk}`)
+        cy.get('.project-navigation').children('li').contains('État des lieux').click({force:true})
+        cy.url().should('include', '/connaissance')
+        cy.contains('Présentation').click({force:true})
+        cy.url().should('include', '/presentation')
+    })
+})
