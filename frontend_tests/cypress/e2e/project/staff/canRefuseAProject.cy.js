40f9b16f900f9bfc445d38c2eb27e77369aad7cc
qdanneville <quentin@esperluette.io>
2023-03-20 13:41:12 +0000

    [Cypress][Staff] Approve & decline project test updated

--- frontend_tests/cypress/e2e/project/staff/canRefuseAProject.cy.js
+++ frontend_tests/cypress/e2e/project/staff/canRefuseAProject.cy.js
@@ -6,11 +6,13 @@ describe('I can go to the dashboard and see the pending projects, and refuse one
     it('refuses a project', () => {
 
         cy.visit('/projects')
+        cy.contains("Revenir sur l'ancien tableau de bord").click({ force: true })
 
         cy.contains("Projets en attente d'acceptation")
         cy.contains("Friche à refuser")
         cy.get("#draft-projects").siblings().contains('Friche à refuser').parents('tr').contains('Supprimer').click({ force: true })
         cy.url().should('include', '/projects/')
+        cy.contains("Revenir sur l'ancien tableau de bord").click({ force: true })
         cy.get("#draft-projects").siblings().contains('Friche à refuser').should('not.exist')
     })
 })
