73b50bf9f859e00df7d89c4253fcba9d4faafbdb
Raphael Marvie <raphael.marvie@beta.gouv.fr>
2023-04-05 17:21:33 +0200

    [projects] test for template tag is_staff_for_current_site

--- urbanvitaliz/apps/projects/tests/test_templatetags.py
+++ urbanvitaliz/apps/projects/tests/test_templatetags.py
@@ -9,8 +9,10 @@ created: 2021-06-01 10:11:56 CEST
 
 
 import pytest
+from django.contrib.auth import models as auth
 from django.contrib.sites.shortcuts import get_current_site
-from model_bakery.recipe import Recipe
+from model_bakery import baker
+from urbanvitaliz import utils
 
 from .. import models
 from ..templatetags import projects_extra
@@ -23,6 +25,24 @@ from ..templatetags import projects_extra
 @pytest.mark.django_db
 def test_current_project_tag(request):
     current_site = get_current_site(request)
-    project = Recipe(models.Project, sites=[current_site]).make()
+    project = baker.make(models.Project, sites=[current_site])
     session = {"project_id": project.id}
     assert projects_extra.current_project(session) == project
+
+
+@pytest.mark.django_db
+def test_is_staff_for_current_site_success(request):
+    site = get_current_site(request)
+    group = utils.get_group_for_site("staff", site)
+    user = baker.make(auth.User)
+    user.groups.add(group)
+    assert projects_extra.is_staff_for_current_site(user) is True
+
+
+@pytest.mark.django_db
+def test_is_staff_for_current_site_failure(request):
+    user = baker.make(auth.User)
+    assert projects_extra.is_staff_for_current_site(user) is False
+
+
+# eof
