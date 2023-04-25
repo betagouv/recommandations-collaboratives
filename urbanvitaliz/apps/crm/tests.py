f9218244a604f22b7dc17fc75d38e6dc47f1891c
Guillaume Libersat <glibersat@sigill.org>
2023-04-25 12:06:43 +0200

    [CRM] fix missing related object in test

--- urbanvitaliz/apps/crm/tests.py
+++ urbanvitaliz/apps/crm/tests.py
@@ -207,9 +207,9 @@ def test_site_dashboard_available_for_staff_users(client):
 @pytest.mark.django_db
 def test_compute_tag_cloud():
     site = baker.make(site_models.Site)
-    project = baker.make(models.ProjectAnnotations, site=site)
-    project.tags.add("tag0", "tag1")
-    note = baker.make(models.Note, site=site)
+    project_annotation = baker.make(models.ProjectAnnotations, site=site)
+    project_annotation.tags.add("tag0", "tag1")
+    note = baker.make(models.Note, site=site, related=project_annotation.project)
     note.tags.add("tag0", "tag2")
     tags = views.compute_tag_occurences(site)
     assert tags == collections.OrderedDict(
