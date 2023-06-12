4a60837af8eb580dea905545728c3c0b4ce13ddb
qdanneville <quentin@esperluette.io>
2023-06-12 12:46:36 +0200

    [User] popper added along with style - Missing role & inactif (#WIP)

--- urbanvitaliz/frontend/src/js/components/User.js
+++ urbanvitaliz/frontend/src/js/components/User.js
@@ -1,42 +1,58 @@
 import Alpine from 'alpinejs'
 import { createPopper } from '@popperjs/core';
+import appStore from '../store/app'
 
 Alpine.data("User", User)
 
 function User() {
     return {
         popper: null,
+        isOpening: false,
         init() {
-            console.log('user component ready : ');
+            const popper = createPopper(this.$refs.user, this.$refs.userTooltip, {
+                placement: 'top-start',
+                modifiers: [
+                    {
+                        name: 'offset',
+                        options: {
+                            offset: [0, 10],
+                        },
+                    },
+                ],
+            })
 
-            // const popper = createPopper(this.$refs.user, this.$refs.userTooltip)
-
-            console.log(this.$refs.user);
-
-            // this.$refs.userTooltip.style.display = 'none'
-            // this.popper = popper
+            this.popper = popper
         },
 
         async show() {
-            this.$refs.userTooltip.style.display = 'block'
+            this.$refs.userTooltip.setAttribute('data-show', '');
             await this.popper.update()
         },
-    
+
         hide() {
-            this.$refs.userTooltip.style.display = 'none'
+            this.$refs.userTooltip.removeAttribute('data-show');
         },
-    
+
         async onUserClick() {
             await this.show()
             this.isOpening = true
         },
-    
+
         onOutsideUserClick() {
             if (this.isOpening) {
                 this.isOpening = false
             } else {
                 this.hide()
             }
+        },
+        clipboardCopy(type, text) {
+            navigator.clipboard.writeText(text).then(function () {
+                this.selectText(type);
+            }.bind(this));
+        },
+        selectText: function (text) {
+            appStore.notification.message = `${text} a bien été copiée`
+            appStore.notification.isOpen = true
         },
     }
 }
