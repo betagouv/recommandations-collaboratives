bfc6ae19ed2ec6a350c26cdd812c546be3a39def
Raphael Marvie <raphael.marvie@beta.gouv.fr>
2023-06-13 18:00:52 +0200

    drafts/dumpurls.py Update script to produce the sqlite db containing urls

--- drafts/dumpurls.py
+++ drafts/dumpurls.py
@@ -1,29 +1,43 @@
-import csv
+# encoding: utf-8
+
+"""
+Collect all the urls of the application
+
+Insert them in a sqlite db
+
+authors: raphael@softosapiens.fr
+created: 2023-06-13 17:48:16 CEST
+"""
+
+import sqlite3
 
 from django.urls import resolvers
 
+from urbanvitaliz import urls
 
-def collect_urls(urlpatterns):
+
+def collect_urls(urlpatterns, prefix=""):
     entries = []
     for p in urlpatterns:
         if isinstance(p, resolvers.URLPattern):
-            entry = [p.name, p.pattern.regex.pattern, p.lookup_str]
+            pattern = f"{prefix}{p.pattern.regex.pattern}".replace("^", "")
+            entry = [p.name, pattern, p.lookup_str]
             entries.append(entry)
         if isinstance(p, resolvers.URLResolver):
-            sub_entries = collect_urls(p.url_patterns)
+            sub_prefix = f"{prefix}{p.pattern.regex.pattern}"
+            sub_entries = collect_urls(p.url_patterns, sub_prefix)
             entries.extend(sub_entries)
     return entries
 
 
 def dump_urls(urls):
     rows = collect_urls(urls.urlpatterns)
-    print(len(rows))
-    with open("urls.csv", "w") as f:
-        writer = csv.writer(f)
-        writer.writerows(rows)
+    c = sqlite3.connect("urls.sqlite3")
+    c.execute("create table urls(name,pattern,view)")
+    c.executemany("insert into urls(name,pattern,view) values(?,?,?)", rows)
+    c.commit()
+    c.close()
 
-
-from urbanvitaliz import urls
 
 dump_urls(urls)
 
