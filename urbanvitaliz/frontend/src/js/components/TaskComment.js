bd09f1c7ee1a4150251ef44e40324f9c3d7dbf7c
qdanneville <quentin@esperluette.io>
2023-07-11 14:33:27 +0200

    [Task] Comment and update component added

--- urbanvitaliz/frontend/src/js/components/TaskComment.js
+++ urbanvitaliz/frontend/src/js/components/TaskComment.js
@@ -1,1 +1,21 @@
 //TODO edit a task comment component
+import Alpine from 'alpinejs'
+
+export default function TaskComment() {
+    return {
+        isEditing: false,
+        init() {
+            console.log('task comment init')
+        },
+        async handleEditComment(comment, task) {
+            this.isEditing = true
+            task.isLoading = true
+            await this.$store.tasksData.patchTask(task.id, { content: comment })
+            await this.$store.tasksView.updateViewWithTask(task.id)
+            task.isLoading = false
+            this.isEditing = false
+        }
+    }
+}
+
+Alpine.data("TaskComment", TaskComment)
