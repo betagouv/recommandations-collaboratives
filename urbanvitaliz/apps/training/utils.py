fe58f5e0afe9258fc344ae3dfa57ece177d3db3a
Raphael Marvie <raphael@softosapiens.fr>
2023-08-21 15:06:21 +0200

    [training] Towards repeatable challenges
    - introduces week inactivity repeat for defining repetition of
      challenges (0 means always present challenge)
    - remove unique (challenge definition, user) to allow repetition and
      track all challenges
    - update utils.get_challenge_for to take repetition into account
    - update training template tags to take into account new model

--- urbanvitaliz/apps/training/utils.py
+++ urbanvitaliz/apps/training/utils.py
@@ -1,15 +1,42 @@
-from .models import Challenge, ChallengeDefinition
+# encoding: utf-8
+
+"""
+Utility functions for training
+
+authors: guillaume.libersat@beta.gouv.fr, raphael.marvie@beta.gouv.fr
+created: 2023-08-21 14:28:45 CEST
+"""
+
+
+from django.utils import timezone
+
+from . import models
 
 
 def get_challenge_for(user, codename):
-    """Return a challenge or create it for the given user"""
+    """Return existing open challenge, new one if necessary, or None"""
     try:
-        challenge_definition = ChallengeDefinition.objects.get(code=codename)
-
-    except ChallengeDefinition.DoesNotExist:
+        challenge_definition = models.ChallengeDefinition.objects.get(code=codename)
+    except models.ChallengeDefinition.DoesNotExist:
         return None
 
-    challenge, created = Challenge.objects.get_or_create(
-        user=user, challenge_definition=challenge_definition
+    try:
+        return models.Challenge.objects.get(
+            challenge_definition=challenge_definition, user=user
+        )
+    except models.Challenge.DoesNotExist:
+        pass  # it's ok, let look if a new one is required
+
+    # should we repeat ?
+    inactivity = (timezone.now() - user.last_login).days
+    repetition = challenge_definition.week_inactivity_repeat * 7
+    if inactivity < repetition:
+        return None  # do not repeat for the moment
+
+    # Start a new challenge
+    return models.Challenge.objects.create(
+        challenge_definition=challenge_definition, user=user
     )
-    return challenge
+
+
+# eof
