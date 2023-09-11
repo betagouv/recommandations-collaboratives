760352d0b185e2121c744bd620e821ce2456b714
Guillaume Libersat <glibersat@sigill.org>
2023-09-11 15:10:41 +0200

    [REST] API can now return XML, using ?format=xml Readonly is allowed for anonymous users at resource endpoints Author is now provided on resources

--- urbanvitaliz/apps/resources/serializers.py
+++ urbanvitaliz/apps/resources/serializers.py
@@ -1,7 +1,18 @@
+from django.contrib.auth import models as auth_models
 from rest_framework import serializers
 from taggit.serializers import TaggitSerializer, TagListSerializerField
 
 from .models import Resource
+
+
+class ResourceCreatorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = auth_models.User
+
+        fields = [
+            "first_name",
+            "last_name",
+        ]
 
 
 class ResourceSerializer(TaggitSerializer, serializers.HyperlinkedModelSerializer):
@@ -14,6 +25,7 @@ class ResourceSerializer(TaggitSerializer, serializers.HyperlinkedModelSerialize
             "tags",
             "status",
             "created_on",
+            "created_by",
             "updated_on",
             "web_url",
             "embeded_url",
@@ -22,3 +34,4 @@ class ResourceSerializer(TaggitSerializer, serializers.HyperlinkedModelSerialize
     web_url = serializers.URLField(source="get_absolute_url", read_only=True)
     embeded_url = serializers.URLField(source="get_embeded_url", read_only=True)
     tags = TagListSerializerField()
+    created_by = ResourceCreatorSerializer(read_only=True, many=False)
