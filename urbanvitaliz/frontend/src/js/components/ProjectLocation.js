5f5cf293d7e01e20591e26db3c53e77ec52e88c7
Patricia Boh <patricia.boh@beta.gouv.fr>
2023-09-20 10:46:53 +0200

    [Geoloc] Fix map coordinates and popup

--- urbanvitaliz/frontend/src/js/components/ProjectLocation.js
+++ urbanvitaliz/frontend/src/js/components/ProjectLocation.js
@@ -9,26 +9,34 @@ import 'leaflet-providers'
 import { statusToColorClass } from '../utils/statusToText'
 
 
-function ProjectLocation(latitude, longitude, status) {
+function ProjectLocation(nameProject, status, nameCommune, postal, longitude, latitude) {
 	return {
-		// map
-		map: null,
 		mapIsSmall: true,
 
 		async init() {
-			if(!this.map){
-				this.map = initMap(parseFloat(latitude), parseFloat(longitude));
+			let project = {
+				name: nameProject,
+				status,
+				commune: {
+					name: nameCommune,
+					postal,
+					latitude: latitude ? parseFloat(latitude) : 46.51,
+					longitude: longitude ? parseFloat(longitude) :1.20,
+				}
 			}
-			initMapLayers(this.map, parseFloat(latitude), parseFloat(longitude), status)
+			const	Map = initMap(project.commune.latitude, project.commune.longitude);
+			initMapLayers(Map, project)
 			//Center Map
-			this.map.panTo(new L.LatLng(46.51, 1.20));
+			Map.panTo(new L.LatLng(project.commune.latitude, project.commune.longitude));
 		},
 	}
 }
 
 // Map base layer 
 function initMap(latitude, longitude) {
-	const map = L.map('map').setView([latitude, longitude], 5);
+	const map = L.map('map').setView([latitude, longitude], 7);
+	setTimeout(function(){  map.invalidateSize()}, 0); // forces map redraw to fit container
+
 	L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
 		maxZoom: 20,
 		attribution: '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -36,18 +44,32 @@ function initMap(latitude, longitude) {
 	
 	L.tileLayer.provider('OpenStreetMap.France').addTo(map);
 
-	setTimeout(function(){  map.invalidateSize()}, 0);
 	return map
 }
 
 // Crete layers composed with markers
-function initMapLayers(map, latitude, longitude, status) {
-		let marker = L.marker([latitude, longitude], { icon: createMarkerIcon(status) }).addTo(map)
+function initMapLayers(map, project) {
+		const { latitude, longitude } = project.commune;
+		let marker = L.marker([latitude, longitude], { icon: createMarkerIcon(project.status) }).addTo(map)
+
+		marker.bindPopup(markerPopupTemplate(project))
 		L.layerGroup(marker)
 }
 
 function createMarkerIcon(status) {
 	return L.divIcon({ className: 'map-marker ' + statusToColorClass(status) });
+}
+
+function markerPopupTemplate(project) {
+
+	return `
+		<div class="marker-popup">
+			<main class="d-flex flex-column">
+				<p class="m-0 mb-2 fs-6 fw-bold">Commune</p>
+				<p class="m-0 fs-7 text-capitalize">${project.commune.name} (${project.commune.postal})</p>
+			</main>
+		</div>
+	`
 }
 
 Alpine.data("ProjectLocation", ProjectLocation)
