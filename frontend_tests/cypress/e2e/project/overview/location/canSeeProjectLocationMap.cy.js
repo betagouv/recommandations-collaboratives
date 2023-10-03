8fefcbed00adefa6c8abe3c64c3058c408a10ce3
Patricia Boh <patricia.boh@beta.gouv.fr>
2023-10-03 13:23:07 +0200

    [e2e] Add geolocation test - no location data

--- frontend_tests/cypress/e2e/project/overview/location/canSeeProjectLocationMap.cy.js
+++ frontend_tests/cypress/e2e/project/overview/location/canSeeProjectLocationMap.cy.js
@@ -6,23 +6,32 @@ describe('I can see the location of a project on the project overview', () => {
         cy.login("jean");
     })
 
-    it('displays the area of the commune if the commune geolocation data exists', () => {
-        const currentProject = projects[9];
+
+    it(`displays no area indicator if the project's commune is not provided`, () => {
+        const currentProject = projects[10];
         cy.visit(`/project/${currentProject.pk}`).then(() => {
             cy.get('[data-test-id="project-map-static"]').find('.leaflet-overlay-pane').then(() => {
                 cy.get('.area-circle').should('not.exist');
             });
         })
-
     })
 
-    it(`displays a circle around the project's commune coordinates`, () => {
+    it(`displays an area circle around the centroid of the commune if geolocation does not exist `, () => {
         const currentProject = projects[1];
         cy.visit(`/project/${currentProject.pk}`).then(() => {
             cy.get('[data-test-id="project-map-static"]').find('.leaflet-overlay-pane').find('.area-circle');
         })
     })
 
+    it('displays the area of the commune if geolocation data exists', () => {
+        const currentProject = projects[9];
+        cy.visit(`/project/${currentProject.pk}`).then(() => {
+            cy.get('[data-test-id="project-map-static"]').find('.leaflet-overlay-pane').then(() => {
+                // TODO: find a way to test commune area overlay
+                cy.get('.area-circle').should('not.exist');
+            });
+        })
+    })
 
     it(`opens a modal with an interactive map`, () => {
         const currentProject = projects[9];
