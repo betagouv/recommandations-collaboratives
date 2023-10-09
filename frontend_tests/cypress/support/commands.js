e883eacd52d40a61e0be053c87074c07ff4aa488
Patricia Boh <patricia.boh@beta.gouv.fr>
2023-10-09 12:59:53 +0200

    [e2e] Fix task banner test

--- frontend_tests/cypress/support/commands.js
+++ frontend_tests/cypress/support/commands.js
@@ -220,38 +220,41 @@ Cypress.Commands.add('navigateToProject', (index) => {
  * - img-presentation
  * - img-functional
  */
-Cypress.Commands.add('testImage', (img, role, type) => {
-    expect(img.alt).to.exist;
-    expect(img.src).not.to.equal('');
-    switch(role) {
-        case 'img-presentation': {
-            expect(img.alt).to.equal('');
-            break;
+Cypress.Commands.add('testImage', { prevSubject: true }, (subject, role, type) => {
+    cy.wrap(subject)
+    .should(([img]) => {
+        expect(img.alt).to.exist;
+        expect(img.src).not.to.equal('');
+        switch(role) {
+            case 'img-presentation': {
+                expect(img.alt).to.equal('');
+                break;
+            }
+            case 'img-functional':
+            case 'img-informative': {
+                expect(img.alt).not.to.equal('');
+                break;
+            }
+            default:
+                assert(false)
+                break;
         }
-        case 'img-functional':
-        case 'img-informative': {
-            expect(img.alt).not.to.equal('');
-            break;
+        switch(type) {
+            case 'svg': {
+                expect(img.width).to.be.greaterThan(0); // TODO: fix this test, as it will pass even if svg is not loaded
+                expect(img.alt).to.equal('');
+                break;
+            }
+            case 'png':
+            case 'jpg':
+            case 'jpeg': {
+                // "naturalWidth" and "naturalHeight" are set when the image loads
+                expect(img.naturalWidth).to.be.greaterThan(0);
+                break;
+            }
+            default:
+                assert(false)
+                break;
         }
-        default:
-            assert(false)
-            break;
-    }
-    switch(type) {
-        case 'svg': {
-            expect(img.width).to.be.greaterThan(0); // TODO: fix this test, as it will pass event if svg is not loaded
-            expect(img.alt).to.equal('');
-            break;
-        }
-        case 'png':
-        case 'jpg':
-        case 'jpeg': {
-            // "naturalWidth" and "naturalHeight" are set when the image loads
-            expect(img.naturalWidth).to.be.greaterThan(0);
-            break;
-        }
-        default:
-            assert(false)
-            break;
-    }
+    })
 })
