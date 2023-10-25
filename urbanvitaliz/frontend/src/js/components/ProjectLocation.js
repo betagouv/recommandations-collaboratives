1a44d51f6576cdfb46265e480faf0bea6b30c5ed
Patricia Boh <patricia.boh@beta.gouv.fr>
2023-10-25 14:11:29 +0200

    [geoloc] Fetch data from API Adresse, add marker layer

--- urbanvitaliz/frontend/src/js/components/ProjectLocation.js
+++ urbanvitaliz/frontend/src/js/components/ProjectLocation.js
@@ -2,15 +2,34 @@ import Alpine from 'alpinejs'
 
 import * as L from 'leaflet';
 import 'leaflet/dist/leaflet.css'
+import 'leaflet-control-geocoder';
 import 'leaflet-providers'
+
+import { statusToColorClass } from '../utils/statusToText'
 
 const codesComParis = ['75101','75102','75103','75104','75105','75106','75107','75108','75109','75110','75111','75112','75113','75114','75115','75116','75117','75118','75119','75120']
 const codesComLyon = ['69381','69382','69383','69384','69385','69386','69387','69388','69389']
 const codesComMarseille = ['13201','13202','13203','13204','13205','13206','13207','13208','13209','13210','13211','13212','13213','13214','13215','13216']
 
+// const iconRetinaUrl = 'assets/marker-icon-2x.png';
+// const iconUrl = 'assets/marker-icon.png';
+// const shadowUrl = 'assets/marker-shadow.png';
+// const iconDefault = L.icon({
+//   iconRetinaUrl,
+//   iconUrl,
+//   shadowUrl,
+//   iconSize: [25, 41],
+//   iconAnchor: [12, 41],
+//   popupAnchor: [1, -34],
+//   tooltipAnchor: [16, -28],
+//   shadowSize: [41, 41]
+// });
+// L.Marker.prototype.options.icon = iconDefault;
+
 
 // Doc: https://apicarto.ign.fr/api/doc/cadastre#/Commune/get_cadastre_commune
 const apiCadastre = 'https://apicarto.ign.fr/api/cadastre'
+const apiAdresse = 'https://api-adresse.data.gouv.fr'
 const latLongFrance = [46.5, 1.20] // latitude and longitude of centroid of France
 
 function getGlobalCityCodeFromCodeInsee(codeInsee) {
@@ -47,6 +66,13 @@ async function fetchCommuneIgn(insee) {
 	return communeGeo;
 }
 
+async function fetchGeolocationByAddress(address) {
+	const apiEndpoint = `${apiAdresse}/search?`;
+	const searchParams = { q: address, limit: 10 } // TODO
+	const geoJSON = await fetch(apiEndpoint + new URLSearchParams(searchParams)).then(response => response.json());
+	return geoJSON;
+}
+
 function ProjectLocation(projectOptions) {
 	return {
 		mapIsSmall: true,
@@ -73,11 +99,11 @@ function ProjectLocation(projectOptions) {
 			
 			const geoData = {}
 			geoData.commune = await fetchCommuneIgn(insee);
-
+			geoData.location = await fetchGeolocationByAddress(this.project.location)
 			initMapLayers(Map, this.project, geoData);
 			
 			// forces map redraw to fit container
-			setTimeout(function(){  Map.invalidateSize()}, 0); 
+			setTimeout(function(){Map.invalidateSize()}, 0);
 			//Center Map
 			Map.panTo(new L.LatLng(latitude, longitude));
 
@@ -132,10 +158,14 @@ function initMap(idMap, project, options, zoom) {
 // Create layers composed with markers
 function initMapLayers(map, project, geoData) {
 	try {
-		addLayerAreaCommune(map, geoData.commune);
+		addLayerLatLong(map, geoData.location, project);
 	} catch(e) {
-		if(project.commune.latitude && project.commune.longitude) {
-			addLayerAreaCircle(map, project)
+		try {
+			addLayerAreaCommune(map, geoData.commune);
+		} catch(e) {
+			if(project.commune.latitude && project.commune.longitude) {
+				addLayerAreaCircle(map, project)
+			}
 		}
 	}
 }
@@ -148,6 +178,13 @@ function addLayerAreaCommune(map, geoData) {
 	L.geoJSON(geoData.features[0].geometry).addTo(map);
 }
 
+function addLayerLatLong(map, geoData, project) {
+	if(geoData.code && geoData.code === 400 || geoData.features.length === 0) {
+		throw Error(`Données API Adresse indisponibles pour ' ${geoData.location}`)
+	}
+	const coordinates = geoData.features[0].geometry.coordinates
+  L.marker(coordinates, { icon: createMarkerIcon(project) }).addTo(map)
+}
 // Create layers composed with markers
 function addLayerAreaCircle(map, project) {
 	const { latitude, longitude } = project.commune;
@@ -161,6 +198,29 @@ function addLayerAreaCircle(map, project) {
 }
 
 
+function createMarkerIcon(project) {
+	return L.divIcon({ className: `map-marker ${project.status === "NEW" ? 'project-marker new-project-marker' : 'project-marker'}` });
+}
+
+function markerPopupTemplate(project) {
+	const date = new Date(project.created_on).toLocaleDateString()
+
+	return `
+			<div class="marker-popup">
+					<header><h3><a href="/project/${project.id}/presentation">${project.name}</a></h3></header>
+					<main class="d-flex flex-column">
+							<p class="m-0 mb-2 fs-6 fw-bold">Organisation</p>
+							<p class="m-0 fs-7">${project.org_name}</p>
+							<hr/>
+							<p class="m-0 mb-2 fs-6 fw-bold">Lieu</p>
+							<p class="m-0 fs-7 text-capitalize">${project?.commune?.name} (${project?.commune?.postal})</p>
+							<hr/>
+							<p class="m-0 mb-2 fs-6 fw-bold">Date de dépot</p>
+							<p class="m-0 fs-7">${date}</p>
+					</main>
+			</div>
+	`
+}
 
 function mapOptions({interactive}) {
 	return {
