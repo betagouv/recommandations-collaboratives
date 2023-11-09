5881aeed76766de6430f85bfe96a683635d86e1b
Patricia Boh <patricia.boh@beta.gouv.fr>
2023-11-09 18:37:32 +0100

    [e2e] Add reactivate action to staff test

--- frontend_tests/cypress/e2e/project/staff/administration/canChangeProjectActiveStatus.cy.js
+++ frontend_tests/cypress/e2e/project/staff/administration/canChangeProjectActiveStatus.cy.js
@@ -7,20 +7,26 @@ describe('I can go to administration area of a project and change the active sta
         cy.login("staff");
     })
 
-    it('puts the project on stand-by', () => {
+    it('can change project active status', () => {
 
-        cy.visit(`/project/${currentProject.pk}`)
-        cy.get("[data-test-id='navigation-administration-tab']").click({force:true})
-        cy.url().should('include', '/administration')
+			cy.visit(`/project/${currentProject.pk}`)
+			cy.get("[data-test-id='navigation-administration-tab']").click({force:true})
+			cy.url().should('include', '/administration')
 
-        cy.get('[data-test-id="button-open-modal-deactivate-project"]')
-            .click({force:true})
+			cy.get('[data-test-id="button-open-modal-deactivate-project"]')
+					.click({force:true})
 
-        cy.get('[data-test-id="form__admin-deactivate-project"]')
-            .submit()
+			cy.get('[data-test-id="button-deactivate-project"')
+					.click({force:true})
 
-        cy.visit(`/project/${currentProject.pk}`)
-        cy.get('[data-test-id="banner-activate-project"]')
-            .should.exist
+			cy.visit(`/project/${currentProject.pk}`)
+			cy.get('[data-test-id="fr-consent-banner"]').find('[data-test-id="button-consent-decline-all"]').click()
+			cy.get('[data-test-id="button-open-modal-deactivate-project"]').should('not.exist')
+			cy.get('[data-test-id="banner-activate-project"]')
+					.get('[data-test-id="button-activate-project"]').click({force:true})
+			cy.visit(`/project/${currentProject.pk}`).then(() => {
+				cy.reload()
+				cy.get('[data-test-id="banner-activate-project"]').should('not.exist')
+			})
     })
 })
