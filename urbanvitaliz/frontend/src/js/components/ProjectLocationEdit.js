7b943f5b7f62d40d815567f8fa2d9534ee449d09
Patricia Boh <patricia.boh@beta.gouv.fr>
2023-11-24 10:32:05 +0100

    [geoloc] Fix static map centering, cleanup

--- urbanvitaliz/frontend/src/js/components/ProjectLocationEdit.js
+++ urbanvitaliz/frontend/src/js/components/ProjectLocationEdit.js
@@ -12,7 +12,6 @@ function ProjectLocationEdit(projectOptions) {
 	return {
 		mapIsSmall: true,
 		project: null,
-		editLocationModal: null,
 		map: null,
 		zoom: 5,
 
@@ -27,32 +26,14 @@ function ProjectLocationEdit(projectOptions) {
 					longitude: projectOptions.commune.longitude ? parseFloat(projectOptions.commune.longitude) : null,
 				}
 			}
-			const options = mapUtils.mapOptions({interactive: true, zoom:false});
 			const { latitude, longitude, insee } = this.project.commune;
-
-
 			this.zoom = latitude && longitude ? 11 : this.zoom;
 
 			const geoData = {}
 
 			geoData.commune = await geolocUtils.fetchCommuneIgn(insee);
 			geoData.location = await geolocUtils.fetchGeolocationByAddress(this.project.location);
-
-			const Map = mapUtils.initMap('map-location-edit', this.project, options, this.zoom);
-			mapUtils.initMapLayers(Map, this.project, geoData);
-
-			// forces map redraw to fit container
-			setTimeout(function(){Map.invalidateSize()}, 0);
-
-			//Center Map
-			Map.panTo(new L.LatLng(latitude, longitude));
-			this.map = Map;
-			this.initLocationEditMap(this.project, geoData);
-		},
-
-		openEditLocationModal() {
-			this.mapIsSmall = false;
-			this.editLocationModal.show();
+			this.initInteractiveMap(this.project, geoData);
 		},
 
 		updateProjectLocation(coordinates)  {
@@ -66,17 +47,24 @@ function ProjectLocationEdit(projectOptions) {
 			console.log(coordinates);
 		},
 
-		initLocationEditMap(project, geoData) {
-			const onClick = (coordinates) => this.updateProjectLocation(coordinates)
-			mapUtils.initMapLayers(this.map, project, geoData);
-			mapUtils.initMapControllerBAN(this.map, project, geoData, onClick);
+		initInteractiveMap(project, geoData) {
+			const options = mapUtils.mapOptions({interactive: true, zoom:true});
+			const [latitude, longitude] = mapUtils.getDefaultLatLngForMap(project)
 
-			this.map.on('click', function(e) {
+			const Map = mapUtils.initMap('map-location-edit', project, options, this.zoom);
+			mapUtils.initMapLayers(Map, project, geoData);
+
+			//Center Map
+			const onClick = (coordinates) => this.updateProjectLocation(coordinates)
+			mapUtils.initMapLayers(Map, project, geoData);
+			mapUtils.initMapControllerBAN(Map, project, geoData, onClick);
+
+			Map.on('click', function(e) {
 				onClick(e.latlng)
 			});
-			const map = this.map;
-
-			setTimeout(function(){map.invalidateSize()}, 0);
+			Map.panTo(new L.LatLng(latitude, longitude));
+			setTimeout(function(){Map.invalidateSize()}, 0);
+			this.map = Map;
 		},
 	}
 }
