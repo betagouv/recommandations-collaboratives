92769799059ffb30b79891bfdf0eb8ef1d3f9241
Sébastien Reuiller <sebastien@reuiller.email>
2023-12-22 16:19:16 +0100

    dynamic project tags in crm

--- urbanvitaliz/apps/home/utils.py
+++ urbanvitaliz/apps/home/utils.py
@@ -39,12 +39,11 @@ def make_new_site(name: str, domain: str, sender_email: str, sender_name: str) -
     try:
         models.SiteConfiguration.objects.get(site=site)
     except models.SiteConfiguration.DoesNotExist:
-        onboarding = onboarding_models.Onboarding.objects.create(
-            form="[{'type': 'header', 'subtype': 'h1', 'label': 'Header'}, {'type':"
-            "'text', 'required': False, 'label': '<br>',"
-            "'className': 'form-control', 'name':"
-            "'text-1660055681026-0', 'subtype': 'text'}]"
+        json_form = (
+            '[{"type":"header","subtype":"h1","label":"Header"},{"type":"text","required":false,"label":"<br>",'
+            '"className":"form-control","name":"text-1660055681026-0","subtype":"text"}]'
         )
+        onboarding = onboarding_models.Onboarding.objects.create(json_form)
 
         survey, created = survey_models.Survey.objects.get_or_create(
             site=site,
