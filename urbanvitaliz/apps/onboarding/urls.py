732ed9fc30ccc3e319412a7f8ff7c889f8d5f433
Patricia Boh <patricia.boh@beta.gouv.fr>
2024-01-29 20:24:37 +0100

    [dsrc_dj] (wip/tmp) Create test view for dsrc model (onboarding)

--- urbanvitaliz/apps/onboarding/urls.py
+++ urbanvitaliz/apps/onboarding/urls.py
@@ -30,6 +30,11 @@ urlpatterns = [
         views.select_commune,
         name="projects-onboarding-select-commune",
     ),
+    path(
+        r"dsrc-onboarding/",
+        views.dsrc_onboarding,
+        name="dsrc-onboarding",
+    ),
 ]
 
 # eof
