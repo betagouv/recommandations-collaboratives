93376909738bca98b89fb8f902cb233d86a7767d
Patricia Boh <patricia.boh@beta.gouv.fr>
2024-02-01 20:20:06 +0100

    [dsrc_dj] Custom form demo

--- urbanvitaliz/dsrc_ui/rollup.config.js
+++ urbanvitaliz/dsrc_ui/rollup.config.js
@@ -1,9 +1,34 @@
 import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
+import * as glob from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 export default {
 	// The source JavaScript files that we want to bundle
-	input: 'src/lib/index.js',
+	input: {
+		'dsrc-main.js': 'src/lib/index.js',
+		...Object.fromEntries(
+			glob.sync('src/ext/**/*.js').map((file) => [
+				// This removes `src/` as well as the file extension from each
+				// file, so e.g. src/nested/foo.js becomes nested/foo
+				path.relative('src/ext/', file.slice(0, file.length - path.extname(file).length)),
+				// This expands the relative paths to absolute paths, so e.g.
+				// src/nested/foo becomes /project/src/nested/foo.js
+				fileURLToPath(new URL(file, import.meta.url))
+			])
+		),
+		...Object.fromEntries(
+			glob.sync('src/lib/components/**/*.js').map((file) => [
+				// This removes `src/` as well as the file extension from each
+				// file, so e.g. src/nested/foo.js becomes nested/foo
+				path.relative('src/lib/', file.slice(0, file.length - path.extname(file).length)),
+				// This expands the relative paths to absolute paths, so e.g.
+				// src/nested/foo becomes /project/src/nested/foo.js
+				fileURLToPath(new URL(file, import.meta.url))
+			])
+		)
+	},
 	output: { dir: '../../static/assets/js/', format: 'esm' },
 	plugins: [
 		resolve({
--- urbanvitaliz/dsrc_ui/src/lib/index.js
+++ urbanvitaliz/dsrc_ui/src/lib/index.js
@@ -1,5 +1,1 @@
-// place files you want to import through the `$lib` alias in this folder.
-// import * as dsfr_scoped from '../ext/dsfr';
-import * as dsfr from '@gouvfr/dsfr/dist/dsfr.module'; // TODO: import nomodule too
-
-export default { dsfr };
+export default {};
