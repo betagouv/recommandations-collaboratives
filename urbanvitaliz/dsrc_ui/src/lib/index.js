0a3a3ea3efef7d89e5c9bf07bc876203a738ba8d
Patricia Boh <patricia.boh@beta.gouv.fr>
2024-02-01 21:37:18 +0100

    [dsrc_ui] Fix dsrc module import

--- urbanvitaliz/dsrc_ui/src/lib/index.js
+++ urbanvitaliz/dsrc_ui/src/lib/index.js
@@ -1,1 +1,5 @@
-export default {};
+// place files you want to import through the `$lib` alias in this folder.
+// import * as dsfr_scoped from '../ext/dsfr';
+import * as dsfr from '@gouvfr/dsfr/dist/dsfr.module'; // TODO: import nomodule too
+
+export { dsfr };
