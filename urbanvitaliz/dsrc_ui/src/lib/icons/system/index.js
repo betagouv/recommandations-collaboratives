026aab94ffc4b6e020505d60b72e0aefd9cff185
Patricia Boh <patricia.boh@beta.gouv.fr>
2024-02-01 15:42:21 +0100

    [dsrc_ui] Add error svg asset

--- urbanvitaliz/dsrc_ui/src/lib/icons/system/index.js
+++ urbanvitaliz/dsrc_ui/src/lib/icons/system/index.js
@@ -1,7 +1,9 @@
 import warningFill from './fr--warning-fill.svg';
 import infoFill from './fr--info-fill.svg';
+import errorFill from './fr--error-fill.svg';
 
 export default {
 	warningFill,
-	infoFill
+	infoFill,
+	errorFill
 };
