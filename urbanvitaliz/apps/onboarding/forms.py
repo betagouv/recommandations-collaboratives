49bbefcba3f8e78838b2ac4afda2a37494a503b4
Patricia Boh <patricia.boh@beta.gouv.fr>
2024-02-01 19:58:39 +0100

    [dsrc_dj] Page center layout base

--- urbanvitaliz/apps/onboarding/forms.py
+++ urbanvitaliz/apps/onboarding/forms.py
@@ -17,6 +17,16 @@ from dsrc.forms import DsrcBaseForm
 from . import models
 
 
+def set_autofocus_on_first_error(self):
+	"""
+	Sets the autofocus on the first field with an error message.
+	Not included in the __init__ by default because it can cause some side effects on
+	non-standard fields/forms.
+	"""
+	for field in self.errors.keys():
+		self.fields[field].widget.attrs.update({"autofocus": ""})
+		break
+
 ##################################################
 # Notes
 ##################################################
@@ -77,7 +87,7 @@ class OnboardingResponseWithCaptchaForm(OnboardingResponseForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
+        set_autofocus_on_first_error(self)
         # Skip captcha during tests
         if "PYTEST_CURRENT_TEST" in os.environ:
             self.fields.pop("captcha")
@@ -102,5 +112,145 @@ class SelectCommuneForm(forms.Form):
             queryset=communes, widget=forms.RadioSelect, label="Votre commune :"
         )
 
+class DsrcExampleForm(DsrcBaseForm):
+    
+    class Meta:
+        model = models.OnboardingResponse
+        fields = [
+            "first_name",
+            "last_name",
+            "phone",
+            "org_name",
+            "email",
+            "name",
+            "location",
+            "insee",
+            "description",
+            "response",
+            "captcha",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Skip captcha during tests
+        if "PYTEST_CURRENT_TEST" in os.environ:
+            self.fields.pop("captcha")
+
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox(api_params={"hl": "fr"}))
+    # basic fields
+    user_name = forms.CharField(label="Nom d’utilisateur", max_length=100)
+
+    user_email = forms.EmailField(
+        label="Adresse électronique",
+        help_text="Format attendu : prenom.nom@domaine.fr",
+        required=False,
+    )
+
+    password = forms.CharField(
+        label="Mot de passe", required=False, widget=forms.PasswordInput
+    )
+
+    sample_number = forms.IntegerField(
+        label="Nombre entier",
+        help_text="Un nombre inférieur à 0 déclenchera un message d’erreur",
+    )
+
+    sample_decimal = forms.DecimalField(
+        label="Nombre décimal",
+        required=False,
+    )
+
+    sample_disabled_field = forms.CharField(
+        label="Champ désactivé",
+        help_text="Ce champ est désactivé",
+        max_length=100,
+        disabled=True,
+        required=False,
+    )
+
+    # date and time
+    sample_date = forms.DateField(
+        label="Date",
+        required=False,
+        widget=forms.DateInput(attrs={"type": "date"}),
+    )
+
+    """
+    Not managed by the DSFR:
+    - DateTimeField
+    """
+
+    # Booleans and choicefields
+    sample_boolean = forms.BooleanField(label="Cochez la case", required=False)
+
+    sample_select = forms.ChoiceField(
+        label="Liste déroulante",
+        required=False,
+        choices=[(1, "Option 1"), (2, "Option 2"), (3, "Option 3")],
+    )
+
+    sample_multiple_select = forms.MultipleChoiceField(
+        label="Liste déroulante à choix multiples",
+        required=False,
+        choices=[(1, "Option 1"), (2, "Option 2"), (3, "Option 3")],
+    )
+
+    sample_radio = forms.ChoiceField(
+        label="Boutons radio",
+        required=False,
+        choices=[(1, "Premier choix"), (2, "Second choix"), (3, "Troisième choix")],
+        help_text="Le troisième choix renvoie une erreur s’il est sélectionné",
+        widget=forms.RadioSelect,
+    )
+
+    sample_checkbox = forms.ChoiceField(
+        label="Cases à cocher",
+        required=False,
+        choices=[
+            ("1", "Premier choix"),
+            ("2", "Second choix"),
+            ("3", "Troisième choix"),
+        ],
+        help_text="Le troisième choix renvoie une erreur s’il est sélectionné",
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    # text blocks
+    sample_comment = forms.CharField(widget=forms.Textarea, required=False)
+
+    sample_json = forms.JSONField(label="Fichier JSON", required=False)
+
+    # files
+
+    sample_file = forms.FileField(label="Pièce jointe", required=False)
+
+    def clean_sample_number(self):
+        sample_number = self.cleaned_data["sample_number"]
+
+        if sample_number < 0:
+            raise forms.ValidationError("Merci d’entrer un nombre positif")
+
+        return sample_number
+
+    def clean_sample_radio(self):
+        sample_radio = self.cleaned_data["sample_radio"]
+
+        if sample_radio == "3":
+            raise forms.ValidationError("Le troisième choix est interdit")
+
+        return sample_radio
+
+    def clean_sample_checkbox(self):
+        sample_checkbox = self.cleaned_data["sample_checkbox"]
+
+        if sample_checkbox == ["2"]:
+            raise forms.ValidationError("Le troisième choix est interdit")
+
+        return sample_checkbox
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_autofocus_on_first_error()
 
 # eof
