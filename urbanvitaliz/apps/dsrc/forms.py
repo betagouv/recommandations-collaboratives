9882bb03623ed1ac47513fc7109d4e102b316c68
Patricia Boh <patricia.boh@beta.gouv.fr>
2024-02-07 06:43:50 +0100

    [dsrc] Dsrc form fields rendering OK

--- urbanvitaliz/apps/dsrc/forms.py
+++ urbanvitaliz/apps/dsrc/forms.py
@@ -1,37 +1,48 @@
 from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset, Submit, ButtonHolder, HTML
+from crispy_forms.layout import Layout, Fieldset
+
 
 """
-Base DSRC form with built-in error handling and autofocus on first error.
+Base DSRC form:
+- built-in error handling
+- autofocus on first error
+- dsrc classes for form elements
 """
 
 class DsrcBaseForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.set_autofocus_on_first_error()
         self.helper = FormHelper()
+
+        # If the widget is already set for a Field: merge the default `dsrc` attrs with the ones provided by the widget
         for field_name, field in self.fields.items():
             if isinstance(field, forms.CharField):
                 if isinstance(field.widget, forms.Textarea):
+                    # If the widget is already a Textarea
                     field.widget.attrs = field.widget.attrs | {"classes": "dsrc-input"}
                     continue
                 if isinstance(field.widget, forms.PasswordInput):
-                    field.widget.attrs = field.widget.attrs | {"size": "sm", "classes": "dsrc-input"}
+                    # If the widget is already a PasswordInput
+                    field.widget.attrs = field.widget.attrs | {"classes": "dsrc-input"}
                     continue
+                # Default: set a TextInput widget with dsrc classes
                 field.widget = forms.TextInput(attrs={"classes": "dsrc-input"})
             if isinstance(field, forms.BooleanField):
+                # If the widget is already a CheckboxInput
                 if isinstance(field.widget, forms.CheckboxInput):
-                    field.widget.attrs={"size": "sm", "classes": "dsrc-checkbox-group"}
+                    field.widget.attrs = field.widget.attrs | {"classes": "dsrc-checkbox-group"}
                     continue
             if isinstance(field, forms.ChoiceField):
-                 # If the widget is already a RadioSelect or CheckboxSelectMultiple, skip it
+                # If the widget is already a RadioSelect
                 if isinstance(field.widget, forms.RadioSelect):
-                    field.widget.attrs={"classes": "dsrc-radio-group", "size": "sm"}
+                    field.widget.attrs = field.widget.attrs | {"classes": "dsrc-radio-group"}
                     continue
+                # If the widget is already a CheckboxSelectMultiple
                 if isinstance(field.widget, forms.CheckboxSelectMultiple):
-                    field.widget.attrs={"classes": "dsrc-checkbox-group", "size": "sm"}
+                    field.widget.attrs = field.widget.attrs | {"classes": "dsrc-checkbox-group"}
                     continue
+                # Default: set a Select widget with dsrc classes
                 field.widget = forms.Select(attrs={"classes": "dsrc-select"})
 
     def set_autofocus_on_first_error(self):
@@ -70,80 +81,13 @@ class DsrcExampleForm(DsrcBaseForm):
                 'sample_radio_group',
                 'sample_checkbox_group',
             ),
-            ButtonHolder(
-                    HTML('<span>Information Saved</span>'),
-                    Submit('submit', 'submit'),
-                )
         )
-
-    sample_text = forms.CharField(label="Nom d'usager", initial="", required=True)
-    sample_phone = forms.CharField(max_length=16, label="Téléphone", initial="", required=True)
-    sample_email =  forms.EmailField(
-        label="Courriel",
-        help_text="Format attendu : prenom.nom@domaine.fr",
-        required=True,
-    )
 
     def clean_email(self):
         """Make sure email is lowercased"""
         email = self.cleaned_data["email"]
         return email.lower()
 
-    sample_password = forms.CharField(
-        label="Mot de passe", required=True, widget=forms.PasswordInput()
-    )
-
-    sample_postcode = forms.CharField(max_length=5, required=False, label="Code Postal")
-
-    sample_description = forms.CharField(label="Description", widget=forms.Textarea(attrs={"rows":"5"}), required=False)
-
-    sample_disabled_field = forms.CharField(
-        label="Champ désactivé",
-        help_text="Ce champ est désactivé",
-        max_length=100,
-        disabled=True,
-        required=False,
-    )
-
-    # Booleans and choicefields
-    sample_boolean = forms.BooleanField(label="Cochez la case", required=False, widget=forms.CheckboxInput())
-
-    # Basic Select
-    sample_select = forms.ChoiceField(
-        label="Liste déroulante",
-        required=False,
-        choices=[(None, "Sélectionner une option"), (1, "Option 1"), (2, "Option 2"), (3, "Option 3")],
-    )
-
-    sample_disabled_field = forms.CharField(
-        label="Champ désactivé",
-        help_text="Ce champ est désactivé",
-        max_length=100,
-        disabled=True,
-        required=False,
-    )
-
-    # Multiple choice Select
-    sample_radio_group = forms.ChoiceField(
-        label="Boutons radio",
-        required=False,
-        choices=[(1, "Premier choix unique"), (2, "Second choix unique"), (3, "Troisième choix unique")],
-        help_text="Le troisième choix renvoie une erreur s’il est sélectionné",
-        widget=forms.RadioSelect(),
-    )
-
-    # Checkbox group
-    sample_checkbox_group = forms.MultipleChoiceField(
-        label="Cases à cocher",
-        required=False,
-        choices=[
-            ("1", "Premier choix"),
-            ("2", "Second choix"),
-            ("3", "Troisième choix"),
-        ],
-        help_text="Le troisième choix renvoie une erreur s’il est sélectionné",
-        widget=forms.CheckboxSelectMultiple(),
-    )
 
 	# Example clean method
     def clean_sample_radio(self):
@@ -162,4 +106,80 @@ class DsrcExampleForm(DsrcBaseForm):
             raise forms.ValidationError("Le troisième choix est interdit")
 
         return checkbox_group
+
+    # Basic text input
+    sample_text = forms.CharField(label="Nom d'usager", initial="", required=True)
+
+    # Phone input: input type=text, uses max_length to limit the number of characters
+    # TODO: add a phone number validation, pattern / mask
+    sample_phone = forms.CharField(max_length=16, label="Téléphone", initial="", required=True)
+
+    # Email input: input type=email
+    # TODO: add a phone number validation, pattern / mask
+    sample_email =  forms.EmailField(
+        label="Courriel",
+        help_text="Format attendu : prenom.nom@domaine.fr",
+        required=True,
+    )
+
+    # Password input, with a password widget, show/hide control, and a help text
+    sample_password = forms.CharField(
+        label="Mot de passe", required=True, widget=forms.PasswordInput(attrs={"size": "sm"})
+    )
+
+    # Postal code input: input type=text, uses max_length to limit the number of characters
+    # TODO: add a phone number validation, pattern / mask
+    sample_postcode = forms.CharField(max_length=5, required=False, label="Code Postal")
+
+    # Long text input: native html textarea
+    sample_description = forms.CharField(label="Description", widget=forms.Textarea(attrs={"rows":"5"}), required=False)
+
+    sample_disabled_field = forms.CharField(
+        label="Champ désactivé",
+        help_text="Ce champ est désactivé",
+        max_length=100,
+        disabled=True,
+        required=False,
+    )
+
+    # Boolean choice: checkbox
+    sample_boolean = forms.BooleanField(label="Cochez la case", required=False, widget=forms.CheckboxInput(attrs={"size": "sm"}))
+
+    # Native html Select
+    sample_select = forms.ChoiceField(
+        label="Liste déroulante",
+        required=False,
+        choices=[(None, "Sélectionner une option"), (1, "Option 1"), (2, "Option 2"), (3, "Option 3")],
+    )
+
+    # Text input, disabled
+    sample_disabled_field = forms.CharField(
+        label="Champ désactivé",
+        help_text="Ce champ est désactivé",
+        max_length=100,
+        disabled=True,
+        required=False,
+    )
+
+    # Unique choice : radio group
+    sample_radio_group = forms.ChoiceField(
+        label="Boutons radio",
+        required=False,
+        choices=[(1, "Premier choix unique"), (2, "Second choix unique"), (3, "Troisième choix unique")],
+        help_text="Le troisième choix renvoie une erreur s’il est sélectionné",
+        widget=forms.RadioSelect(attrs={"size": "sm"}),
+    )
+
+    # Multiple choice : checkbox group
+    sample_checkbox_group = forms.MultipleChoiceField(
+        label="Cases à cocher",
+        required=False,
+        choices=[
+            ("1", "Premier choix"),
+            ("2", "Second choix"),
+            ("3", "Troisième choix"),
+        ],
+        help_text="Le troisième choix renvoie une erreur s’il est sélectionné",
+        widget=forms.CheckboxSelectMultiple(attrs={"size": "sm"}),
+    )
 # eof
