1a770d8e0befd489578ce7d6a1ec1d6c964bd64b
Patricia Boh <patricia.boh@beta.gouv.fr>
2024-02-11 14:24:55 +0100

    [dsrc_ui] Update input validation classes and aria attribute

--- urbanvitaliz/dsrc_ui/src/lib/components/forms/DsrcForm.js
+++ urbanvitaliz/dsrc_ui/src/lib/components/forms/DsrcForm.js
@@ -8,13 +8,14 @@ function DsrcForm(formId, formData) {
 		ajvValidate: ValidationDsrcForm,
 		async init() {
 			if (!formData) {
-				// TODO: handle errors
+				// We shouldn't reach this state: the data should be available, or the server should have returned an error before reaching this point and the form should not have been rendered
 				console.error('Error fetching form data');
 			}
 			if (formData.errors) {
-				// TODO: handle errors
+				// We shouldn't reach this state: AJV validation should have prevented form submission
+				console.error('Error validating form data with AJV');
 			} else {
-				// If no errors: This is a blank form
+				// There are no errors: This is a blank form
 				const fields = Object.keys(formData);
 				fields.forEach((field) => {
 					this.form[field] = { ...formData[field], errors: [], touched: false };
@@ -29,9 +30,19 @@ function DsrcForm(formId, formData) {
 					}
 				});
 			});
+			// Disable browser validation as we are using our own
+			document.getElementById(formId).setAttribute('novalidate', '');
+			// Let the server know that JS is enabled
+			document.getElementById(`${formId}_js_enabled`).value = 'true';
 		},
 		validate(event) {
-			let valid = this.ajvValidate(this.form);
+			// debug
+			const fields = Object.keys(this.form);
+			const validateMap = {};
+			fields.forEach((field) => {
+				validateMap[field] = this.form[field].value;
+			});
+			let valid = this.ajvValidate(validateMap);
 			if (!valid) {
 				this.errors = this.ajvValidate.errors;
 			} else {
@@ -42,16 +53,29 @@ function DsrcForm(formId, formData) {
 			const errors = this.errors.filter((error) => error.instancePath.substring(1) === fieldName);
 			return errors.map((error) => error.message);
 		},
+		fieldHasError(fieldName) {
+			const field = this.form[fieldName];
+			return field.touched === true && field.errors.length > 0;
+		},
 		validateInput(event) {
 			const field = event.target.name;
 			this.validate();
 			this.form[field].errors = this.getFieldErrors(field);
+			if (this.form[field].is_valid === false && this.form[field].errors.length === 0) {
+				this.form[field].is_valid = true;
+				this.form[field].valid_class = 'valid';
+			} else if (this.form[field].errors.length > 0) {
+				this.form[field].is_valid = false;
+				this.form[field].valid_class = 'error';
+			}
 		},
 		touchInput(event) {
-			this.form[event.target.name].touched = true;
+			const field = event.target.name;
+			this.form[field].touched = true;
 		},
 		changeInput(event) {
-			this.form[event.target.name].changed = true;
+			const field = event.target.name;
+			this.form[field].changed = true;
 		}
 	};
 }
