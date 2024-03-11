6614d4dbe35c264030fc68469d967f007a7452b9
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2024-03-11 11:04:14 +0100

    [refactor] extract package and add css onboarding

--- urbanvitaliz/frontend/src/js/utils/ajv/schema/ajv.schema.OnboardingSigninForm.js
+++ urbanvitaliz/frontend/src/js/utils/ajv/schema/ajv.schema.OnboardingSigninForm.js
@@ -1,4 +1,4 @@
-import { schemaFormInputs } from './ajv.schema.forms';
+import { schemaFormInputs, maxLengthErrorMessage, minLengthErrorMessage } from './ajv.schema.forms';
 
 /**
  * Validation schema for the form: DsrcFormValidator
@@ -8,12 +8,25 @@ export const schemaOnboardingStep1FormValidator = {
   type: 'object',
   properties: {
     first_name: { $ref: '#/definitions/text' },
-    last_name: { $ref: '#/definitions/text' },
-    company_name: { $ref: '#/definitions/text' },
+    last_name: { 
+      allOf: [
+        {
+          type: 'string',
+          minLength: 1,
+          errorMessage: minLengthErrorMessage(1),
+        },
+        {
+          type: 'string',
+          maxLength: 50,
+          errorMessage: maxLengthErrorMessage(50),
+        },
+      ],
+    },
+    org_name: { $ref: '#/definitions/text' },
     role: { $ref: '#/definitions/text' },
-    email_adress: { $ref: '#/definitions/email' },
+    email: { $ref: '#/definitions/email' },
     password: { $ref: '#/definitions/password' },
-    phone_number: { $ref: '#/definitions/phone' },
+    phone: { $ref: '#/definitions/phone' },
   },
   required: [
     'first_name',
