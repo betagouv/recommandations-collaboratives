fccbd3055380536569d8d5a59e9500fcfc2e110a
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2024-04-29 15:20:02 +0200

    [test] start geoloc testing

--- frontend_tests/cypress/support/tools/geolocation.js
+++ frontend_tests/cypress/support/tools/geolocation.js
@@ -3,145 +3,170 @@
  */
 
 const domElements = {
-	// Project Location
-	PROJECT_LOCATION: '[data-test-id="project-location"]',
-	LINK_PROJECT_LOCATION_EDIT: '[data-test-id="link-project-location-edit"]',
-	MESSAGE_LOCATION_UNKNOWN: '[data-test-id="message-project-location-unknown"]',
+    // Project Location
+    PROJECT_LOCATION: '[data-test-id="project-location"]',
+    LINK_PROJECT_LOCATION_EDIT: '[data-test-id="link-project-location-edit"]',
+    MESSAGE_LOCATION_UNKNOWN:
+        '[data-test-id="message-project-location-unknown"]',
 
-	// Map Selectors
-	LEAFLET_LOCATION_OVERLAY_PANE: '.leaflet-overlay-pane',
-	LEAFLET_LOCATION_CONTROL_PANE: '.leaflet-control-pane',
-	LEAFLET_LOCATION_MARKER_PANE: '.leaflet-marker-pane',
-	LEAFLET_AREA_CIRCLE: '.area-circle',
-	LEAFLET_AREA_COMMUNE: '.area-commune',
-	LEAFLET_MARKER_PROJECT_LOCATION: '.project-location-marker',
-	LEAFLET_MARKER_PROJECT_COORDINATES: '.project-coordinates-marker',
-	LEAFLET_POPUP_LATITUDE: '[data-test-id="project-coord-x-latitude"]',
-	LEAFLET_POPUP_LONGITUDE: '[data-test-id="project-coord-y-longitude"]',
-	LEAFLET_CONTROL_ZOOM: '.leaflet-control-zoom',
+    // Map Selectors
+    LEAFLET_LOCATION_OVERLAY_PANE: '.leaflet-overlay-pane',
+    LEAFLET_LOCATION_CONTROL_PANE: '.leaflet-control-pane',
+    LEAFLET_LOCATION_MARKER_PANE: '.leaflet-marker-pane',
+    LEAFLET_AREA_CIRCLE: '.area-circle',
+    LEAFLET_AREA_COMMUNE: '.area-commune',
+    LEAFLET_MARKER_PROJECT_LOCATION: '.project-location-marker',
+    LEAFLET_MARKER_PROJECT_COORDINATES: '.project-coordinates-marker',
+    LEAFLET_POPUP_LATITUDE: '[data-test-id="project-coord-x-latitude"]',
+    LEAFLET_POPUP_LONGITUDE: '[data-test-id="project-coord-y-longitude"]',
+    LEAFLET_CONTROL_ZOOM: '.leaflet-control-zoom',
 
-	// Map - Project Overview
-	BUTTON_OPEN_MAP_MODAL: '[data-test-id="toggle-open-map-modal"]',
-	PROJECT_LOCATION_OVERVIEW:'[data-test-id="project-overview-map"]',
-	PROJECT_LOCATION_MODAL:'[data-test-id="project-location-modal"]',
-	PROJECT_MAP_STATIC:'[data-test-id="map-static"]',
-	PROJECT_MAP_INTERACTIVE:'[data-test-id="map-interactive"]',
-	PROJECT_MAP_EDIT: '[data-test-id="map-edit"]',
-	INPUT_ADDRESS_LOCATION_EDIT: '.leaflet-control-geocoder-ban-form input',
-	SELECT_ADDRESS_LOCATION_EDIT: '.leaflet-control-geocoder-ban-alternatives li:first-child',
-	BUTTON_SAVE_PROJECT_LOCATION: '[data-test-id="button-save-project-location"]'
-}
+    // Map - Project Overview
+    BUTTON_OPEN_MAP_MODAL: '[data-test-id="toggle-open-map-modal"]',
+    PROJECT_LOCATION_OVERVIEW: '[data-test-id="project-overview-map"]',
+    PROJECT_LOCATION_MODAL: '[data-test-id="project-location-modal"]',
+    PROJECT_MAP_STATIC: '[data-test-id="map-static"]',
+    PROJECT_MAP_INTERACTIVE: '[data-test-id="map-interactive"]',
+    PROJECT_MAP_EDIT: '[data-test-id="map-edit"]',
+    INPUT_ADDRESS_LOCATION_EDIT: '.leaflet-control-geocoder-ban-form input',
+    SELECT_ADDRESS_LOCATION_EDIT:
+        '.leaflet-control-geocoder-ban-alternatives li:first-child',
+    BUTTON_SAVE_PROJECT_LOCATION:
+        '[data-test-id="button-save-project-location"]',
+};
 
 class ProjectLocation {
-	dom
+    dom;
 
-	constructor(dom) {
-		this.dom = dom
-	}
+    constructor(dom) {
+        this.dom = dom;
+    }
 
-	getSelector(map) {
-			let selector 
-			switch(map) {
-				case 'map-static':
-					selector = this.dom.PROJECT_MAP_STATIC;
-					break;
-				case 'map-interactive':
-					selector = this.dom.PROJECT_MAP_INTERACTIVE
-					break;
-				case 'map-edit':
-					selector = this.dom.PROJECT_MAP_EDIT
-					break;
-			}
-			return selector 
-	}
+    getSelector(map) {
+        let selector;
+        switch (map) {
+            case 'map-static':
+                selector = this.dom.PROJECT_MAP_STATIC;
+                break;
+            case 'map-interactive':
+                selector = this.dom.PROJECT_MAP_INTERACTIVE;
+                break;
+            case 'map-edit':
+                selector = this.dom.PROJECT_MAP_EDIT;
+                break;
+        }
+        return selector;
+    }
 
-	// Navigation
+    // Navigation
 
-	navigateToLocationEditPage() {
-		cy.get(this.dom.LINK_PROJECT_LOCATION_EDIT).click({force:true}).then(() => {
-			cy.wait(600); // TODO: fix by testing loading state (+ add loading spinner)
-			cy.get(this.dom.PROJECT_MAP_EDIT).should('be.visible')
-		});
-	}
+    navigateToLocationEditPage() {
+        cy.get(this.dom.LINK_PROJECT_LOCATION_EDIT)
+            .click({ force: true })
+            .then(() => {
+                cy.wait(600); // TODO: fix by testing loading state (+ add loading spinner)
+                cy.get(this.dom.PROJECT_MAP_EDIT).should('be.visible');
+            });
+    }
 
-	navigateToLocationEditPageFromOverview() {
-		cy.get(this.dom.PROJECT_LOCATION_OVERVIEW).find(this.dom.LINK_PROJECT_LOCATION_EDIT).click({force:true})
-		cy.get(this.dom.PROJECT_MAP_EDIT).should('be.visible')
-	}
+    navigateToLocationEditPageFromOverview() {
+        cy.get(this.dom.LINK_PROJECT_LOCATION_EDIT)
+            .first()
+            .click({ force: true });
+        cy.get(this.dom.PROJECT_MAP_EDIT).should('be.visible');
+    }
 
-	// Actions
+    // Actions
 
-	openMapModal() {
-		cy.get(this.dom.BUTTON_OPEN_MAP_MODAL).click({force:true})
-		cy.get(this.dom.PROJECT_LOCATION_MODAL).should('be.visible')
-		cy.get(this.dom.PROJECT_MAP_INTERACTIVE).find(this.dom.LEAFLET_CONTROL_ZOOM)
-	}
+    openMapModal() {
+        cy.get(this.dom.BUTTON_OPEN_MAP_MODAL).click({ force: true });
+        cy.get(this.dom.PROJECT_LOCATION_MODAL).should('be.visible');
+        cy.get(this.dom.PROJECT_MAP_INTERACTIVE).find(
+            this.dom.LEAFLET_CONTROL_ZOOM
+        );
+    }
 
-	editProjectLocationUsingAddressField(address) {
-		cy.get(this.dom.INPUT_ADDRESS_LOCATION_EDIT).focus().type(address)
-		cy.get(this.dom.SELECT_ADDRESS_LOCATION_EDIT).click({force:true}).then(() => {
-			cy.get(this.dom.SELECT_ADDRESS_LOCATION_EDIT).click({force:true})
-		})
-	}
+    editProjectLocationUsingAddressField(address) {
+        cy.get(this.dom.INPUT_ADDRESS_LOCATION_EDIT).focus().type(address);
+        cy.get(this.dom.SELECT_ADDRESS_LOCATION_EDIT)
+            .click({ force: true })
+            .then(() => {
+                cy.get(this.dom.SELECT_ADDRESS_LOCATION_EDIT).click({
+                    force: true,
+                });
+            });
+    }
 
-	editProjectLocationUsingInteractiveMap() {
-		// TODO: fix this test
-		cy.get(this.dom.PROJECT_MAP_EDIT).click({force:true})
-	}
+    editProjectLocationUsingInteractiveMap() {
+        // TODO: fix this test
+        cy.get(this.dom.PROJECT_MAP_EDIT).click({ force: true });
+    }
 
-	saveProjectLocation() {
-		cy.get(this.dom.BUTTON_SAVE_PROJECT_LOCATION).click({force:true})
-	}
+    saveProjectLocation() {
+        cy.get(this.dom.BUTTON_SAVE_PROJECT_LOCATION).click({ force: true });
+    }
 
-	// Verifications
+    // Verifications
 
-	checkMissingCoordinatesMessage(condition='not.exist') {
-		cy.get(this.dom.MESSAGE_LOCATION_UNKNOWN).should(condition)
-	}
+    checkMissingCoordinatesMessage(condition = 'not.exist') {
+        cy.get(this.dom.MESSAGE_LOCATION_UNKNOWN).should(condition);
+    }
 
-	checkMapLayerProjectCoordinates(map='map-static') {
-		cy.get(this.getSelector(map)).get(this.dom.LEAFLET_LOCATION_MARKER_PANE).then(() => {
-			cy.get(this.dom.LEAFLET_MARKER_PROJECT_COORDINATES).should('exist');
-			cy.get(this.dom.LEAFLET_POPUP_LATITUDE).should('not.exist')
-		});
-	}
+    checkMapLayerProjectCoordinates(map = 'map-static') {
+        cy.get(this.getSelector(map))
+            .get(this.dom.LEAFLET_LOCATION_MARKER_PANE)
+            .then(() => {
+                cy.get(this.dom.LEAFLET_MARKER_PROJECT_COORDINATES).should(
+                    'exist'
+                );
+                cy.get(this.dom.LEAFLET_POPUP_LATITUDE).should('not.exist');
+            });
+    }
 
-	checkMapLayerProjectLocation(map='map-static') {
-		cy.get(this.getSelector(map)).get(this.dom.LEAFLET_LOCATION_MARKER_PANE).then(() => {
-			cy.get(this.dom.LEAFLET_MARKER_PROJECT_LOCATION).click({force: true})
-			cy.get(this.dom.LEAFLET_POPUP_LATITUDE).should('exist')
-			cy.get(this.dom.LEAFLET_POPUP_LONGITUDE).should('exist')
-		});
-	}
+    checkMapLayerProjectLocation(map = 'map-static') {
+        cy.get(this.getSelector(map))
+            .get(this.dom.LEAFLET_LOCATION_MARKER_PANE)
+            .then(() => {
+                cy.get(this.dom.LEAFLET_MARKER_PROJECT_LOCATION).click({
+                    force: true,
+                });
+                cy.get(this.dom.LEAFLET_POPUP_LATITUDE).should('exist');
+                cy.get(this.dom.LEAFLET_POPUP_LONGITUDE).should('exist');
+            });
+    }
 
-	checkMapLayerAreaCommune(map='map-static') {
-		cy.get(this.getSelector(map)).get(this.dom.LEAFLET_LOCATION_OVERLAY_PANE).then(() => {
-			cy.get(this.dom.LEAFLET_AREA_COMMUNE).should('exist');
-		});
-	}
+    checkMapLayerAreaCommune(map = 'map-static') {
+        cy.get(this.getSelector(map))
+            .get(this.dom.LEAFLET_LOCATION_OVERLAY_PANE)
+            .then(() => {
+                cy.get(this.dom.LEAFLET_AREA_COMMUNE).should('exist');
+            });
+    }
 
-	checkMapLayerCircle(condition='exist', map='map-static') {
-		let selector 
-		switch(map) {
-			case 'map-static':
-				selector = this.dom.PROJECT_MAP_STATIC
-			case 'map-interactive':
-				selector = this.dom.PROJECT_MAP_INTERACTIVE
-			case 'map-edit':
-				selector = this.dom.PROJECT_MAP_EDIT
-		}
-		cy.get(this.getSelector(map)).find(this.dom.LEAFLET_LOCATION_OVERLAY_PANE).then(() => {
-			cy.get(this.dom.LEAFLET_AREA_CIRCLE).should(condition);
-		});
-	}
+    checkMapLayerCircle(condition = 'exist', map = 'map-static') {
+        let selector;
+        switch (map) {
+            case 'map-static':
+                selector = this.dom.PROJECT_MAP_STATIC;
+            case 'map-interactive':
+                selector = this.dom.PROJECT_MAP_INTERACTIVE;
+            case 'map-edit':
+                selector = this.dom.PROJECT_MAP_EDIT;
+        }
+        cy.get(this.getSelector(map))
+            .find(this.dom.LEAFLET_LOCATION_OVERLAY_PANE)
+            .then(() => {
+                cy.get(this.dom.LEAFLET_AREA_CIRCLE).should(condition);
+            });
+    }
 
-	checkProjectAddressInput(address) {
-		cy.get(this.dom.LEAFLET_POPUP_LATITUDE).should('not.exist')
-		cy.get(this.dom.LEAFLET_POPUP_LONGITUDE).should('not.exist')
-		cy.contains(address).should('exist')
-	}
+    checkProjectAddressInput(address) {
+        cy.get(this.dom.LEAFLET_POPUP_LATITUDE).should('not.exist');
+        cy.get(this.dom.LEAFLET_POPUP_LONGITUDE).should('not.exist');
+        cy.contains(address).should('exist');
+    }
 }
 
-const projectLocation = new ProjectLocation(domElements)
+const projectLocation = new ProjectLocation(domElements);
 
-export default projectLocation
+export default projectLocation;
