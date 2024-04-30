f5d5cf981f9148dc6113dcfdd9cf5ff4556f40ef
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2024-04-30 14:42:01 +0200

    [fix] add visual info in required field

--- recoco/frontend/src/js/components/CitySelect.js
+++ recoco/frontend/src/js/components/CitySelect.js
@@ -1,36 +1,90 @@
-import Alpine from 'alpinejs'
+import Alpine from 'alpinejs';
+import {
+  removeAndAddClassConditionaly,
+  removeClassIfExists,
+} from '../utils/cssUtils';
 
-Alpine.data("CitySearch", CitySearch)
+Alpine.data('CitySearch', CitySearch);
 
-function CitySearch() {
-    return {
-        // other default properties
-        isLoading: false,
-        postal: null,
-        cities: null,
-        getPostcode(postcode, insee) {
+function CitySearch(required = false) {
+  return {
+    // other default properties
+    isLoading: false,
+    postal: null,
+    cities: null,
+    required: required,
 
-            const postCodeString = JSON.parse(postcode.textContent)
-            const inseeString = JSON.parse(insee.textContent)
+    init() {
+      // [
+      //   {
+      //     fieldName: 'postcode',
+      //     events: ['focusout', 'input'],
+      //     tests: [{ property: 'length', value: 5 }],
+      //   },
+      //   {
+      //     fieldName: 'insee',
+      //     events: ['change'],
+      //     tests: [{ property: 'length', value: 5 }],
+      //   },
+      // ]
+      ['focusout', 'input'].forEach((event) => {
+        this.$refs.postcode.addEventListener(event, (e) => {
+          const errors = required && e.target.value.length < 5;
+          removeAndAddClassConditionaly(
+            errors,
+            e.target.parentElement,
+            'fr-input-group--valid',
+            'fr-input-group--error'
+          );
+        });
+      });
 
-            if (postCodeString) this.postal = postCodeString
-            if (inseeString) this.fetchCities(inseeString)
+      ['change'].forEach((event) => {
+        this.$refs.insee.addEventListener(event, (e) => {
+          console.log('insee change event', e.target.value);
+          const errors = required && e.target.value == '';
+          removeAndAddClassConditionaly(
+            errors,
+            e.target.parentElement,
+            'fr-input-group--valid',
+            'fr-input-group--error'
+          );
+        });
+      });
+    },
+    getPostcode(postcode, insee) {
+      const postCodeString = JSON.parse(postcode.textContent);
+      const inseeString = JSON.parse(insee.textContent);
 
-        },
-        fetchCities(currentInsee = null) {
+      if (postCodeString) this.postal = postCodeString;
+      if (inseeString) this.fetchCities(inseeString);
+    },
+    fetchCities(currentInsee = null) {
+      if (this.postal == '') return;
 
-            if (this.postal == "")
-                return
-
-            this.isLoading = true;
-            fetch(`/api/communes/?postal=${this.postal}`)
-                .then(res => res.json())
-                .then(data => {
-                    this.isLoading = false;
-                    this.cities = data;
-                }).finally(() => {
-                    if (currentInsee) this.$refs.insee.value = currentInsee;
-                });
-        }
-    }
+      this.isLoading = true;
+      fetch(`/api/communes/?postal=${this.postal}`)
+        .then((res) => res.json())
+        .then((data) => {
+          this.isLoading = false;
+          this.cities = data;
+          if (this.cities.length == 1) {
+            removeAndAddClassConditionaly(
+              true,
+              this.$refs.insee.parentElement,
+              'fr-input-group--error',
+              'fr-input-group--valid'
+            );
+          } else {
+            removeClassIfExists(
+              this.$refs.insee.parentElement,
+              'fr-input-group--valid'
+            );
+          }
+        })
+        .finally(() => {
+          if (currentInsee) this.$refs.insee.value = currentInsee;
+        });
+    },
+  };
 }
