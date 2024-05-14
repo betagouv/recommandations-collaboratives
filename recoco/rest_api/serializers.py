477553a03ad210ffc608de4cdcf826f78097d508
Etchegoyen Matthieu <etchegom@gmail.com>
2024-05-14 09:50:30 +0200

    add claims to token

--- recoco/rest_api/serializers.py
+++ recoco/rest_api/serializers.py
@@ -8,9 +8,7 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-
-        # Add custom claims
-        # token['name'] = user.name
-        # ...
-
+        token["email"] = user.email
+        token["first_name"] = user.first_name
+        token["last_name"] = user.last_name
         return token
