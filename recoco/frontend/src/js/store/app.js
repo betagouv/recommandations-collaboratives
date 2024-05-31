afdb8058e3e8567006bea66acdaf5c27d5f347fd
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2024-05-31 11:23:36 +0200

    [refactor] toast notifications

--- recoco/frontend/src/js/store/app.js
+++ recoco/frontend/src/js/store/app.js
@@ -1,11 +1,14 @@
-import Alpine from 'alpinejs'
+import Alpine from 'alpinejs';
+import { ToastType } from '../models/toastType';
 
 Alpine.store('app', {
-    isLoading: false,
-    notification: {
-        isOpen: false,
-        message: ''
-    }
-})
+  isLoading: false,
+  notification: {
+    isOpen: false,
+    message: '',
+    timeout: 2000,
+    type: ToastType.success,
+  },
+});
 
-export default Alpine.store('app')
+export default Alpine.store('app');
