2cd5253f663b26e4db7633a9d4ac48e12e05f9cd
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2024-07-01 14:45:47 +0200

    [feat] search project list

--- recoco/frontend/src/js/apps/advisorDashboard.js
+++ recoco/frontend/src/js/apps/advisorDashboard.js
@@ -3,3 +3,4 @@ import '../components/PersonalAdvisorDashboard';
 import '../../css/personalAdvisorDashboard.css';
 import '../../css/timeline.css';
 import '../../css/activityStream.css';
+import '../../css/regionFilter.css';
