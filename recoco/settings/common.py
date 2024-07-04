3b4f6abea4f0f616329065158a1cd232713ca473
Guillaume Libersat <glibersat@sigill.org>
2024-07-04 11:48:23 +0200

    [setings] comment out DRF page size for now

--- recoco/settings/common.py
+++ recoco/settings/common.py
@@ -354,7 +354,7 @@ REST_FRAMEWORK = {
         "django_filters.rest_framework.DjangoFilterBackend",
     ],
     # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 50,
+    # "PAGE_SIZE": 50, # XXX Disabled until pagination is activated on all endpoints
 }
 
 # https://django-rest-framework-simplejwt.readthedocs.io/en/latest/settings.html
