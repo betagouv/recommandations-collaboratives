af2db91d90b2ea9a8ac80836430536a4f5b097ad
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2024-09-09 15:06:36 +0200

    [test] FE collectivity notif when new task

--- frontend_tests/cypress/e2e/project/collectivity/canAccessRecommandations.cy.js
+++ frontend_tests/cypress/e2e/project/collectivity/canAccessRecommandations.cy.js
@@ -3,17 +3,30 @@ import projects from '../../../fixtures/projects/projects.json';
 const currentProject = projects[1];
 
 describe('I can access the recommandations', () => {
+  before(() => {
+    cy.login('jean');
+    cy.visit(`/project/${currentProject.pk}`);
+    cy.contains('Recommandations').click({ force: true });
+    cy.createTask('Notif test');
+    cy.logout();
+  });
+
   beforeEach(() => {
     cy.login('bob');
   });
 
-  it('goes to recommandations tab and see recommandations', () => {
+  it('goes to recommandations tab and see new recommandations', () => {
     cy.visit(`/project/${currentProject.pk}`);
-
     cy.contains('Recommandations').click({ force: true });
 
     cy.url().should('include', '/actions');
 
-    cy.contains('Ajouter une recommandation').should('not.exist');
+    cy.get('Ajouter une recommandation').should('not.exist');
+
+    cy.get('[data-test-id="badge-tab-new-task"]').should('exist');
+    cy.get('[data-test-id="banner-new-tasks"]').should('exist');
+    cy.get('[data-test-id="badge-new-task"]').should('exist');
+
+    cy.contains('Notif test').should('exist');
   });
 });
