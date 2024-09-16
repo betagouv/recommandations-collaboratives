f8aed9e42aed7bc81224f4f0ac33cced557aa334
Guillaume Libersat <glibersat@sigill.org>
2024-09-16 14:12:19 +0200

    [fronted] update manifest so actionsEmbed is usable

--- recoco/frontend/vite.config.js
+++ recoco/frontend/vite.config.js
@@ -57,6 +57,7 @@ const config = {
         projectEmailReminder: resolve('./src/js/apps/projectEmailReminder.js'),
         mapEditor: resolve('./src/js/apps/mapEditor.js'),
         menuNotification: resolve('./src/js/apps/menuNotification.js'),
+        actionsEmbed: resolve('./src/js/apps/actionsEmbed.js'),
       },
       output: {
         chunkFileNames: undefined,
