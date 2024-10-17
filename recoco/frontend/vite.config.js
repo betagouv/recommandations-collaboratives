fec1f2bf7bbdb5985cf814ef8053072ff1598e71
Guillaume Libersat <glibersat@sigill.org>
2024-10-17 09:06:34 +0200

    add projectKnowledge to manifest

--- recoco/frontend/vite.config.js
+++ recoco/frontend/vite.config.js
@@ -59,6 +59,7 @@ const config = {
         menuNotification: resolve('./src/js/apps/menuNotification.js'),
         actionsEmbed: resolve('./src/js/apps/actionsEmbed.js'),
         tasksEmbed: resolve('./src/js/apps/tasksEmbed.js'),
+        projectKnowledge: resolve('./src/js/apps/projectKnowledge.js'),
       },
       output: {
         chunkFileNames: undefined,
