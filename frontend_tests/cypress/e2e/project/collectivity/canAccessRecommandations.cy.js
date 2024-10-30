9b1f3e1a0d0f2ef32d45f9064fe24df2559ddfb2
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2024-10-30 17:06:48 +0100

    [test] fix access task

--- frontend_tests/cypress/e2e/project/collectivity/canAccessRecommandations.cy.js
+++ frontend_tests/cypress/e2e/project/collectivity/canAccessRecommandations.cy.js
@@ -3,32 +3,33 @@ import projects from '../../../fixtures/projects/projects.json';
 const currentProject = projects[1];
 
 describe('I can access the recommandations', () => {
-  before(() => {
+  it('goes to recommandations tab and see new recommandations', () => {
     cy.login('conseiller1');
-    cy.visit(`/project/${currentProject.pk}`);
-    cy.contains('Recommandations').click({ force: true });
+    cy.visit(`/project/${currentProject.pk}/actions`);
     cy.createTask('Notif test');
     cy.logout();
-  });
 
-  beforeEach(() => {
     cy.login('collectivité1');
-  });
-
-  it('goes to recommandations tab and see new recommandations', () => {
-    cy.visit(`/project/${currentProject.pk}`);
-    cy.contains('Recommandations').click({ force: true });
-
-    cy.url().should('include', '/actions');
-
+    cy.visit(`/project/${currentProject.pk}/actions`);
     cy.get('Ajouter une recommandation').should('not.exist');
+
+    // TODO : using intercept and dynamic wait
+    // cy.intercept(
+    //   'POST',
+    //   /\/api\/projects\/\d+\/tasks\/\d+\/notifications\/mark_all_as_read\/$/,
+    //   'success'
+    // ).as('markVisited');
 
     cy.get('[data-test-id="badge-new-task"]')
       .should('exist')
       .first()
       .click({ force: true });
+    // TODO : using intercept and dynamic wait
+    // cy.wait('@markVisited');
     cy.wait(500);
-    cy.get('[data-test-id="close-modal-task"]').click({ force: true });
+    cy.get('[data-test-id="close-modal-task"]')
+      .should('exist')
+      .click({ force: true });
     cy.get('[data-test-id="banner-new-tasks"]').should('not.exist');
     cy.get('[data-test-id="badge-new-task"]').should('not.be.visible');
   });
