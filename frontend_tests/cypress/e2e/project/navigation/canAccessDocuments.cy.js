0beb054aa36a285d5315a43a37ed353303587da9
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2024-10-30 19:34:07 +0100

    [test] access document

--- frontend_tests/cypress/e2e/project/navigation/canAccessDocuments.cy.js
+++ frontend_tests/cypress/e2e/project/navigation/canAccessDocuments.cy.js
@@ -1,26 +1,22 @@
 import projects from '../../../fixtures/projects/projects.json';
 const currentProject = projects[1];
 
-describe('I can access documents tab in a project as a member', () => {
-  beforeEach(() => {
+describe('I can access documents tab in a project', () => {
+  it('goes to the documents page of my project as a member', () => {
     cy.login('collectivité1');
-  });
-
-  it('goes to the documents page of my project', () => {
     cy.visit(`/project/${currentProject.pk}`);
-    cy.contains('Fichiers et liens').click({ force: true });
+    cy.get('[data-test-id="project-navigation-documents"]').click({
+      force: true,
+    });
     cy.url().should('include', '/documents');
   });
-});
 
-describe('I can access documents tab in a project as an advisor', () => {
-  beforeEach(() => {
+  it('goes to the documents page of my project as an advisor', () => {
     cy.login('conseiller1');
-  });
-
-  it('goes to the documents page of my project', () => {
     cy.visit(`/project/${currentProject.pk}`);
-    cy.contains('Fichiers et liens').click({ force: true });
+    cy.get('[data-test-id="project-navigation-documents"]').click({
+      force: true,
+    });
     cy.url().should('include', '/documents');
   });
 });
