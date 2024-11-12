af64aeb9031ded4eac040859e19b3ff035cc0c29
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2024-11-12 17:53:33 +0100

    [test] update link addition tests to use project-specific URLs and improve input delays

--- frontend_tests/cypress/e2e/tools/documents/canAddALink.cy.js
+++ frontend_tests/cypress/e2e/tools/documents/canAddALink.cy.js
@@ -1,4 +1,6 @@
 import link from '../../../fixtures/documents/link.json';
+import projects from '../../../fixtures/projects/projects.json';
+const currentProject = projects[1];
 
 describe('I can add a link on the document tab', () => {
   beforeEach(() => {
@@ -6,36 +8,24 @@ describe('I can add a link on the document tab', () => {
   });
 
   it('add a link', () => {
-    cy.visit('/');
-
-    cy.contains('Friche numéro 1').click({ force: true });
-
-    cy.contains('Fichiers et liens').click({ force: true });
-
-    cy.url().should('include', '/documents');
+    cy.visit(`/project/${currentProject.pk}/documents`);
 
     cy.document().then((doc) => {
       var popover = doc.getElementById('link-popover');
       popover.style = 'display:block !important;';
 
       cy.get('[name="the_link"]')
-        .type(link.url, { force: true })
+        .type(link.url, { force: true, delay: 0 })
         .should('have.value', link.url);
       cy.get('#link-description')
-        .type(link.description, { force: true })
+        .type(link.description, { force: true, delay: 0 })
         .should('have.value', link.description);
       cy.get('#link-submit-button').click({ force: true });
     });
   });
 
   it('show the link in the link list', () => {
-    cy.visit('/');
-
-    cy.contains('Friche numéro 1').click({ force: true });
-
-    cy.contains('Fichiers et liens').click({ force: true });
-
-    cy.url().should('include', '/documents');
+    cy.visit(`/project/${currentProject.pk}/documents`);
 
     cy.contains(link.description);
   });
