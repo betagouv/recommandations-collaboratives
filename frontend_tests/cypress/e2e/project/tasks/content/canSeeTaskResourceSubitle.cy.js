c4edfa32677707979c185f1c8212639bb25fee7a
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2024-11-12 20:01:43 +0100

    [test] refactor task resource subtitle test to use dynamic project ID for improved navigation

--- frontend_tests/cypress/e2e/project/tasks/content/canSeeTaskResourceSubitle.cy.js
+++ frontend_tests/cypress/e2e/project/tasks/content/canSeeTaskResourceSubitle.cy.js
@@ -1,25 +1,23 @@
 import resources from '../../../../fixtures/resources/resources.json';
 const currentResource = resources[4];
 const taskName = 'task intent';
+let currentProjectId;
 
 describe('I can go to tasks tab', () => {
   beforeEach(() => {
     cy.login('conseiller1');
-    cy.createProject('task intent');
+    cy.createProject('task intent').then((projectId) => {
+      currentProjectId = projectId;
+    });
   });
 
   it('creates a task with a resource and see the resource subtitle', () => {
-    cy.visit(`/projects`);
-    cy.contains('task intent').click({ force: true });
-    cy.becomeAdvisor();
-    cy.contains('Recommandations').click({ force: true });
-    cy.url().should('include', '/actions');
+    cy.visit(`/project/${currentProjectId}`);
+    cy.becomeAdvisor(currentProjectId);
+    cy.visit(`/project/${currentProjectId}/actions`);
 
     cy.createTask(taskName, '', true);
-    cy.get('[data-test-id="list-tasks-switch-button"]').should(
-      'have.class',
-      'active'
-    );
+    cy.get('[data-test-id="list-tasks-switch-button"]').should('be.checked');
 
     cy.contains(currentResource.fields.subtitle);
   });
