eda92285082bbe6eb8bcab9a8e75c3834b3bbbb0
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2024-11-12 18:27:21 +0100

    [test] refactor task comment editing test for improved navigation and assertion checks

--- frontend_tests/cypress/e2e/tools/task/canEditTaskComments.cy.js
+++ frontend_tests/cypress/e2e/tools/task/canEditTaskComments.cy.js
@@ -13,16 +13,11 @@ describe('As advisor, I can make a comment on a task', () => {
 
   it('adds a new comment, and stops from submitting the comment more than once', () => {
     cy.visit(`/project/${currentProject.pk}`);
-    cy.becomeAdvisor();
-    cy.contains('Recommandations').click({ force: true });
-    cy.url().should('include', '/actions');
+    cy.becomeAdvisor(currentProject.pk);
+    cy.visit(`/project/${currentProject.pk}/actions`);
 
     cy.createTask(taskName, '', true);
-    // cy.createTask(taskName, '', true);
-    cy.get('[data-test-id="list-tasks-switch-button"]').should(
-      'have.class',
-      'active'
-    );
+    cy.get('[data-test-id="list-tasks-switch-button"]').should('be.checked');
     cy.get('[data-test-id="task-initial-comment"]').should('exist');
 
     cy.get('[data-test-id="edit-comment-button"]')
@@ -30,7 +25,7 @@ describe('As advisor, I can make a comment on a task', () => {
       .click({ force: true });
     cy.get('[data-test-id="tiptap-editor-content"] .ProseMirror').type(
       message,
-      { force: true }
+      { force: true, delay: 0 }
     );
     cy.get('[data-test-id="button-submit-edit"]').click({ force: true });
     cy.get('[data-test-id="task-initial-comment"]').should('contain', message);
