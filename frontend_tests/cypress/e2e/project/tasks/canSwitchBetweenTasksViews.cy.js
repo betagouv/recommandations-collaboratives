bc88918560f00f645c8fb43d23d2008c6af76001
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2024-11-12 16:34:05 +0100

    [test] can switch between task view

--- frontend_tests/cypress/e2e/project/tasks/canSwitchBetweenTasksViews.cy.js
+++ frontend_tests/cypress/e2e/project/tasks/canSwitchBetweenTasksViews.cy.js
@@ -9,23 +9,16 @@ describe('I can go tasks tab', () => {
 
   it('switch between inline & kanban view', () => {
     cy.visit(`/project/${currentProject.pk}`);
-    cy.becomeAdvisor();
-    cy.contains('Recommandations').click({ force: true });
-    cy.url().should('include', '/actions');
+    cy.becomeAdvisor(currentProject.pk);
+    cy.visit(`/project/${currentProject.pk}/actions`);
     cy.createTask('test');
 
     cy.get('[data-test-id="kanban-tasks-switch-button"]').click({
       force: true,
     });
-    cy.get('[data-test-id="kanban-tasks-switch-button"]').should(
-      'have.class',
-      'active'
-    );
+    cy.get('[data-test-id="kanban-tasks-switch-button"]').should('be.checked');
 
     cy.get('[data-test-id="list-tasks-switch-button"]').click({ force: true });
-    cy.get('[data-test-id="list-tasks-switch-button"]').should(
-      'have.class',
-      'active'
-    );
+    cy.get('[data-test-id="list-tasks-switch-button"]').should('be.checked');
   });
 });
