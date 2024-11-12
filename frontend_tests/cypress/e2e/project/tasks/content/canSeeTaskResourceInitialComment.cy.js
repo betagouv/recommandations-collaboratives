285c0d414dad3e6b2190254816fb079e200d043c
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2024-11-12 20:01:02 +0100

    [test] refactor task resource initial comment test for improved project navigation and dynamic project ID handling

--- frontend_tests/cypress/e2e/project/tasks/content/canSeeTaskResourceInitialComment.cy.js
+++ frontend_tests/cypress/e2e/project/tasks/content/canSeeTaskResourceInitialComment.cy.js
@@ -2,30 +2,24 @@
 
 // const currentResource = resources[4]
 const taskName = 'task intent';
+let currentProjectId;
 
 describe('I can go to tasks tab', () => {
   beforeEach(() => {
     cy.login('conseiller1');
-    cy.createProject('new task');
+    cy.createProject('new task').then((projectId) => {
+      currentProjectId = projectId;
+    });
   });
 
   it('creates a task with a resource and see the initial comment', () => {
-    cy.visit(`/projects`);
-    cy.contains('new task').first().click({ force: true });
-    cy.becomeAdvisor();
-    // cy.wait(1000);
-    // cy.get('[data-test-id]="button-join-as-advisor"').click({
-    //     force: true,
-    // });
-    // cy.contains('Conseiller le projet').click({ force: true });
-    cy.contains('Recommandations').click({ force: true });
-    cy.url().should('include', '/actions');
+    cy.visit(`/project/${currentProjectId}`);
+    cy.becomeAdvisor(currentProjectId);
+
+    cy.visit(`/project/${currentProjectId}/actions`);
 
     cy.createTask(taskName, '', true);
-    cy.get('[data-test-id="list-tasks-switch-button"]').should(
-      'have.class',
-      'active'
-    );
+    cy.get('[data-test-id="list-tasks-switch-button"]').should('be.checked');
 
     cy.get('[data-test-id="task-initial-comment"]').should('exist');
   });
