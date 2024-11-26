da98cd1c6b5f928c2d188868f69eb83dc1251f62
Etchegoyen Matthieu <etchegom@gmail.com>
2024-11-26 16:43:30 +0100

    fixing sql queries

--- recoco/apps/metrics/sql_queries/projects.py
+++ recoco/apps/metrics/sql_queries/projects.py
@@ -17,16 +17,24 @@ def get_queryset() -> QuerySet:
         .annotate(
             hash=hash_field("id", salt="project"),
             site_domain=F("sites__domain"),
+            site_id=F("sites__id"),
         )
         .annotate(
             recommandation_count=Subquery(
-                Task.objects.filter(project_id=OuterRef("pk"), public=True)
+                Task.objects.filter(
+                    project_id=OuterRef("pk"),
+                    site_id=F("site_id"),
+                    public=True,
+                )
                 .order_by()
                 .annotate(count=Func(F("id"), function="Count"))
                 .values("count")
             ),
             advisor_count=Subquery(
-                ProjectSwitchtender.objects.filter(project_id=OuterRef("pk"))
+                ProjectSwitchtender.objects.filter(
+                    project_id=OuterRef("pk"),
+                    site_id=F("site_id"),
+                )
                 .order_by()
                 .annotate(count=Func(F("id"), function="Count"))
                 .values("count")
@@ -36,13 +44,17 @@ def get_queryset() -> QuerySet:
         .annotate(
             public_message_count=Count(
                 "notes",
-                filter=Q(notes__public=True),
+                filter=Q(
+                    notes__public=True,
+                    notes__site_id=F("site_id"),
+                ),
                 distinct=True,
             ),
             public_message_from_members_count=Count(
                 "notes",
                 filter=Q(
                     notes__public=True,
+                    notes__site_id=F("site_id"),
                     notes__created_by__in=F("members"),
                 ),
                 distinct=True,
@@ -51,6 +63,7 @@ def get_queryset() -> QuerySet:
                 "notes",
                 filter=Q(
                     notes__public=True,
+                    notes__site_id=F("site_id"),
                     notes__created_by__in=F("switchtender_sites__switchtender"),
                 ),
                 distinct=True,
@@ -59,7 +72,10 @@ def get_queryset() -> QuerySet:
         .annotate(
             private_message_count=Count(
                 "notes",
-                filter=Q(notes__public=False),
+                filter=Q(
+                    notes__public=False,
+                    notes__site_id=F("site_id"),
+                ),
                 distinct=True,
             )
         )
@@ -78,9 +94,10 @@ def get_queryset() -> QuerySet:
             ),
             commune_insee=F("commune__insee"),
         )
-        # .annotate(
-        #     status=F("project_sites__status"), filter=Q(project_sites__site__pk=site_id)
-        # )
+        .annotate(
+            status=F("project_sites__status"),
+            filter=Q(project_sites__site__pk=F("site_id")),
+        )
         .annotate(
             site_origin=F("project_sites__site__domain"),
             filter=Q(project_sites__is_origin=True),
@@ -92,7 +109,7 @@ def get_queryset() -> QuerySet:
         .values(
             "hash",
             "site_domain",
-            # "status",
+            "status",
             "created_on",
             "inactive_since",
             "commune_insee",
--- recoco/apps/metrics/sql_queries/users.py
+++ recoco/apps/metrics/sql_queries/users.py
@@ -1,65 +1,71 @@
 from django.contrib.auth.models import User
-from django.db.models import F, QuerySet
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models import BooleanField, Case, F, QuerySet, TextField, Value, When
+from django.db.models.functions import Concat, Lower, Replace
 
-# from django.contrib.postgres.aggregates import ArrayAgg
-# from django.contrib.sites.models import Site
-# from django.db.models import TextField, Value, When, BooleanField, Case,
-# from recoco.utils import make_group_name_for_site
 from ..utils import hash_field
 
 
 def get_queryset() -> QuerySet:
-    # site = Site.objects.get(pk=site_id)
-
-    # advisor_group_name = make_group_name_for_site("advisor", site)
-    # staff_group_name = make_group_name_for_site("staff", site)
-
     return (
         User.objects.order_by("date_joined")
         .annotate(
             hash=hash_field("id", salt="user"),
             site_domain=F("profile__sites__domain"),
+            site_slug=Lower(
+                Replace(
+                    Replace(
+                        "site_domain",
+                        Value("-"),
+                        Value("_"),
+                    ),
+                    Value("."),
+                    Value("_"),
+                )
+            ),
+            advisor_group_name=Concat("site_slug", Value("_advisor")),
+            staff_group_name=Concat("site_slug", Value("_staff")),
         )
-        # .annotate(
-        #     is_advisor=Case(
-        #         When(groups__name=advisor_group_name, then=True),
-        #         default=False,
-        #         output_field=BooleanField(),
-        #     ),
-        #     advising_departments=ArrayAgg("profile__departments__code", distinct=True),
-        # )
-        # .annotate(
-        #     is_site_staff=Case(
-        #         When(groups__name=staff_group_name, then=True),
-        #         default=False,
-        #         output_field=BooleanField(),
-        #     )
-        # )
-        # .annotate(
-        #     advisor_scope=Case(
-        #         When(
-        #             is_advisor=True,
-        #             profile__departments__isnull=True,
-        #             then=Value("NATIO"),
-        #         ),
-        #         When(
-        #             is_advisor=True,
-        #             profile__departments__isnull=False,
-        #             then=Value("REGIO"),
-        #         ),
-        #         default=None,
-        #         output_field=TextField(),
-        #     )
-        # )
+        .annotate(
+            is_advisor=Case(
+                When(groups__name=F("advisor_group_name"), then=True),
+                default=False,
+                output_field=BooleanField(),
+            ),
+            advising_departments=ArrayAgg("profile__departments__code", distinct=True),
+        )
+        .annotate(
+            is_site_staff=Case(
+                When(groups__name=F("staff_group_name"), then=True),
+                default=False,
+                output_field=BooleanField(),
+            )
+        )
+        .annotate(
+            advisor_scope=Case(
+                When(
+                    is_advisor=True,
+                    profile__departments__isnull=True,
+                    then=Value("NATIO"),
+                ),
+                When(
+                    is_advisor=True,
+                    profile__departments__isnull=False,
+                    then=Value("REGIO"),
+                ),
+                default=None,
+                output_field=TextField(),
+            )
+        )
         .values(
             "hash",
             "site_domain",
             "date_joined",
             "last_login",
-            # "is_advisor",
-            # "advising_departments",
-            # "advisor_scope",
-            # "is_site_staff",
+            "is_advisor",
+            "advising_departments",
+            "advisor_scope",
+            "is_site_staff",
         )
         .order_by("hash")
         .distinct()
