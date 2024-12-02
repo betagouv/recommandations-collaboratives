5c450bdb38966dc4ea18ed8741a25158a9879a8d
Etchegoyen Matthieu <etchegom@gmail.com>
2024-12-02 12:11:08 +0100

    WIP

--- recoco/apps/demarches_simplifiees/adapters/79838.py
+++ recoco/apps/demarches_simplifiees/adapters/79838.py
@@ -5,22 +5,15 @@ from recoco.apps.projects.models import Project
 
 def make(project: Project) -> dict[str, Any]:
     d = {
-        "identite_prenom": project.first_name,
-        "identite_nom": project.last_name,
+        # "identite_prenom": project.first_name,
+        # "identite_nom": project.last_name,
         "champ_Q2hhbXAtMzUyMTg1Mg": project.name,
-        "champ_Q2hhbXAtMzUyMTg0Nw": "",  # Fonction du référent
     }
-
-    if len(project.first_name) and len(project.last_name):
-        d.update(
-            {
-                "champ_Q2hhbXAtMzUyMTg0Ng": f"{project.last_name} {project.first_name}",
-            }
-        )
 
     if referrer := project.owner:
         d.update(
             {
+                "champ_Q2hhbXAtMzUyMTg0Ng": f"{referrer.last_name} {referrer.first_name}",
                 "champ_Q2hhbXAtMzUyMTg0OA": referrer.email,
                 "champ_Q2hhbXAtMzUyMTg0Nw": referrer.organization_position,
                 "champ_Q2hhbXAtMzUyMTg0OQ": referrer.phone_no,
