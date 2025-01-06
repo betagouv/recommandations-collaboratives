3425c260f3e17e35b3fd91e56eb34cdb7d24c907
Guillaume Libersat <glibersat@sigill.org>
2025-01-06 13:43:52 +0100

    Update common.py
    remove stale statements

--- recoco/settings/common.py
+++ recoco/settings/common.py
@@ -19,10 +19,6 @@ from multisite import SiteID
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# import sys
-# sys.path.append(str(BASE_DIR / "recoco"))
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
