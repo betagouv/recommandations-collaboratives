f1debd5bb0c00d151099ddaf9196477e7da83be8
Louis <louis.pluvinage0@gmail.com>
2025-01-14 17:59:48 +0100

    feat: update contact info test to ensure button visibility after initial click

--- frontend_tests/cypress/e2e/project/tasks/contact/canclickToSeeContact.cy.js
+++ frontend_tests/cypress/e2e/project/tasks/contact/canclickToSeeContact.cy.js
@@ -7,25 +7,12 @@ describe('I can see contacts information', () => {
     cy.login('bob');
   });
 
-  it('click to see contat info', () => {
-    cy.visit(`/project/${currentProject.pk}/actions`);
-    cy.get('[data-test-id="task-item"]').first().click();
-
-    cy.get('iframe')
-        .its('0.contentDocument.body')
-        .should('not.be.empty')
-        // eslint-disable-next-line @typescript-eslint/unbound-method
-        .then(cy.wrap)
-        .find('#caVaMarcher')
-        .each(($button)=>{cy.wrap($button).click({ force: true })});
-    cy.wait(5000);
-    cy.visit(`/project/${currentProject.pk}/actions`);
-    cy.get('[data-test-id="task-item"]').first().click();
-    cy.get('[data-test-id="see-contact-info-button"]').should('not.exist');
-//   cy.get('[data-test-id="see-contact-info-button"]').each(($button)=>{cy.wrap($button).click({ force: true })});
+  it('click to see contat info and doesnt have to click again', () => {
+    cy.visit(`/ressource/2`);
+    cy.get('[data-test-id="see-contact-info-button"]').each(($button)=>{cy.wrap($button).click({ force: true })});
+    cy.get('[data-test-id="see-contact-info-button"]').should('not.be.visible');
+    cy.visit(`/ressource/2`);
+    cy.get('[data-test-id="see-contact-info-button"]').should('not.be.visible');
   });
-//   it('doesnt have to click again to see the same contact info', () => {
 
-
-//   });
 });
