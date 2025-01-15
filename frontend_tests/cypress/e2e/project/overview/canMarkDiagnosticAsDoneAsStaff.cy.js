db31e790acd3a52cc1786ce2d52ca9b1fe922925
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2025-01-15 15:28:52 +0100

    [test] update tests

--- frontend_tests/cypress/e2e/project/overview/canMarkDiagnosticAsDoneAsStaff.cy.js
+++ frontend_tests/cypress/e2e/project/overview/canMarkDiagnosticAsDoneAsStaff.cy.js
@@ -3,18 +3,19 @@ describe('I can mark project diagnostic done ', () => {
     cy.login('staff');
     cy.visit(`/project/2/presentation`);
     cy.get('[data-cy="button-diag-project-done"]').should('be.visible').click();
-    cy.get('[data-cy="button-diag-project-done"]').should('not.be.visible');
+    cy.get('[data-cy="button-diag-project-done"]').should('not.exist');
+    cy.get('[data-cy="diag-project-done"]').should('be.visible');
   });
 
   it('do not display diagnostic button (as owner)', () => {
     cy.login('bob');
     cy.visit(`/project/2/presentation`);
-    cy.get('[data-cy="button-diag-project-done"]').should('not.be.visible');
+    cy.get('[data-cy="button-diag-project-done"]').should('not.exist');
   });
 
   it('do not display diagnostic button (as advisor)', () => {
     cy.login('conseiller1');
     cy.visit(`/project/2/presentation`);
-    cy.get('[data-cy="button-diag-project-done"]').should('not.be.visible');
+    cy.get('[data-cy="button-diag-project-done"]').should('not.exist');
   });
 });
