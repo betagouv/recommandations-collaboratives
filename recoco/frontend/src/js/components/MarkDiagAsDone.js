e299992f350513155d7dd466f6bbd52b86abe2e8
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2025-01-16 12:00:31 +0100

    [refactor] cleanup

--- recoco/frontend/src/js/components/MarkDiagAsDone.js
+++ recoco/frontend/src/js/components/MarkDiagAsDone.js
@@ -1,18 +1,11 @@
 import Alpine from 'alpinejs';
-import api, { hitcountUrl, projectUrl } from '../utils/api';
+import api, { projectUrl } from '../utils/api';
 
 Alpine.data(
   'MarkDiagAsDone',
   (currentUserId, projectId, isDiagDone = false) => {
     return {
       displayDiagButton: !isDiagDone,
-      // async init() {
-      //   const response = await api.get(projectUrl(projectId));
-      //   console.log(response.data);
-      //   console.log(response.data.is_diagnostic_done);
-      //   response.status === 200 &&
-      //     (this.displayDiagButton = !response.data.is_diagnostic_done);
-      // },
       async handleClickMarkDiagAsDone() {
         const response = await api.patch(projectUrl(projectId), {
           is_diagnostic_done: true,
