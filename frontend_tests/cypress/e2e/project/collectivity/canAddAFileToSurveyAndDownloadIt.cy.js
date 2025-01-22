05486b7dad528baed0d705d1a05a553508bb8c51
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2025-01-22 12:56:01 +0100

    [test] fix add file to survey

--- frontend_tests/cypress/e2e/project/collectivity/canAddAFileToSurveyAndDownloadIt.cy.js
+++ frontend_tests/cypress/e2e/project/collectivity/canAddAFileToSurveyAndDownloadIt.cy.js
@@ -6,16 +6,14 @@ const currentProject = projects[1];
 describe('I can fill a project survey @critical', () => {
   beforeEach(() => {
     cy.login('collectivité1');
-    cy.visit(`/project/${currentProject.pk}`);
   });
 
   it('fills up the survey and upload a file', () => {
-    cy.visit(`/project/${currentProject.pk}`);
+    cy.visit(`/project/${currentProject.pk}/connaissance`);
 
-    cy.get('[data-test-id="project-navigation-knowledge"]').click({
-      force: true,
-    });
-    cy.get('[data-test-id="link-fill-survey"]').first().click({ force: true });
+    cy.get('[data-test-id="link-fill-survey-cta"]')
+      .first()
+      .click({ force: true });
 
     // cy.url().should('include', '/projects/survey/')
 
@@ -37,7 +35,6 @@ describe('I can fill a project survey @critical', () => {
       .type('Fake comment on first survey question', { force: true })
       .should('have.value', 'Fake comment on first survey question');
 
-
     cy.get('[data-test-id="button-submit-survey-questionset"]').click({
       force: true,
     });
@@ -54,15 +51,14 @@ describe('I can fill a project survey @critical', () => {
   it('can see and download the file', () => {
     cy.visit(`/project/${currentProject.pk}/documents`);
 
-    cy.contains('Fichier récupéré de l\'état des lieux');
+    cy.contains("Fichier récupéré de l'état des lieux");
 
     // test download with cy verification
-    // cy.get('[data-test-id="project-navigation-documents"]').click({
-    //   force: true,
-    // });
-    // cy.get('[data-test-id="download-attachment"]').click({ force: true });
-    // cy.readFile(`${Cypress.config('downloadsFolder')}/ui-screenshot.png`)
-
-
+    cy.get('[data-cy="attachment-filename"]')
+      .invoke('text')
+      .then((text) => {
+        cy.get('[data-test-id="download-attachment"]').click();
+        cy.readFile(`${Cypress.config('downloadsFolder')}/${text}`);
+      });
   });
 });
