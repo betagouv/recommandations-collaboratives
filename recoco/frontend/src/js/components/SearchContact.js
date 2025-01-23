baa5e37a29321338ce5f2cd56b105235da2b61ff
Louis <louis.pluvinage0@gmail.com>
2025-01-23 12:33:03 +0100

    [feat] add contact card in messages finish all design of contact feature

--- recoco/frontend/src/js/components/SearchContact.js
+++ recoco/frontend/src/js/components/SearchContact.js
@@ -35,7 +35,36 @@ function SearchContact() {
     },
     addContact() {
       let textArea = document.querySelector('.tiptap.ProseMirror');
-      textArea.innerHTML += `<p>@${this.selectedContact.first_name} ${this.selectedContact.last_name}</p>`;
+      textArea.innerHTML += `<div class="fr-p-3v">
+    <div class="d-flex">
+        <div>
+            <span class="same-for-all-text contact-names fr-pr-1v">${this.selectedContact.first_name}</span>
+        </div>
+        <div>
+            <span class="same-for-all-text contact-names fr-pr-1v">${this.selectedContact.last_name}</span>
+        </div>
+        <div>
+            <span class="color-3a3a3a text-position">role organisation</span>
+        </div>
+    </div>
+    <div>
+        <span class="color-3a3a3a text-organisation">Organisation</span>
+    </div>
+    <div>
+        <a href="mailto:${this.selectedContact.email}"
+           class="same-for-all-text text-position">${this.selectedContact.email}
+        </a>
+    </div>
+    <div>
+        <a href="tel:+0606060606" class="same-for-all-text text-position"><span class="fr-icon--sm fr-icon-phone-line" aria-hidden="true"></span> tel user</a>
+    </div>
+    <div>
+        <a href="tel:+0606060606" class="same-for-all-text text-position"><span class="fr-icon--sm fr-icon-phone-line" aria-hidden="true"></span> num user</a>
+    </div>
+    <div>
+        <span class="same-for-all-text text-date">mis à jour le xxxxxx</span>
+    </div>
+</div>`;
       this.closeModal();
     },
     closeModal() {
