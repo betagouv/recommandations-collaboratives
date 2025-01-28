386431aadb70da417ec326e7703f14272d4822a5
Etchegoyen Matthieu <etchegom@gmail.com>
2025-01-28 09:12:02 +0100

    change field _detail to _link

--- recoco/apps/addressbook/serializers.py
+++ recoco/apps/addressbook/serializers.py
@@ -52,7 +52,7 @@ class OrganizationListSerializer(OrganizationSerializer):
     departments = SerializerMethodField()
     group = OrganizationGroupSerializer(read_only=True)
 
-    _detail = HyperlinkedIdentityField(view_name="api-addressbook-organization-detail")
+    _link = HyperlinkedIdentityField(view_name="api-addressbook-organization-detail")
     _search_rank = FloatField(source="search_rank", read_only=True)
 
     class Meta:
@@ -62,7 +62,7 @@ class OrganizationListSerializer(OrganizationSerializer):
             "name",
             "group",
             "departments",
-            "_detail",
+            "_link",
             "_search_rank",
         ]
 
@@ -87,7 +87,7 @@ class NestedOrganizationSerializer(OrganizationListSerializer):
         fields = [
             "id",
             "name",
-            "_detail",
+            "_link",
         ]
 
 
@@ -116,7 +116,7 @@ class ContactSerializer(BaseSerializerMixin, ModelSerializer):
 class ContactListSerializer(ContactSerializer):
     organization = NestedOrganizationSerializer(read_only=True, many=False)
 
-    _detail = HyperlinkedIdentityField(view_name="api-addressbook-contact-detail")
+    _link = HyperlinkedIdentityField(view_name="api-addressbook-contact-detail")
     _search_rank = FloatField(source="search_rank", read_only=True)
 
     class Meta:
@@ -130,7 +130,7 @@ class ContactListSerializer(ContactSerializer):
             "mobile_no",
             "division",
             "organization",
-            "_detail",
+            "_link",
             "_search_rank",
         ]
         read_only_fields = fields
@@ -163,6 +163,6 @@ class NestedContactSerializer(ContactListSerializer):
             "mobile_no",
             "division",
             "organization",
-            "_detail",
+            "_link",
         ]
         read_only_fields = fields
