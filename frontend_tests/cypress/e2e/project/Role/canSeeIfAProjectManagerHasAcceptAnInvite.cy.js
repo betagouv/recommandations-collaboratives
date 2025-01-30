c5b0106b91a464b8eb1939a07f5a6ed920dd056e
Louis <louis.pluvinage0@gmail.com>
2025-01-30 09:41:46 +0100

    [feat] add test V0.1

---
+++ frontend_tests/cypress/e2e/project/Role/canSeeIfAProjectManagerHasAcceptAnInvite.cy.js
@@ -0,0 +1,22 @@
+describe('I can go to a project and see if the main collaborator has accepted the invitation', () => {
+
+  it('can see if the main colloborator has accepted the invitation', () => {
+  //connect as advisor
+  //create a project
+  //invite new referent
+  //go to overview
+  //see if the main collaborator has not accepted the invitation
+  //go to conversation
+    //see if the main collaborator has not accepted the invitation
+
+    // OU aller sur un autre projet non initié par le référent et voir si le référent a accepté l'invitation
+    //connect as referent
+    //accept the invitation
+    //connect as advisor
+    //go to overview
+    //see if the main collaborator has accepted the invitation
+    //go to conversation
+    //see if the main collaborator has accepted the invitation
+
+  });
+});
