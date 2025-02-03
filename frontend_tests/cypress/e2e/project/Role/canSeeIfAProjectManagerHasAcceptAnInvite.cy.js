b02ce3fc7b004e2eed55ad5e013c06aacb939be8
Louis <louis.pluvinage0@gmail.com>
2025-02-03 14:54:15 +0100

    [feat] add test and fixtures for this feature

--- frontend_tests/cypress/e2e/project/Role/canSeeIfAProjectManagerHasAcceptAnInvite.cy.js
+++ frontend_tests/cypress/e2e/project/Role/canSeeIfAProjectManagerHasAcceptAnInvite.cy.js
@@ -1,22 +1,11 @@
 describe('I can go to a project and see if the main collaborator has accepted the invitation', () => {
 
-  it('can see if the main colloborator has accepted the invitation', () => {
-  //connect as advisor
-  //create a project
-  //invite new referent
-  //go to overview
-  //see if the main collaborator has not accepted the invitation
-  //go to conversation
-    //see if the main collaborator has not accepted the invitation
+  it('can see if the main colloborator has accepted the invitation or not', () => {
+    cy.login('jean');
+    cy.visit('/project/27/presentation');
+    cy.get('[data-test-id="invite-not-accepted-banner"]').should('exist');
 
-    // OU aller sur un autre projet non initié par le référent et voir si le référent a accepté l'invitation
-    //connect as referent
-    //accept the invitation
-    //connect as advisor
-    //go to overview
-    //see if the main collaborator has accepted the invitation
-    //go to conversation
-    //see if the main collaborator has accepted the invitation
-
+    cy.visit('/project/23/presentation');
+    cy.get('[data-test-id="invite-not-accepted-banner"]').should('not.exist');
   });
 });
