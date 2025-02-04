41c172d9a900ebb9f10886971c6f86bfe68c87be
Louis <louis.pluvinage0@gmail.com>
2025-02-04 18:45:45 +0100

    [test] make the test work

--- frontend_tests/cypress/e2e/project/tasks/contact/canShareContactOnFollowUp.cy.js
+++ frontend_tests/cypress/e2e/project/tasks/contact/canShareContactOnFollowUp.cy.js
@@ -4,18 +4,22 @@ describe('I can search and share a contact on a message editor', () => {
   });
 
   it('can search, select and shar a contact on a foolowup', () => {
-    cy.visit(`/project/2/action`);
-
-    cy.get('[data-test-id="task-item"]').click({ force: true });
+    cy.visit(`/project/2/actions#`);
+    //click on recommandation
+    cy.get('[data-test-id="task-item"]').first().click({ force: true });
+    //click on add contact button
     cy.get('[data-test-id="button-add-contact"]').click({ force: true });
-
+    //search for a contact
     cy.get('#search-contact-input')
       .type('Test', { force: true })
-
-    cy.get('[data-test-id="contact-to-select"]').click({ force: true });
+    //select a contact
+    cy.get('[data-test-id="contact-to-select"]').first().click({ force: true });
+    //send contact to tiptap editor
+    cy.get('[data-test-id="button-add-contact-to-tiptap-editor"]').click({ force: true });
+    //validate message followup
     cy.get('[data-test-id="button-submit-new"]').click({ force: true });
-
-    cy.get('[data-test-id="contact-to-select"]').should('be.visible');
+    //my contact should be visible on the followup
+    cy.get('[data-test-id="contact-card"]').should('be.visible');
 });
 
 });
