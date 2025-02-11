edcccdeffd42897317d2b68254a3ec37c06cf349
Etchegoyen Matthieu <etchegom@gmail.com>
2025-02-11 14:33:15 +0100

    enhance code

--- recoco/apps/crm/templatetags/crm_tags.py
+++ recoco/apps/crm/templatetags/crm_tags.py
@@ -8,7 +8,9 @@ created: 2021-06-29 11:30:42 CEST
 """
 
 from django import template
+from django.urls import reverse
 
+from recoco.apps.crm.models import Note
 from recoco.apps.projects.models import Project
 
 register = template.Library()
@@ -21,9 +23,15 @@ def projects_for_tag(tag_name):
 
 
 @register.simple_tag
-def note_update_url_name(related_ct: str) -> str | None:
-    return {
-        "organisation": "crm-organization-note-update",
-        "utilisateur": "crm-user-note-update",
-        "project": "crm-project-note-update",
-    }.get(related_ct.lower(), None)
+def get_note_update_url(note: Note) -> str | None:
+    match note.content_type.name.lower():
+        case "project":
+            return reverse("crm-project-note-update", args=(note.object_id, note.id))
+        case "utilisateur":
+            return reverse("crm-user-note-update", args=(note.object_id, note.id))
+        case "organization":
+            return reverse(
+                "crm-organization-note-update", args=(note.object_id, note.id)
+            )
+        case _:
+            return None
--- recoco/apps/crm/tests/test_crm_tags.py
+++ recoco/apps/crm/tests/test_crm_tags.py
@@ -1,8 +1,26 @@
-from recoco.apps.crm.templatetags.crm_tags import note_update_url_name
+import pytest
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from model_bakery import baker
+
+from recoco.apps.addressbook.models import Contact, Organization
+from recoco.apps.crm.models import Note
+from recoco.apps.crm.templatetags.crm_tags import get_note_update_url
+from recoco.apps.projects.models import Project
 
 
-def test_note_update_url_name():
-    assert note_update_url_name("organisation") == "crm-organization-note-update"
-    assert note_update_url_name("Utilisateur") == "crm-user-note-update"
-    assert note_update_url_name("project") == "crm-project-note-update"
-    assert note_update_url_name("unknown") is None
+@pytest.mark.django_db
+def test_get_note_update_url():
+    note = baker.prepare(Note, id=1, object_id=2)
+
+    note.content_type = ContentType.objects.get_for_model(User)
+    assert get_note_update_url(note) == "/crm/user/2/note/1"
+
+    note.content_type = ContentType.objects.get_for_model(Organization)
+    assert get_note_update_url(note) == "/crm/org/2/note/1"
+
+    note.content_type = ContentType.objects.get_for_model(Project)
+    assert get_note_update_url(note) == "/crm/project/2/note/1"
+
+    note.content_type = ContentType.objects.get_for_model(Contact)
+    assert get_note_update_url(note) is None
