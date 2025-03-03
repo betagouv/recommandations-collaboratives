1deff8b1b7d3ad8fc0c0404b09b0ba96ad629ff4
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2025-03-03 17:48:24 +0100

    [feat] add modal create contact

--- recoco/frontend/src/js/apps/createAndShareContact.js
+++ recoco/frontend/src/js/apps/createAndShareContact.js
@@ -1,7 +1,12 @@
-import '../components/SearchContact.js'
-import '../components/CreateContact.js'
-import '../components/CreateOrganisation.js'
-import '../components/SearchOrganisation.js'
-import '../components/MultiSelect.js'
-import '../store/contact.js'
-import '../store/previewModal.js'
+import '../components/SearchContact.js';
+import '../components/CreateContact.js';
+import '../components/CreateOrganisation.js';
+import '../components/SearchOrganisation.js';
+import '../components/MultiSelect.js';
+import '../store/contact.js';
+import '../store/previewModal.js';
+
+import Alpine from 'alpinejs';
+import focus from '@alpinejs/focus';
+
+Alpine.plugin(focus);
