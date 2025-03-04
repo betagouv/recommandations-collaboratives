63929e12480efa8279aba7f099fcb1f8c9a50309
Guillaume Libersat <glibersat@sigill.org>
2025-03-04 17:47:10 +0100

    [crm] use content types for matching in tag instead of type name

--- recoco/apps/crm/templatetags/crm_tags.py
+++ recoco/apps/crm/templatetags/crm_tags.py
@@ -8,8 +8,11 @@ created: 2021-06-29 11:30:42 CEST
 """
 
 from django import template
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 from django.urls import reverse
 
+from recoco.apps.addressbook.models import Organization
 from recoco.apps.crm.models import Note
 from recoco.apps.projects.models import Project
 
@@ -24,12 +27,16 @@ def projects_for_tag(tag_name):
 
 @register.simple_tag
 def get_note_update_url(note: Note) -> str | None:
-    match note.content_type.name.lower():
-        case "project":
+    project_ct = ContentType.objects.get_for_model(Project)
+    organization_ct = ContentType.objects.get_for_model(Organization)
+    user_ct = ContentType.objects.get_for_model(User)
+
+    match note.content_type.id:
+        case project_ct.id:
             return reverse("crm-project-note-update", args=(note.object_id, note.id))
-        case "utilisateur":
+        case user_ct.id:
             return reverse("crm-user-note-update", args=(note.object_id, note.id))
-        case "organization":
+        case organization_ct.id:
             return reverse(
                 "crm-organization-note-update", args=(note.object_id, note.id)
             )
