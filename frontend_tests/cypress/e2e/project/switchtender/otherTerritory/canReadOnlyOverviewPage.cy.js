912320bc5da7d3e8f683fdb37aff43cfa0591bd9
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2025-03-05 09:58:58 +0100

    [test] fixture fix

--- frontend_tests/cypress/e2e/project/switchtender/otherTerritory/canReadOnlyOverviewPage.cy.js
+++ frontend_tests/cypress/e2e/project/switchtender/otherTerritory/canReadOnlyOverviewPage.cy.js
@@ -1,13 +1,10 @@
-import projects from '../../../../fixtures/projects/projects.json';
-const currentProject = projects[1];
-
 describe('I can read only overview page', () => {
   beforeEach(() => {
     cy.login('conseiller3');
   });
 
   it('goes to the overview page and not see the advisor note', () => {
-    cy.visit(`/project/${currentProject.pk}`);
+    cy.visit(`/project/29`);
 
     cy.contains('Note interne').should('not.exist');
   });
--- frontend_tests/cypress/e2e/project/switchtender/readOnly/canReadOnlyPublicNotes.cy.js
+++ frontend_tests/cypress/e2e/project/switchtender/readOnly/canReadOnlyPublicNotes.cy.js
@@ -1,13 +1,10 @@
-import projects from '../../../../fixtures/projects/projects.json';
-const currentProject = projects[1];
-
 describe('I can read only public notes', () => {
   beforeEach(() => {
     cy.login('conseiller3');
   });
 
   it('cant goes to public notes ', () => {
-    cy.visit(`/project/${currentProject.pk}/`);
+    cy.visit(`/project/29/`);
 
     cy.get('[data-test-id="project-navigation-conversations"]').should(
       'be.disabled'
