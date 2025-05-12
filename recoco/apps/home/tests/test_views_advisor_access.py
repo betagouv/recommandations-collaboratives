1a30c77b4010100a463f50c4b596ddb2a9326bf0
Etchegoyen Matthieu <etchegom@gmail.com>
2025-05-12 10:17:15 +0200

    add tests

--- recoco/apps/home/tests/test_views_advisor_access.py
+++ recoco/apps/home/tests/test_views_advisor_access.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock, patch
+
 import pytest
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -26,10 +28,11 @@ class TestAdvisorAccessRequestView:
         assert response.url == "/"
 
     @pytest.mark.django_db(transaction=True)
-    def test_advisor_access_request(self, client, current_site):
+    def test_post_request(self, client, current_site):
         user = baker.make(User)
         baker.make(Department, code="64", name="Pyrénées-Atlantiques")
         baker.make(Department, code="33", name="Gironde")
+        baker.make(Department, code="40", name="Landes")
 
         client.force_login(user)
         response = client.get(reverse("advisor-access-request"))
@@ -53,3 +56,103 @@ class TestAdvisorAccessRequestView:
         assert advisor_access_request.user == user
         assert advisor_access_request.status == "PENDING"
         assert advisor_access_request.departments.count() == 2
+
+        response = client.post(
+            reverse("advisor-access-request"),
+            data={"departments": ["64", "33", "40"]},
+        )
+        assert response.status_code == 200
+
+        advisor_access_request.refresh_from_db()
+        assert AdvisorAccessRequest.objects.count() == 1
+        assert advisor_access_request.departments.count() == 3
+
+
+class TestAdvisorAccessRequestModeratorView:
+    @pytest.mark.django_db
+    def test_redirect_anonymous(self, client):
+        response = client.get(reverse("advisor-access-request-moderator", args=[1]))
+        assert response.status_code == 302
+        assert response.url == "/accounts/login/?next=/advisor-access-request/1/"
+
+    @pytest.mark.django_db
+    @patch("recoco.apps.home.views.is_project_moderator", Mock(return_value=False))
+    def test_not_project_moderator(self, client):
+        user = baker.make(User)
+        client.force_login(user)
+        response = client.get(
+            client.get(reverse("advisor-access-request-moderator", args=[1]))
+        )
+        # FIXME:
+        assert response.status_code != 200
+        # assert response.status_code == 403
+
+    @pytest.mark.django_db
+    @patch("recoco.apps.home.views.is_project_moderator", Mock(return_value=True))
+    def test_object_not_found(self, client):
+        user = baker.make(User)
+        client.force_login(user)
+        response = client.get(
+            client.get(reverse("advisor-access-request-moderator", args=[1]))
+        )
+        assert response.status_code == 404
+
+    @pytest.mark.django_db
+    @patch("recoco.apps.home.views.is_project_moderator", Mock(return_value=True))
+    def test_get_context(self, client, current_site):
+        user = baker.make(User)
+        client.force_login(user)
+
+        advisor_access_request = baker.make(AdvisorAccessRequest, site=current_site)
+        advisor_access_request.departments.add(
+            baker.make(Department, code="64", name="Pyrénées-Atlantiques"),
+            baker.make(Department, code="33", name="Gironde"),
+        )
+
+        url = reverse(
+            "advisor-access-request-moderator", args=[advisor_access_request.id]
+        )
+
+        response = client.get(url)
+        assert response.status_code == 200
+        assert response.context["advisor_access_request"] == advisor_access_request
+        assert response.context["departments"] == [
+            {"name": "Gironde", "code": "33"},
+            {"name": "Pyrénées-Atlantiques", "code": "64"},
+        ]
+        assert response.context["selected_departments"] == ["33", "64"]
+        assert response.context["form"] is not None
+
+        advisor_access_request.status = "ACCEPTED"
+        advisor_access_request.save()
+
+        response = client.get(url)
+        assert response.status_code == 302
+        assert response.url == reverse("projects-moderation-list")
+
+    @pytest.mark.django_db
+    @patch("recoco.apps.home.views.is_project_moderator", Mock(return_value=True))
+    def test_post_request(self, client, current_site):
+        user = baker.make(User)
+        client.force_login(user)
+
+        advisor_access_request = baker.make(AdvisorAccessRequest, site=current_site)
+        (baker.make(Department, code="64", name="Pyrénées-Atlantiques"),)
+        (baker.make(Department, code="33", name="Gironde"),)
+
+        url = reverse(
+            "advisor-access-request-moderator", args=[advisor_access_request.id]
+        )
+
+        response = client.post(url, data={"departments": ["dummy_code"]})
+        assert response.status_code == 200
+        assert response.context["form"].errors is not None
+
+        response = client.post(url, data={"departments": ["64", "33"]})
+        assert response.status_code == 302
+        assert response.url == reverse("projects-moderation-list")
+
+        advisor_access_request.refresh_from_db()
+        assert list(
+            advisor_access_request.departments.values_list("code", flat=True)
+        ) == ["33", "64"]
