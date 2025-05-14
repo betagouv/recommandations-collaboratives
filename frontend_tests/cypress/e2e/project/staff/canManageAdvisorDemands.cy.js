62a27c5594a8d9ebbf57c535e624a6de8ef73af0
Louis <louis.pluvinage0@gmail.com>
2025-05-14 15:20:24 +0200

    [test] add test to manage each case of advisor moderation

--- frontend_tests/cypress/e2e/project/staff/canManageAdvisorDemands.cy.js
+++ frontend_tests/cypress/e2e/project/staff/canManageAdvisorDemands.cy.js
@@ -4,7 +4,6 @@ describe('I can go to the dashboard and see the pending demand for advising, and
   });
 
   it('approves a national advisor ', () => {
-    sessionStorage.setItem('view', 'advisor');
     cy.visit('/projects/moderation');
     cy.get('[data-test-id="advisor-view"]').click({
       force: true,
@@ -48,7 +47,38 @@ describe('I can go to the dashboard and see the pending demand for advising, and
     );
   });
 
-  xit('modify then accept a regional advisor', () => {
-    // data-test-id="modify-advisor-access"
+  it('modify then accept a regional advisor', () => {
+    cy.visit('/projects/moderation');
+    cy.get('[data-test-id="advisor-view"]').click({
+      force: true,
+    });
+
+    cy.contains('regionalAdvisorRequest@test.fr')
+      .closest("[data-test-id='moderation-advisor-card']")
+      .find('[data-test-id="modify-advisor-access"]')
+      .click();
+
+    cy.get('[data-test-id="remove-selected-item"]').first().click();
+
+    cy.get('[data-test-id="save-modif-departments"]').click();
+
+    cy.get('[data-test-id="moderation-advisor-card"]')
+      .find('[data-test-id="department-ask-for-access-advisor"]')
+      .should('have.length', 1);
+
+    cy.contains('regionalAdvisorRequest@test.fr')
+      .closest("[data-test-id='moderation-advisor-card']")
+      .find('[data-test-id="accept-advisor-access"]')
+      .click();
+
+    // Check that the advisor is now in the list of advisors (NOT WORKING ON PROD)
+    // cy.visit('/crm/users/?username=regionalAdvisor&role=1&ordering=');
+    // cy.contains('NationalAdvisor');
+
+    cy.visit('/projects/moderation');
+    cy.get('[data-test-id="advisor-account-moderation-page"]').should(
+      'not.include.text',
+      'regionalAdvisorRequest@test.fr'
+    );
   });
 });
