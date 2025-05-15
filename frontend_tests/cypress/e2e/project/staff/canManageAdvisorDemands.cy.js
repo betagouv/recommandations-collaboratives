f6a7b1448f34a9c0518aa96b3fe0dbda74132325
Louifine <louis.pluvinage0@gmail.com>
2025-05-15 16:28:39 +0200

    [fix] suppress unused code comment on test to verify if an access request for user worked

--- frontend_tests/cypress/e2e/project/staff/canManageAdvisorDemands.cy.js
+++ frontend_tests/cypress/e2e/project/staff/canManageAdvisorDemands.cy.js
@@ -14,10 +14,6 @@ describe('I can go to the dashboard and see the pending demand for advising, and
       .find('[data-test-id="accept-advisor-access"]')
       .click();
 
-    // Check that the advisor is now in the list of advisors (NOT WORKING ON PROD)
-    // cy.visit('/crm/users/?username=NationalAdvisor&role=1&ordering=');
-    // cy.contains('NationalAdvisor');
-
     cy.visit('/projects/moderation');
     cy.get('[data-test-id="advisor-account-moderation-page"]').should(
       'not.include.text',
@@ -35,10 +31,6 @@ describe('I can go to the dashboard and see the pending demand for advising, and
       .closest("[data-test-id='moderation-advisor-card']")
       .find('[data-test-id="refuse-advisor-access"]')
       .click();
-
-    // Check that the advisor is now in the list of advisors (NOT WORKING ON PROD)
-    // cy.visit('/crm/users/?username=refuseAdvisor&role=1&ordering=');
-    // cy.not.contains('refuseAdvisor');
 
     cy.visit('/projects/moderation');
     cy.get('[data-test-id="advisor-account-moderation-page"]').should(
