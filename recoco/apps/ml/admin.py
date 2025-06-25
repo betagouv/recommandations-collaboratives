730dff1116866681e7452dca0db488938c1abd0a
Guillaume Libersat <glibersat@sigill.org>
2025-06-25 10:02:40 +0200

    Introduce ML app  - Models and admin for LLM  - Add app to settings

---
+++ recoco/apps/ml/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from . import models
+
+
+@admin.register(models.LLMConfig)
+class LLMConfigAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.LLMPrompt)
+class LLMPromptAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Summary)
+class SummaryAdmin(admin.ModelAdmin):
+    readonly_fields = ["prompt", "config"]
---
+++ recoco/apps/ml/models.py
@@ -0,0 +1,32 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+
+class LLMPrompt(models.Model):
+    name = models.CharField(max_length=64)
+    text = models.TextField()
+
+
+class LLMConfig(models.Model):
+    name = models.CharField(max_length=64)
+    uri = models.URLField("lmstdio://xxxxx")
+
+
+class Summary(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveBigIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    text = models.TextField()
+    config = models.ForeignKey(
+        LLMConfig, null=True, blank=True, on_delete=models.SET_NULL
+    )
+    prompt = models.ForeignKey(
+        LLMPrompt, null=True, blank=True, on_delete=models.SET_NULL
+    )
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["content_type", "object_id"]),
+        ]
