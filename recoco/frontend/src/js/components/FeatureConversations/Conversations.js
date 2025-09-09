1b7759cb01555e309676adbb5e268bf226f4b43c
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2025-09-09 17:00:34 +0200

    feat(new-conv): display recommendation

--- recoco/frontend/src/js/components/FeatureConversations/Conversations.js
+++ recoco/frontend/src/js/components/FeatureConversations/Conversations.js
@@ -1,7 +1,15 @@
 import Alpine from '../../utils/globals';
 
-Alpine.data('Conversations', (feed) => ({
+Alpine.data('Conversations', (feed, projectId) => ({
+  projectId,
   feed,
+  init() {},
+  get recommendations() {
+    return this.$store.tasksData.tasks;
+  },
+  getRecommendationById(id) {
+    return this.recommendations.find((task) => task.id === id);
+  },
   getMessageById(id) {
     return this.feed.messages.find((message) => message.id === +id);
   },
