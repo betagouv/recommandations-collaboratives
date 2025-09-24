e0f1df7a2658286a9f48ed4eaaa04f697c34398a
Louis <louis.pluvinage0@gmail.com>
2025-09-24 15:27:41 +0200

    refactor(conversations): consolidate message counting logic into a single method + add style to count display

--- recoco/frontend/src/js/components/FeatureConversations/Conversations.js
+++ recoco/frontend/src/js/components/FeatureConversations/Conversations.js
@@ -32,11 +32,7 @@ Alpine.data('Conversations', (projectId, currentUserId) => ({
     this.$store.tasksData._subscribe(() => {
       this.tasks = this.$store.tasksData.tasks;
     });
-    this.countOf.messages = this.countMessagesInDiscussion();
-    this.countOf.tasks = this.countTasksInDiscussion();
-    this.countOf.contacts = this.countContactsInDiscussion();
-    this.countOf.documents = this.countDocumentsInDiscussion();
-    this.countOf.new_messages = this.countNewMessagesInDiscussion();
+    this.countElementsInDiscussion();
   },
   getRecommendationById(id) {
     const foundRecommendation = this.tasks.find(
@@ -150,112 +146,23 @@ Alpine.data('Conversations', (projectId, currentUserId) => ({
       }
     }
   },
-  countDocumentsInDiscussion() {
-    let count = 0;
+  countElementsInDiscussion() {
+    //TODO: add count of new messages since last visit
     for (const message of this.feed.messages) {
       for (const node of message.nodes) {
         if (node.type === 'DocumentNode') {
-          count += 1;
+          this.countOf.documents += 1;
         }
-      }
-    }
-    return count;
-  },
-  countTasksInDiscussion() {
-    let count = 0;
-    for (const message of this.feed.messages) {
-      for (const node of message.nodes) {
         if (node.type === 'RecommendationNode') {
-          count += 1;
+          this.countOf.tasks += 1;
         }
-      }
-    }
-    return count;
-  },
-  countContactsInDiscussion() {
-    let count = 0;
-    for (const message of this.feed.messages) {
-      for (const node of message.nodes) {
         if (node.type === 'ContactNode') {
-          count += 1;
+          this.countOf.contacts += 1;
         }
-      }
-    }
-    return count;
-  },
-  countMessagesInDiscussion() {
-    let count = 0;
-    console.log(this.feed.messages);
-    for (const message of this.feed.messages) {
-      for (const node of message.nodes) {
         if (node.type === 'MarkdownNode') {
-          count += 1;
+          this.countOf.messages += 1;
         }
       }
     }
-    return count;
-  },
-  countNewMessagesInDiscussion() {
-    let count = 0;
-    for (const message of this.feed.messages) {
-      if (!message.read_by.includes(this.currentUserId)) {
-        count += 1;
-      }
-    }
-    return count;
-  },
-  countDocumentsInDiscussion() {
-    let count = 0;
-    for (const message of this.feed.messages) {
-      for (const node of message.nodes) {
-        if (node.type === 'DocumentNode') {
-          count += 1;
-        }
-      }
-    }
-    return count;
-  },
-  countTasksInDiscussion() {
-    let count = 0;
-    for (const message of this.feed.messages) {
-      for (const node of message.nodes) {
-        if (node.type === 'RecommendationNode') {
-          count += 1;
-        }
-      }
-    }
-    return count;
-  },
-  countContactsInDiscussion() {
-    let count = 0;
-    for (const message of this.feed.messages) {
-      for (const node of message.nodes) {
-        if (node.type === 'ContactNode') {
-          count += 1;
-        }
-      }
-    }
-    return count;
-  },
-  countMessagesInDiscussion() {
-    let count = 0;
-    console.log(this.feed.messages);
-    for (const message of this.feed.messages) {
-      for (const node of message.nodes) {
-        if (node.type === 'MarkdownNode') {
-          count += 1;
-        }
-      }
-    }
-    return count;
-  },
-  countNewMessagesInDiscussion() {
-    let count = 0;
-    for (const message of this.feed.messages) {
-      if (!message.read_by.includes(this.currentUserId)) {
-        count += 1;
-      }
-    }
-    return count;
   },
 }));
