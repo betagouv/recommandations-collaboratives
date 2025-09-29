b0903cbe6b4ec10a228126c4879dd5f445ecfb63
alice.telescoop <alice@telescoop.fr>
2025-09-29 19:21:46 +0200

    feat(new-conv): extract document upload consequences and use in rest

--- recoco/apps/projects/views/documents.py
+++ recoco/apps/projects/views/documents.py
@@ -21,7 +21,6 @@ from recoco.utils import has_perm_or_403
 
 from .. import models, signals
 from ..forms import DocumentUploadForm
-from ..utils import get_collaborators_for_project
 
 
 @login_required
@@ -79,15 +78,6 @@ def document_upload(request, project_id):
 
             try:
                 instance.save()
-
-                # Reactivate project if was set inactive
-                if request.user in get_collaborators_for_project(project):
-                    project.last_members_activity_at = timezone.now()
-
-                    if project.inactive_since:
-                        project.reactivate()
-
-                    project.save()
 
                 signals.document_uploaded.send(
                     sender=document_upload, instance=instance
