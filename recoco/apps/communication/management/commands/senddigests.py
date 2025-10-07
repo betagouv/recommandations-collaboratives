e68840e195f8b115693fc46e8fe540b4fe69c757
alice.telescoop <alice@telescoop.fr>
2025-10-07 16:32:28 +0200

    feat(new-conv): first shot at sending digests per user

--- recoco/apps/communication/management/commands/senddigests.py
+++ recoco/apps/communication/management/commands/senddigests.py
@@ -28,39 +28,56 @@ class Command(BaseCommand):
         parser.add_argument(
             "-d", "--dry-run", action="store_true", help="Do not actually send stuff"
         )
+        parser.add_argument(
+            "-u",
+            "--user-id",
+            action="store",
+            help="Do not actually send stuff",
+            type=int,
+        )
 
     def handle(self, *args, **options):
         dry_run = options["dry_run"]
+        user_id = options["user_id"]
 
-        self.send_email_digests(dry_run=dry_run)
+        self.send_email_digests(dry_run=dry_run, user_id=user_id)
 
-    def send_email_digests(self, dry_run):
+    def send_email_digests(self, dry_run, user_id):
         for site in Site.objects.all():
             with settings.SITE_ID.override(site.pk):
                 logger.info(f"\n#### Sending digests for site <{site.domain}> ####\n")
-                self.send_email_digests_for_site(site, dry_run)
+                self.send_email_digests_for_site(site, dry_run, user_id)
 
-    def send_email_digests_for_site(self, site, dry_run):
+    def send_email_digests_for_site(self, site, dry_run, user_id):
         advisor_group = get_group_for_site("advisor", site, create=True)
 
         # only send emails to active users and those actually linked to the current site
-        active_users = auth_models.User.objects.filter(
-            is_active=True, profile__sites=site
-        )
+        active_users = auth_models.User.objects
 
-        # Send reminders (new recommendation + whatsup)
-        logger.info("** Sending Project Reminders **")
-        for project in project_models.Project.on_site.all():
-            digests.send_reminder_digests_by_project(project, dry_run)
+        if user_id is not None:
+            active_users = active_users.filter(pk=user_id)
 
-        # Send project collaborators new recommendations digest
-        logger.info("** Sending new recommendations digests **")
-        for (
-            project
-        ) in project_models.Project.on_site.all():  # FIXME include inactive project?
-            for user in project.members.filter(is_active=True):
-                if digests.send_digests_for_new_recommendations_by_user(user, dry_run):
-                    logger.info(f"Sent new reco digest for {user} on {project.name}")
+            logger.info("Specific user required. Ignoring whatsup by project digests")
+        else:
+            active_users = active_users.filter(is_active=True, profile__sites=site)
+
+            # Send reminders (new recommendation + whatsup)
+            logger.info("** Sending Project Reminders **")
+            for project in project_models.Project.on_site.all():
+                digests.send_reminder_digests_by_project(project, dry_run)
+
+            # Send project collaborators new recommendations digest
+            logger.info("** Sending new recommendations digests **")
+            for project in (
+                project_models.Project.on_site.all()
+            ):  # FIXME include inactive project?
+                for user in project.members.filter(is_active=True):
+                    if digests.send_digests_for_new_recommendations_by_user(
+                        user, dry_run
+                    ):
+                        logger.info(
+                            f"Sent new reco digest for {user} on {project.name}"
+                        )
 
         # Digests for non switchtenders
         logger.info("** Sending general digests **")  # FIXME include inactive project?
