546f1594208b79193c4a2b8b94295707355e7264
Guillaume Libersat <glibersat@sigill.org>
2025-10-20 15:33:51 +0200

    feat(conversation): update addressbook permissions  - anonymous are forbidden for everything  - logged in user can read a single entity  - uses sites.use_addressbook for read/list/searchooo  - uses sites.change_addressbook for writes

--- recoco/apps/addressbook/tests/test_rest_organization_group.py
+++ recoco/apps/addressbook/tests/test_rest_organization_group.py
@@ -8,27 +8,53 @@ from ..models import OrganizationGroup
 
 
 @pytest.mark.django_db
-def test_anonymous_can_list_organizationgroups_but_not_create(api_client):
+def test_anonymous_cannot_list_organizationgroups_nor_create(api_client):
     url = reverse("api-addressbook-organization-group-list")
 
     response = api_client.get(url)
-    assert response.status_code == 200
+    assert response.status_code == 403
 
     response = api_client.post(url, data={})
     assert response.status_code == 403
 
 
 @pytest.mark.django_db
-def test_anonymous_can_read_organizationgroup_but_not_update(api_client):
+def test_logged_in_user_cannot_list_organizationgroups_nor_create(api_client):
+    url = reverse("api-addressbook-organization-group-list")
+
+    with login(api_client):
+        response = api_client.get(url)
+        assert response.status_code == 403
+
+        response = api_client.post(url, data={})
+        assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_anonymous_cannot_read_organizationgroup_nor_update(api_client):
     group = baker.make(OrganizationGroup)
 
     url = reverse("api-addressbook-organization-group-detail", args=[group.pk])
 
     response = api_client.get(url)
-    assert response.status_code == 200
+    assert response.status_code == 403
 
     response = api_client.put(url, data={})
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_logged_in_user_can_read_organizationgroup_but_not_update(api_client):
+    group = baker.make(OrganizationGroup)
+
+    url = reverse("api-addressbook-organization-group-detail", args=[group.pk])
+
+    with login(api_client):
+        response = api_client.get(url)
+        assert response.status_code == 200
+
+        response = api_client.put(url, data={})
+        assert response.status_code == 403
 
 
 @pytest.mark.django_db
@@ -37,7 +63,7 @@ def test_orga_group_search(api_client):
     z_org = baker.make(OrganizationGroup, name="zoologie")
     search = "zoo"
 
-    with login(api_client):
+    with login(api_client, groups=["example_com_staff"]):
         url = reverse("api-addressbook-organization-group-list")
         response = api_client.get(url, {"search": search})
         ids = [search_response["id"] for search_response in response.data["results"]]
