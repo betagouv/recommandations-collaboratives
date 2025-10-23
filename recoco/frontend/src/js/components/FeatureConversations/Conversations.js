702238ae6d4c0535cd118ba93efaa7442fcdb495
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2025-10-23 10:00:54 +0200

    feat(new-conv): add correct button attachment

--- recoco/frontend/src/js/components/FeatureConversations/Conversations.js
+++ recoco/frontend/src/js/components/FeatureConversations/Conversations.js
@@ -297,6 +297,10 @@ Alpine.data('Conversations', (projectId, currentUserId) => ({
     this.isEditorInReplyMode = true;
     Alpine.raw(this.$store.editor.editorInstance).commands.focus();
   },
+  onClickHandleAttachment() {
+    this.isEditorFocused = true;
+    Alpine.raw(this.$store.editor.editorInstance).commands.focus();
+  },
   onClickCancelReply() {
     this.messageIdToReply = null;
     this.isEditorInReplyMode = false;
