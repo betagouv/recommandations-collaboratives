d783317f57c46733273830fb6ca8f90f01202c02
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2025-12-10 15:47:52 +0100

    feat(conv): edit error message when upload file failed

--- recoco/frontend/src/js/components/FeatureConversations/Conversations.js
+++ recoco/frontend/src/js/components/FeatureConversations/Conversations.js
@@ -267,8 +267,11 @@ Alpine.data('Conversations', (projectId, currentUserId) => ({
           documentNodesToUpload.map((node) => this.uploadFile(node.file))
         );
       } catch (error) {
+        const errorMessage =
+          error.response?.data?.the_file?.[0] ||
+          "Contactez nous via le chat pour obtenir de l'aide.";
         this.$store.app.displayToastMessage({
-          message: `Erreur lors de l'envoie d'un document`,
+          message: `Erreur lors de l'envoie d'un document : ${errorMessage}`,
           timeout: 5000,
           type: ToastType.error,
         });
