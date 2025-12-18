8dfafceaff8c1e0b1155caf6e3cdea684cd65935
Guillaume Libersat <glibersat@sigill.org>
2025-12-18 10:51:08 +0100

    Revert "fix(ywh): Échappement des scripts potentiels lors du rendu de markdown "

--- recoco/frontend/src/js/utils/markdown.js
+++ recoco/frontend/src/js/utils/markdown.js
@@ -1,7 +1,5 @@
 import { marked } from 'marked';
-import DOMPurify from 'dompurify';
 
 export function renderMarkdown(content) {
-  const html = marked.parse(content);
-  return DOMPurify.sanitize(html);
+  return marked.parse(content);
 }
