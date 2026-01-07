946d4849ae9ea21888892adc633f5f0cace1ce44
Louis <louis.pluvinage0@gmail.com>
2026-01-07 15:16:27 +0100

    [feat] add every elements on the right column

--- recoco/frontend/src/js/components/Resources/CreateResource.js
+++ recoco/frontend/src/js/components/Resources/CreateResource.js
@@ -4,8 +4,20 @@ import api from '../../utils/api';
 Alpine.data('CreateResource', () => {
   return {
     is_draft: true,
+    keywords_options: [],
     init() {
       console.log('CreateResource');
+      this.fetchKeywords();
+    },
+    fetchKeywords() {
+      // api.get('/keywords').then(response => {
+      //   this.keywords_options = response.data;
+      // });
+      this.keywords_options = [
+        { id: 1, text: 'Environnement', value: 'environnement', search: 'environnement' },
+        { id: 2, text: 'Économie', value: 'economie', search: 'economie' },
+        { id: 3, text: 'Social', value: 'social', search: 'social' },
+      ];
     },
   };
 });
