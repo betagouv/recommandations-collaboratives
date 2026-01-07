107ed9c1976233af4c706d8b9397e864bf5eb1c7
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2026-01-07 15:52:25 +0100

    feat(ressource): payload structure

--- recoco/frontend/src/js/components/Resources/CreateResource.js
+++ recoco/frontend/src/js/components/Resources/CreateResource.js
@@ -5,6 +5,41 @@ Alpine.data('CreateResource', () => {
   return {
     is_draft: true,
     keywords_options: [],
+    newRessourcePayload: {
+      title: '',
+      subtitle: '',
+      summary: '',
+      content: '',
+      status: 'DRAFT',
+      category: '',
+      keywords: [],
+      support_orga: '',
+      departments: [],
+      expires_on: '',
+      contacts: [],
+    },
+    options: [
+      {
+        value: 'PRE_DRAFT',
+        text: 'Incomplet',
+      },
+      {
+        value: 'DRAFT',
+        text: 'A modérer',
+      },
+      {
+        value: 'TO_PROCESS',
+        text: 'A traiter',
+      },
+      {
+        value: 'VALIDATED',
+        text: 'Validé',
+      },
+      {
+        value: 'REJECTED',
+        text: 'Refusé',
+      },
+    ],
     init() {
       console.log('CreateResource');
       this.fetchKeywords();
@@ -14,7 +49,12 @@ Alpine.data('CreateResource', () => {
       //   this.keywords_options = response.data;
       // });
       this.keywords_options = [
-        { id: 1, text: 'Environnement', value: 'environnement', search: 'environnement' },
+        {
+          id: 1,
+          text: 'Environnement',
+          value: 'environnement',
+          search: 'environnement',
+        },
         { id: 2, text: 'Économie', value: 'economie', search: 'economie' },
         { id: 3, text: 'Social', value: 'social', search: 'social' },
       ];
