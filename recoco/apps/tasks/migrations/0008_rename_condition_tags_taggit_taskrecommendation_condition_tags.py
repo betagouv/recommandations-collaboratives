5d8564327b6fe6b0076af314cc26dfce6d5dd079
alice.telescoop <alice@telescoop.fr>
2026-01-29 14:21:17 +0100

    fix(task): one migration after another

--- recoco/apps/tasks/migrations/0008_rename_condition_tags_taggit_taskrecommendation_condition_tags.py
+++ recoco/apps/tasks/migrations/0008_rename_condition_tags_taggit_taskrecommendation_condition_tags.py
@@ -5,7 +5,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("tasks", "0007_taskrecommendation_condition_tags_taggit"),
+        ("tasks", "0008_alter_task_content"),
     ]
 
     operations = [
