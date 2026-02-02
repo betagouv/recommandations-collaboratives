1988f2d5f9b6b32eac7064342f28fff6df333723
Louis <louis.pluvinage0@gmail.com>
2026-02-02 16:37:40 +0100

    [fix] impossible to select thje first category from the filter

--- recoco/frontend/src/js/components/resourceCategoryFilter.js
+++ recoco/frontend/src/js/components/resourceCategoryFilter.js
@@ -5,17 +5,18 @@ Alpine.data('ResourceFilter', (categoryOptions) => ({
     selected: [],
     selectedIds: [],
     options: categoryOptions || [],
-    categoryMap: categoryOptions.map(cat => 'cat'+cat.value),
+    categoryMap: Object.fromEntries(categoryOptions.map(cat => [cat.value, 'cat' + cat.value])),
 
     init() {
         const params = new URLSearchParams(document.location.search);
+        const reverseMap = Object.fromEntries(
+            Object.entries(this.categoryMap).map(([k, v]) => [v, k])
+        );
         for (const [key, value] of params) {
-            if (this.categoryMap.includes(key) && value === 'on') {
-                this.selected.push(this.categoryMap.indexOf(key).toString());
-                this.selectedIds.push(this.categoryMap.indexOf(key).toString());
-                console.log('preselected category:', key);
+            if (reverseMap[key] && value === 'on') {
+                this.selected.push(reverseMap[key]);
+                this.selectedIds.push(reverseMap[key]);
             }
         }
-
     },
 }));
