501529be94f967d023b011d9f6064e38a657690b
alice.telescoop <alice@telescoop.fr>
2026-02-18 16:37:06 +0100

    chore(organization): show how orga "Mairie" is filled

--- recoco/apps/home/signals.py
+++ recoco/apps/home/signals.py
@@ -5,16 +5,19 @@ authors: guillaume.libersat@beta.gouv.fr, raphael.marvie@beta.gouv.fr
 created: 2023-06-27 08:06:10 CEST
 """
 
+import sentry_sdk
 from actstream import action
 from allauth.account.signals import user_logged_in as allauth_user_logged_in
 from allauth.account.signals import user_signed_up as allauth_user_signed_up
 from django.contrib.auth.models import update_last_login
 from django.contrib.auth.signals import user_logged_in
 from django.contrib.sites.shortcuts import get_current_site
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from magicauth.signals import user_logged_in as magicauth_user_logged_in
 
 from recoco import verbs
+from recoco.apps.home.models import UserProfile
 from recoco.apps.projects.utils import refresh_user_projects_in_session
 
 
@@ -42,6 +45,16 @@ user_logged_in.disconnect(update_last_login, dispatch_uid="update_last_login")
 @receiver(allauth_user_signed_up)
 def post_signup_workflow(sender, request, user, **kwargs):
     pass
+
+
+@receiver(post_save, sender=UserProfile)
+def watch_organisation_to_understand_mystery(instance: UserProfile, **kwargs):
+    if getattr(instance.organization, "id", None) == 781:
+        text = (
+            f"User {instance.user.id} was assigned to mysterious organization 'Mairie'"
+        )
+        sentry_sdk.capture_exception(Exception(text))
+        print(text)
 
 
 # eof
