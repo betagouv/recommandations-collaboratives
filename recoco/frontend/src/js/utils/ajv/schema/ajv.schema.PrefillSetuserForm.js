89c6c92a4af679a41a13159f1d51031507d006d1
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2026-03-04 18:15:12 +0100

    feat: Add optional text field schema with minLength and maxLength validation

--- recoco/frontend/src/js/utils/ajv/schema/ajv.schema.PrefillSetuserForm.js
+++ recoco/frontend/src/js/utils/ajv/schema/ajv.schema.PrefillSetuserForm.js
@@ -2,6 +2,7 @@ import {
   schemaFormInputs,
   maxLengthErrorMessage,
   minLengthErrorMessage,
+  textOptional,
 } from './ajv.schema.forms';
 
 /**
@@ -40,7 +41,7 @@ export const schemaOnboardingPrefillSetuserFormValidator = {
       ],
     },
     role: { $ref: '#/definitions/text' },
-    message: { $ref: '#/definitions/text' },
+    message: textOptional(3, 100),
     email: { $ref: '#/definitions/email' },
     phone: { $ref: '#/definitions/phone' },
   },
