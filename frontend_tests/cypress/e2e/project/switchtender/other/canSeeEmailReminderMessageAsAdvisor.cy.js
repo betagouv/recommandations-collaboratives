c6616f51a7c28d116efd3cb86730632931f796cb
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2026-04-29 18:21:07 +0200

    fix: canSeeEmailReminderMessageAsAdvisor by by passing join project with fixture

--- frontend_tests/cypress/e2e/project/switchtender/other/canSeeEmailReminderMessageAsAdvisor.cy.js
+++ frontend_tests/cypress/e2e/project/switchtender/other/canSeeEmailReminderMessageAsAdvisor.cy.js
@@ -4,23 +4,21 @@ import projectView from '../../../../support/views/project';
 const ownerEmail = 'bob@test.fr';
 describe('As project advisor, I can see project email reminders @page-projet-presentation-rappel-email', () => {
   it('Displays no reminder message on projects with no scheduled emails', () => {
-    const currentProject = projects[19];
+    const currentProject = projects.find((x) => x.pk == 20);
     cy.login('conseiller1');
     cy.visit(`/project/${currentProject.pk}`);
-    projectView.joinAsAdvisorWithSelector();
     projectView.checkNextEmailReminder({ role: 'advisor' });
   });
 
   it('Displays a reminder message when an email is scheduled to be sent', () => {
-    const currentProject = projects[20];
+    const currentProject = projects.find((x) => x.pk == 21);
     cy.login('conseiller1');
     cy.visit(`/project/${currentProject.pk}`);
-    projectView.joinAsAdvisorWithSelector();
     projectView.checkNextEmailReminder({ email: ownerEmail });
   });
 
   it('Reminders settings popup is accessible and provides access to preferences panel', () => {
-    const currentProject = projects[20];
+    const currentProject = projects.find((x) => x.pk == 21);
     cy.login('conseiller1');
     cy.visit(`/project/${currentProject.pk}`);
     projectView.checkEmailReminderTooltip();
