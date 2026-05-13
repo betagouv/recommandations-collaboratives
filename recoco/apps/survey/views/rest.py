90e62f18407b1d410d93eb9c81fb5fa4c767227e
alice.telescoop <alice@telescoop.fr>
2026-05-13 16:16:54 +0200

    feat(survey): also access survey and session linked to deleted project

--- recoco/apps/survey/views/rest.py
+++ recoco/apps/survey/views/rest.py
@@ -1,22 +1,43 @@
+from django_filters import ModelChoiceFilter
+from django_filters import rest_framework as filters_drf
 from rest_framework.generics import ListAPIView
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 
+from recoco.apps.projects.filters import request_show_deleted_projects
 from recoco.apps.projects.models import Project
+from recoco.apps.survey.models import Answer, Question, Session
+from recoco.apps.survey.serializers import (
+    AnswerSerializer,
+    QuestionSerializer,
+    SessionSerializer,
+)
 from recoco.rest_api.permissions import IsStaffForSite
 
-from ..models import Answer, Question, Session
-from ..serializers import AnswerSerializer, QuestionSerializer, SessionSerializer
+
+def projects_not_deleted_by_default(request):
+    queryset = Project.all_on_site.for_user(request.user)
+    if request_show_deleted_projects(request):
+        queryset = queryset.filter(deleted=None)
+    return queryset
+
+
+class SessionFilterSet(filters_drf.FilterSet):
+    project_id = ModelChoiceFilter(queryset=projects_not_deleted_by_default)
+
+    class Meta:
+        model = Session
+        fields = ["project_id"]
 
 
 class SessionView(ListAPIView):
     serializer_class = SessionSerializer
     permission_classes = [IsAuthenticated]
-    filterset_fields = ["project_id"]
+    filterset_class = SessionFilterSet
     pagination_class = LimitOffsetPagination
 
     def get_queryset(self):
-        project_ids = Project.on_site.for_user(self.request.user).values_list(
+        project_ids = projects_not_deleted_by_default(self.request).values_list(
             "id", flat=True
         )
         return Session.objects.filter(project__in=project_ids)
@@ -28,7 +49,7 @@ class SessionAnswersView(ListAPIView):
     pagination_class = LimitOffsetPagination
 
     def get_queryset(self):
-        project_ids = Project.on_site.for_user(self.request.user).values_list(
+        project_ids = projects_not_deleted_by_default(self.request).values_list(
             "id", flat=True
         )
         try:
