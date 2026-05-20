e0d0e91384145d3ba2680c59a698974abae7d2a9
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2026-05-20 13:17:03 +0200

    style: update layout style and main background color

--- recoco/frontend/vite.config.js
+++ recoco/frontend/vite.config.js
@@ -243,7 +243,12 @@ const config = {
         cardCrmStyles: resolve('./src/js/styles/card-crm.css.js'),
         GravatarCache: resolve('./src/js/components/GravatarCache.js'),
         userCrmStyles: resolve('./src/js/styles/user-crm.css.js'),
-        projectCardCrmStyles: resolve('./src/js/styles/project-card-crm.css.js'),
+        projectCardCrmStyles: resolve(
+          './src/js/styles/project-card-crm.css.js'
+        ),
+        backgroundOverride: resolve(
+          './src/js/styles/background-override.css.js'
+        ),
       },
       output: {
         chunkFileNames: undefined,
