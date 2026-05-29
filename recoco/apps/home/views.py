12da7844f02dd9ef779518a987a0985a5a3c005c
alice.telescoop <alice@telescoop.fr>
2026-05-29 16:44:26 +0200

    feat(auth): setup custom 403 page

--- recoco/apps/home/views.py
+++ recoco/apps/home/views.py
@@ -7,6 +7,8 @@ authors: raphael.marvie@beta.gouv.fr,guillaume.libersat@beta.gouv.fr
 created: 2021-08-16 15:40:08 CEST
 """
 
+import urllib
+
 import django.core.mail
 from actstream import action
 from django.contrib import messages
@@ -15,11 +17,14 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.core.exceptions import ImproperlyConfigured, PermissionDenied
 from django.db.models import Count, F, Prefetch, Q
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseForbidden
 from django.shortcuts import get_object_or_404, redirect, render
+from django.template import loader
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.utils.http import url_has_allowed_host_and_scheme
+from django.views.decorators.csrf import requires_csrf_token
+from django.views.defaults import ERROR_403_TEMPLATE_NAME
 from django.views.generic import FormView, View
 from django.views.generic.base import TemplateView
 from notifications.signals import notify
@@ -437,6 +442,27 @@ class SiteCreateView(LoginRequiredMixin, PermissionRequiredMixin, FormView):
 
     def get_success_url(self):
         return reverse("site-create")
+
+
+@requires_csrf_token
+def permission_denied(request, exception):
+    # customizes PermissionDenied to customize with next url and user's data
+
+    template = loader.get_template(ERROR_403_TEMPLATE_NAME)
+    login_url = (
+        reverse("account_login") + "?" + urllib.parse.urlencode({"next": request.path})
+    )
+    logout_url = reverse("logout") + "?" + urllib.parse.urlencode({"next": login_url})
+
+    return HttpResponseForbidden(
+        template.render(
+            request=request,
+            context={
+                "exception": str(exception),
+                "relogin_url": logout_url,
+            },
+        )
+    )
 
 
 # eof
