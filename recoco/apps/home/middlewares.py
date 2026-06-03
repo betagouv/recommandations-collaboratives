c4d9dbff6cec0b9e20cffbf42b8c0a221173bea2
alice.telescoop <alice@telescoop.fr>
2026-06-03 15:20:59 +0200

    feat(auth): magiclinks need previously set up cookie

--- recoco/apps/home/middlewares.py
+++ recoco/apps/home/middlewares.py
@@ -1,9 +1,13 @@
 from datetime import timedelta
 
 import sentry_sdk
+from django.contrib.auth import login
+from django.contrib.sites.models import Site
 from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpRequest
 from django.utils import timezone
+from sesame.middleware import AuthenticationMiddleware as SesameAuthenticationMiddleware
+from sesame.utils import get_user
 
 from recoco.apps.home.models import SiteConfiguration, UserProfile
 
@@ -55,3 +59,66 @@ class PreviousActivityMiddleware:
                 sentry_sdk.capture_exception(e)
 
         return self.get_response(request)
+
+
+# todo add resul
+class SetEnableSesameCookieMiddleware:
+    """
+    Middleware to set user cookie
+    If user is authenticated and there is no cookie, set the cookie,
+    If the user is not authenticated and the cookie remains, delete it
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request: HttpRequest):
+        response = self.get_response(request)
+        # if user and no cookie, set cookie
+        if request.user.is_authenticated and not request.COOKIES.get(
+            "enable-sesame-user-id"
+        ):
+            current_site = Site.objects.get_current()
+            domain = current_site.domain
+            response.set_signed_cookie(
+                "enable-sesame-user-id",
+                request.user.id,
+                max_age=timedelta(weeks=52 * 2),  # two years
+                domain=domain,
+                http_only=True,
+                secure=True,
+            )
+        return response
+
+
+class SesameWithCookieMiddleware(SesameAuthenticationMiddleware):
+    # this code is mainly a copy of sesame's code
+    # the difference is meant to check if a cookie exists before actually logging the user in
+    def process_request(self, request):
+        user = get_user(
+            request,
+            update_last_login=False if hasattr(request, "session") else None,
+        )
+
+        if user is None:
+            return None
+
+        # this cookie is meant to know if user had already signed in
+        # on this device, to secure this type of authentication
+        cookie_user_id = request.get_signed_cookie(
+            "enable-sesame-user-id", default=None
+        )
+
+        if cookie_user_id != user.id:
+            # we don't change the authenticated user. If the previous one does not have access,
+            # we rely that the 403 page reminds the user of the authentication status
+            return None
+
+        login(request, user)
+        # specific tests below comes from sesame original code
+        if (
+            hasattr(request, "user")
+            and request.method == "GET"
+            and not self.is_safari(request)
+        ):
+            return self.get_redirect(request)
