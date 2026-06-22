ddf09619a0d4e09b8eccc58ebe8bfe062c08c4f9
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2026-06-22 13:09:58 +0200

    refactor(project): add project status enum and update js and template

---
+++ recoco/frontend/src/js/models/projectStatus.enum.js
@@ -0,0 +1,10 @@
+export const ProjectStatus = {
+  PRE_DRAFT:{ text: 'Incomplet', color: 'fr-badge--warning'},
+  DRAFT:{ text: 'A modérer', color: 'fr-badge--new'},
+  TO_PROCESS:{ text: 'A traiter', color: 'fr-badge--info'},
+  READY:{ text: 'En attente', color: 'fr-badge--success-lighter'},
+  IN_PROGRESS:{ text: 'En cours', color: 'fr-badge--info'},
+  DONE:{ text: 'Traité', color: 'fr-badge--success-lighter'},
+  STUCK:{ text: 'Interrompu', color: 'fr-badge--info'},
+  REJECTED:{ text: 'Rejeté', color: 'fr-badge--error'},
+};
