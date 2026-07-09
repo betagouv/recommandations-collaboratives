4a052923ef7625feba1e7a7e122fae5697350c00
alice.telescoop <alice@telescoop.fr>
2026-07-09 14:43:04 +0200

    feat(home): validate brevo email validation

--- recoco/apps/home/validators.py
+++ recoco/apps/home/validators.py
@@ -1,6 +1,7 @@
 import re
 
 from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator
 
 
 class UppercaseAndDigitPasswordValidator:
@@ -26,3 +27,24 @@ class UppercaseAndDigitPasswordValidator:
 
     def get_help_text(self):
         return "Votre mot de passe doit contenir au moins une majuscule et une chiffre."
+
+
+class EmailValidatorForBrevo(EmailValidator):
+    def __call__(self, value):
+        # The maximum length of an email is 320 characters per RFC 3696
+        # section 3 but verriden to 254 for Brevo
+        if not value or "@" not in value or len(value) > 254:
+            raise ValidationError(self.message, code=self.code, params={"value": value})
+
+        user_part, domain_part = value.rsplit("@", 1)
+
+        # 64 caracters because of brevo
+        if not self.user_regex.match(user_part) or len(user_part) > 64:
+            raise ValidationError(self.message, code=self.code, params={"value": value})
+
+        if (
+            domain_part not in self.domain_allowlist
+            and not self.validate_domain_part(domain_part)
+            or len(domain_part) > 63
+        ):
+            raise ValidationError(self.message, code=self.code, params={"value": value})
