418fe6b547727c7bcebf3bfce4bf3bad527e6af5
alice.telescoop <alice@telescoop.fr>
2026-08-27 14:32:57 +0200

    feat(settings): dev adaptations for csp

--- recoco/settings/testing.py
+++ recoco/settings/testing.py
@@ -1,4 +1,7 @@
+from . import common
 from .development import *  # noqa: F403
 
 DS_AUTOLOAD_SCHEMA = False
 CELERY_TASK_ALWAYS_EAGER = True
+
+CONTENT_SECURITY_POLICY = common.CONTENT_SECURITY_POLICY
