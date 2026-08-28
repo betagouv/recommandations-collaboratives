4ea1d0f6bc7db159f01bb6595c697b93b7f4aed9
alice.telescoop <alice@telescoop.fr>
2026-08-28 15:17:08 +0200

    test(challenge): check we have request.user's challenge not others

--- recoco/apps/training/tests/test_rest.py
+++ recoco/apps/training/tests/test_rest.py
@@ -7,6 +7,8 @@ authors: raphael.marvie@beta.gouv.fr, guillaume.libersat@beta.gouv.fr
 created: 2023-06-20 14:10:36 CEST
 """
 
+from datetime import datetime
+
 import pytest
 from django.contrib.auth import models as auth_models
 from django.contrib.sites import models as site_models
@@ -145,7 +147,13 @@ def test_challenge_get_api_returns_existing_challenge_info(request, client):
         auth_models.User, email="me@example.com", last_login=timezone.now()
     )
     definition = baker.make(models.ChallengeDefinition, site=site)
-    challenge = baker.make(models.Challenge, user=user, challenge_definition=definition)
+    started_on = timezone.now()
+    baker.make(
+        models.Challenge,
+        user=user,
+        challenge_definition=definition,
+        started_on=started_on,
+    )
 
     client = APIClient()
     client.force_authenticate(user=user)
@@ -155,7 +163,45 @@ def test_challenge_get_api_returns_existing_challenge_info(request, client):
 
     assert response.status_code == 200
 
-    assert response.data["acquired_on"] is challenge.acquired_on
+    assert datetime.fromisoformat(response.data["started_on"]) == started_on
+    assert dict(response.data["challenge_definition"]) == {
+        "name": definition.name,
+        "code": definition.code,
+        "description": definition.description,
+        "icon_name": None,
+        "next_challenge": None,
+    }
+
+
+@pytest.mark.django_db
+def test_challenge_get_api_does_not_returns_existing_challenge_info_other_user(
+    request, client
+):
+    site = get_current_site(request)
+    user = baker.make(
+        auth_models.User, email="me@example.com", last_login=timezone.now()
+    )
+    other_user = baker.make(
+        auth_models.User, email="other@example.com", last_login=timezone.now()
+    )
+    definition = baker.make(models.ChallengeDefinition, site=site)
+    started_on = timezone.now()
+    baker.make(
+        models.Challenge,
+        user=other_user,
+        challenge_definition=definition,
+        started_on=started_on,
+    )
+
+    client = APIClient()
+    client.force_authenticate(user=user)
+
+    url = reverse("challenges-challenge", args=[definition.code])
+    response = client.get(url)
+
+    assert response.status_code == 200
+
+    assert response.data["started_on"] is None
     assert dict(response.data["challenge_definition"]) == {
         "name": definition.name,
         "code": definition.code,
